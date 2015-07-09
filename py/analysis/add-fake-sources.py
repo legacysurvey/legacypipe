ac19d59824da9563a3bbc989ea848732f15da432
Dustin Lang <dstndstn@gmail.com>
2015-07-09 15:14:07 -0400

    add a script testing out an idea for inserting fake sources into images

---
+++ py/analysis/add-fake-sources.py
@@ -0,0 +1,64 @@
+from tractor import *
+
+from legacypipe.runbrick import *
+from legacypipe.common import *
+
+from astrometry.util.multiproc import *
+
+'''
+Test out a code structure for inserting fake galaxies into images, by
+overriding the Decals class & DecamImage class.
+'''
+
+class FakeDecals(Decals):
+    def __init__(self, decals_dir=None, fake_sources=[]):
+        super(FakeDecals, self).__init__(decals_dir=decals_dir)
+        self.fake_sources = fake_sources
+
+    def get_image_object(self, t):
+        return FakeImage(self, t)
+
+class FakeImage(DecamImage):
+    def __init__(self, decals, t):
+        super(FakeImage, self).__init__(decals, t)
+
+    def get_tractor_image(self, **kwargs):
+        tim = super(FakeImage, self).get_tractor_image(**kwargs)
+
+        print 'Adding fake sources to', tim
+
+        for src in self.decals.fake_sources:
+            patch = src.getModelPatch(tim)
+            if patch is None:
+                continue
+            print 'Adding', src
+            patch.addTo(tim.getImage())
+        
+        return tim
+
+
+
+def main():
+    decals = FakeDecals()
+    # Our list of fake sources to insert into images...
+    decals.fake_sources = [PointSource(RaDecPos(0., 0.),
+                                       NanoMaggies(g=1000, r=1000, z=1000))]
+
+    # Now, we can either call run_brick()....
+    run_brick(None, decals=decals, radec=(0.,0.), width=100, height=100,
+              stages=['image_coadds'])
+
+    # ... or the individual stages in the pipeline:
+    if False:
+        mp = multiproc()
+        kwa = dict(W = 100, H=100, ra=0., dec=0., mp=mp, brickname='fakey')
+        kwa.update(decals=decals)
+        R = stage_tims(**kwa)
+        kwa.update(R)
+        R = stage_image_coadds(**kwa)
+        # ...
+
+
+if __name__ == '__main__':
+    main()
+    
