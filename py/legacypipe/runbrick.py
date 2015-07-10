de2bbbfd80cd89dae805ebcbe5acf3e71cc3d33d
Dustin Lang <dstndstn@gmail.com>
2015-07-10 08:40:36 -0400

    more specific imports

--- py/legacypipe/runbrick.py
+++ py/legacypipe/runbrick.py
@@ -20,13 +20,12 @@ from astrometry.util.resample import resample_with_wcs, OverlapError
 from astrometry.libkd.spherematch import match_radec
 from astrometry.util.ttime import Time
 
-from tractor import *
-from tractor.galaxy import *
-from tractor.source_extractor import *
+from tractor import Tractor, PointSource, Image, ShiftedPsf, NanoMaggies
+from tractor.ellipses import EllipseESoft, EllipseE
+from tractor.galaxy import DevGalaxy, ExpGalaxy, FixedCompositeGalaxy
 from tractor.utils import _GaussianPriors
 
 from common import *
-from runbrick_plots import *
 from runbrick_plots import _plot_mods
 
 ## GLOBALS!  Oh my!
@@ -36,7 +35,6 @@ useCeres = True
 # RGB image args used in the tile viewer:
 rgbkwargs = dict(mnmx=(-1,100.), arcsinh=1.)
 rgbkwargs_resid = dict(mnmx=(-5,5))
-
 
 def runbrick_global_init():
     if nocache:
