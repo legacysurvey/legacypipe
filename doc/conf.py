beaef867bb1d83f42489d8ae4cb0948cc257e078
Dustin Lang <dstndstn@gmail.com>
2015-07-13 11:43:20 -0400

    mocks

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,8 @@
 import sys, os
 import mock
 
-MOCK_MODULES = ['pylab', 'fitsio', 'astrometry', 'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'scipy.ndimage']
+MOCK_MODULES = ['pylab', 'fitsio', 'astrometry', 'astrometry.util', 'astrometry.libkd', 
+                'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'scipy.ndimage']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
