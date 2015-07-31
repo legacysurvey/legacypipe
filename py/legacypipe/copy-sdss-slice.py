92fe236cffe175f19162dc10a5fe4ee70577ddb9
Dustin Lang <dstndstn@gmail.com>
2015-07-31 10:01:12 -0400

    copy-sdss-slice script: add comments, py3, specific imports

--- py/legacypipe/copy-sdss-slice.py
+++ py/legacypipe/copy-sdss-slice.py
@@ -2,8 +2,16 @@
 ''''exec python -u -- "$0" ${1+"$@"} # '''
 # per http://stackoverflow.com/questions/3306518/cannot-pass-an-argument-to-python-with-usr-bin-env-python
 
-from astrometry.util.fits import *
-from astrometry.sdss.dr10 import *
+from __future__ import print_function
+
+'''
+This script copies a subset of the columns from the SDSS photoObj
+files, and also copies only fields that have at least one PRIMARY
+object.  The resulting trimmed dataset is much smaller.
+'''
+
+from astrometry.util.fits import fits_table
+from astrometry.sdss.dr10 import DR10
 import os
 import shutil
 
@@ -11,12 +19,15 @@ T = fits_table('window_flist-cut.fits')
 
 ###
 T.cut((T.ra > 180) * (T.ra < 195) * (T.dec > 5) * (T.dec < 20))
-print 'CUT TO', len(T), 'in region'
+print('CUT TO', len(T), 'in region')
 
-# projects/desi/runbrick.py : stage_srcs
+# These are the 'extra' columns we need in
+# legacypipe/runbrick.py : stage_srcs()
 extracols = ['parent', 'tai', 'mjd', 'psf_fwhm', 'objc_flags2', 'flags2',
-             'devflux_ivar', 'expflux_ivar', 'calib_status', 'raerr', 'decerr']
-# projects/desi/common.py : get_sdss_sources
+             'devflux_ivar', 'expflux_ivar', 'calib_status', 'raerr',
+             'decerr']
+# These columns are the ones we need for
+# legacypipe/common.py : get_sdss_sources()
 cols = ['objid', 'ra', 'dec', 'fracdev', 'objc_type',
         'theta_dev', 'theta_deverr', 'ab_dev', 'ab_deverr',
         'phi_dev_deg',
@@ -29,7 +40,7 @@ cols = ['objid', 'ra', 'dec', 'fracdev', 'objc_type',
         'modelflux', 'modelflux_ivar',
         'devflux', 'expflux', 'extinction'] + extracols
 
-# "dr10", "dr12", etc doesn't really matter
+# "dr10", "dr12", etc doesn't really matter (object catalogs pretty much haven't changed)
 sdss = DR10()
 sdss.useLocalTree()
 
@@ -38,36 +49,30 @@ if not os.path.exists(outdir):
     os.makedirs(outdir)
 
 for i,t in enumerate(T):
-    print 'Copying', i+1, 'of', len(T)
+    print('Copying', i+1, 'of', len(T))
     fn = sdss.getPath('photoObj', t.run, t.camcol, t.field, rerun='301')
     outfn = fn.replace(os.environ['BOSS_PHOTOOBJ'], outdir)
-    #print 'output filename', outfn
     if os.path.exists(outfn):
-        print '  Output exists:', outfn
+        print('  Output exists:', outfn)
         try:
-            #tt = fits_table(outfn)
-            #print '  Read', len(tt)
             cmd = 'liststruc ' + outfn
             rtn = os.system(cmd)
-            print 'return', rtn
+            print('return', rtn)
             if rtn == 0:
                 continue
         except:
-            print 'Failed to read existing output file', outfn
-    print '  Reading', fn
+            print('Failed to read existing output file', outfn)
+    print('  Reading', fn)
     S = fits_table(fn, columns=cols)
     od = os.path.dirname(outfn)
     if not os.path.exists(od):
         os.makedirs(od)
     tempfn = outfn + '.tmp'
     if S is None:
-        print '  Input is empty; copying directly'
+        print('  Input is empty; copying directly')
         shutil.copyfile(fn, tempfn)
     else:
         S.writeto(tempfn)
-    # copy filesystem mode, timestamps, etc.
-    #shutil.copymode(fn, tempfn)
-    #shutil.copystat(fn, tempfn)
     os.rename(tempfn, outfn)
-    print '  Wrote', outfn
+    print('  Wrote', outfn)
 
