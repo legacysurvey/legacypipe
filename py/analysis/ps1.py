a52f6f5302955f8390d0f187eb64c0f7c8f28f6b
John Moustakas <john.moustakas@gmail.com>
2015-08-07 12:37:04 -0700

    ps1 class now returns ps1 stars on a given ccd

---
+++ py/analysis/ps1.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+"""
+Find all the PS1 stars in a given DECaLS CCD.
+"""
+
+import os
+import numpy as np
+from astrometry.util.fits import fits_table, merge_tables
+from astrometry.util.util import radecdegtohealpix, healpix_xy_to_ring
+
+class ps1cat():
+    def __init__(self):
+        self.ps1dir = os.getenv('PS1CAT_DIR')
+        self.nside = 32
+
+    def get_cat(self,ra,dec):
+        nstar = len(ra)
+        ipring = np.empty(nstar)
+        for iobj in range(nstar):
+            print(iobj, ra[iobj], dec[iobj])
+            hpxy = radecdegtohealpix(ra[iobj],dec[iobj],self.nside)
+            print(hpxy)
+            print(healpix_xy_to_ring(hpxy,self.nside))
+            ipring[iobj] = healpix_xy_to_ring(hpxy,self.nside)
+        pix = np.unique(ipring).astype(int)
+
+        cat = list()
+        for ipix in pix:
+            fname = os.path.join(self.ps1dir,'ps1-'+'{:05d}'.format(ipix)+'.fits')
+            print(fname)
+            cat.append(fits_table(fname))
+        cat = merge_tables(cat)
+        return cat
+
+    def get_stars(self,ra,dec,ccdwcs):
+        nstar = len(ra)
+        onoffccd = np.empty(nstar)
+        for iobj in range(nstar):
+            onoffccd[iobj] = ccdwcs.inside(ra[iobj],dec[iobj])
+        onccd = np.where((onccd*1)==1)
+        return onccd
+    
+if __name__== '__main__':
+    ps1 = ps1cat()
+    cat = ps1.get_cat([242.3,242.4],[8.6,8.7])
+    
+    
