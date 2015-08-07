fd78865f21b3fea90e4dac3c65ef32c6052aa246
Dustin Lang <dstndstn@gmail.com>
2015-08-07 12:43:02 -0700

    psf QA plots: re-fit local tractor model for Gaussian and pixelized-PsfEx PSF models; not optimizing!

--- py/analysis/psf-residuals.py
+++ py/analysis/psf-residuals.py
@@ -36,10 +36,12 @@ from PIL import Image, ImageDraw
 from astropy.io import fits
 
 from tractor.psfex import PsfEx
+from tractor import Tractor, NanoMaggies, RaDecPos, PointSource
 from astrometry.util.fits import fits_table
 from legacypipe.common import Decals, DecamImage, LegacySurveyImage
 
-flogging.basicConfig(format='%(message)s',level=logging.INFO,stream=sys.stdout)
+#logging.basicConfig(format='%(message)s',level=logging.INFO,stream=sys.stdout)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG, stream=sys.stdout)
 log = logging.getLogger('check-psfs')
 
 def main():
@@ -68,6 +70,18 @@ def main():
     ccd = decals.find_ccds(expnum=396086,ccdname='S31')[0]
     ccd.about()
 
+    print('CCD center:', ccd.ra, ccd.dec)
+
+    # For a given CCD:
+    # - find PS1 stars
+    # - for each PS1 star:
+    #     - create tractor Image object in a teeny patch around this star
+    #       (for only this one CCD)
+    #     - create PointSource object with PS1 RA,Dec,flux
+    #     - tractor.optimize with DR1-style PSF -> model1
+    #     - tractor.optimize with PsfEx PSF     -> model2
+    #     - plot image, model1, model2
+
     band = ccd.filter
     im = DecamImage(decals,ccd)
     print('Band: ', band)
@@ -77,8 +91,125 @@ def main():
     print('img: ', iminfo)
     H,W = iminfo['dims']
 
-    xpos, ypos = (1501.3, 1800.2)
-    
+    #ra,dec = PS1
+    #mag = PS1
+    ra,dec = (106.2861, 27.4828)
+    mag = 18.
+
+    wcs = im.get_wcs()
+    ok, xpos, ypos = wcs.radec2pixelxy(ra, dec)
+    #xpos, ypos = (1501.3, 1800.2)
+    #ra,dec = wcs.pixelxy2radec(xpos, ypos)
+
+    ix,iy = int(xpos), int(ypos)
+    stampsize = 25
+
+    # create little tractor Image object around the star
+    slc = (slice(max(iy-stampsize, 0), min(iy+stampsize+1, H)),
+           slice(max(ix-stampsize, 0), min(ix+stampsize+1, W)))
+    tim = im.get_tractor_image(slc=slc, const2psf=True)
+    # the PSF model 'const2Psf' is the one used in DR1 -- 2-component Gaussian fit
+    # to PsfEx instantiated in the image center.
+
+    # create tractor PointSource from PS1 measurements
+    flux = NanoMaggies.magToNanomaggies(mag)
+    star = PointSource(RaDecPos(ra, dec), NanoMaggies(**{ band: flux }))
+
+    # re-fit the source RA,Dec,flux.
+    tractor = Tractor([tim], [star])
+    # only fit the source
+    tractor.freezeParam('images')
+
+    #alphas = [0.1, 0.3, 1.0]
+    #optargs = dict(priors=False, shared_params=False, alphas=alphas)
+    optargs = {}
+
+    print('PSF model:', tim.psf)
+
+    print('Fitting params:')
+    tractor.printThawedParams()
+
+    for step in range(50):
+        dlnp,X,alpha = tractor.optimize(**optargs)
+        print('dlnp', dlnp)
+        print('X,alpha', X, alpha)
+        if dlnp < 0.1:
+
+            m0 = tractor.getModelImage(0)
+            chi0 = tractor.getChiImage(0)
+            p0 = np.array(tractor.getParams())
+            for i,step in enumerate([1e-3, 1e-2, 1e-1, 1.]):
+                tractor.setParams(p0 + step * np.array(X))
+                print('Trying update:', star)
+                m1 = tractor.getModelImage(0)
+                chi1 = tractor.getChiImage(0)
+                imchi = dict(interpolation='nearest', origin='lower', vmin=-10, vmax=10)
+                plt.clf()
+                plt.subplot(2,2,1)
+                plt.imshow(m0, **tim.ima)
+                plt.subplot(2,2,2)
+                plt.imshow(m1, **tim.ima)
+                plt.subplot(2,2,3)
+                plt.imshow(chi0, **imchi)
+                plt.subplot(2,2,4)
+                plt.imshow(chi1, **imchi)
+                plt.savefig('fail1-%i.png' % i)
+            tractor.setParams(p0)
+
+            break
+    print('Fit:', star)
+
+    mod1 = tractor.getModelImage(0)
+
+    # Now change the PSF model to a pixelized PSF model from PsfEx instantiated
+    # at this place in the image.
+    psfimg = tim.psfex.instantiateAt(xpos, ypos, nativeScale=True)
+    pixpsf = PixelizedPSF(psfimg)
+
+    # use the new PSF model...
+    tim.psf = pixpsf
+
+    print()
+    print('PSF model:', tim.psf)
+    for step in range(50):
+        dlnp,X,alpha = tractor.optimize(**optargs)
+        print('dlnp', dlnp)
+        print('X,alpha', X, alpha)
+        if dlnp < 0.1:
+
+            m0 = tractor.getModelImage(0)
+            p0 = np.array(tractor.getParams())
+            tractor.setParams(p0 + 0.001 * np.array(X))
+            print('Trying update:', star)
+            m1 = tractor.getModelImage(0)
+            plt.clf()
+            plt.subplot(1,2,1)
+            plt.imshow(m0, **tim.ima)
+            plt.subplot(1,2,2)
+            plt.imshow(m1, **tim.ima)
+            plt.savefig('fail2.png')
+            tractor.setParams(p0)
+
+            break
+    print('Fit:', star)
+
+    mod2 = tractor.getModelImage(0)
+
+
+    plt.clf()
+    plt.subplot(1,3,1)
+    plt.imshow(tim.getImage(), **tim.ima)
+    plt.title('Image')
+    plt.subplot(1,3,2)
+    plt.imshow(mod1, **tim.ima)
+    plt.title('2-Gaussian model')
+    plt.subplot(1,3,3)
+    plt.imshow(mod1, **tim.ima)
+    plt.title('PsfEx model')
+    plt.savefig('psf.png')
+
+
+def oldmain():    
     psfex = PsfEx(im.psffn, W, H, ny=13, nx=7, K=2,
                   psfClass=GaussianMixtureEllipsePSF)
     psfim = PsfEx.instantiateAt(psfmog,xpos,ypos)
@@ -99,7 +230,7 @@ def main():
     # Instantiate the PsfEx model at image center
     imh,imw = im.get_image_shape()
     psfexim = psfex.instantiateAt(imw/2,imh/2)
-    print 'PsfEx patch:', psfexim.shape
+    print('PsfEx patch:', psfexim.shape)
 
     ## Instantiate the Gaussian mixture PSF model.  'gpatch' is a Patch object,
     ## gpatch.patch is a numpy image
@@ -116,7 +247,7 @@ def main():
 
     ps1 = fits_table('tmp/ps1cat-2402p062.fits')
     ra, dec = ps1.ra[0], ps1.dec[0]
-    xx, yy = tim.getWcs().positionToPixel(RaDecPos(ra,dec)))
+    xx, yy = tim.getWcs().positionToPixel(RaDecPos(ra,dec))
 
 
 
