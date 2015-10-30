70a26d9fc829a3d7c849d1fdf399d1e55f464f19
Dustin Lang <dstndstn@gmail.com>
2015-10-30 16:00:49 -0400

    write out multiple subsets of COSMOS CCDs that, with noise, will yield approximately the right depth

--- py/legacyanalysis/cosmos-subs.py
+++ py/legacyanalysis/cosmos-subs.py
@@ -20,46 +20,98 @@ print len(C), 'with exptime >= 50 sec'
 C.cut(decals.photometric_ccds(C))
 print len(C), 'photometric'
 
-nil,I = np.unique(C.expnum, return_index=True)
-E = C[I]
-print len(E), 'exposures'
+efn = 'cosmos-exposures.fits'
+if not os.path.exists(efn):
+    nil,I = np.unique(C.expnum, return_index=True)
+    E = C[I]
+    print len(E), 'exposures'
 
-print 'Exptime', E.exptime.min(), E.exptime.max()
+    E.sig1    = np.zeros(len(E), np.float32)
+    E.psfnorm = np.zeros(len(E), np.float32)
+    E.galnorm = np.zeros(len(E), np.float32)
+    for i in range(len(E)):
+        im = decals.get_image_object(E[i])
+        tim = im.get_tractor_image(pixPsf=True, splinesky=True)
+        E.sig1[i] = tim.sig1
+        E.psfnorm[i] = tim.psfnorm
+        E.galnorm[i] = tim.galnorm
+    E.writeto(efn)
 
-# Target depths (90th percentile)
+else:
+    E = fits_table(efn)
+
+# Target depths (90th percentile), for 5-sigma galaxy profile
 target = dict(g=24.0, r=23.4, z=22.5)
 
-E.sig1    = np.zeros(len(E), np.float32)
-E.psfnorm = np.zeros(len(E), np.float32)
-E.galnorm = np.zeros(len(E), np.float32)
+sets = []
+for iset in xrange(100):
+    print '-------------------------------------------'
+    thisset = []
+    addnoise = []
+    for band in bands:
+        gotband = False
 
-for i in range(len(E)):
-    im = decals.get_image_object(E[i])
-    tim = im.get_tractor_image(pixPsf=True, splinesky=True)
-    E.sig1[i] = tim.sig1
-    E.psfnorm[i] = tim.psfnorm
-    E.galnorm[i] = tim.galnorm
+        B = E[E.filter == band]
+        print len(B), 'exposures in', band, 'band'
 
-E.writeto('cosmos-exposures.fits')
-    
+        Nsigma = 5.
+        sig = NanoMaggies.magToNanomaggies(target[band]) / Nsigma
+        targetiv = 1./sig**2
 
-for band in bands:
-    B = E[E.filter == band]
-    print len(B), 'exposures in', band, 'band'
+        maxfrac = 0.34
+        detiv = 0.
+        for i in range(len(B)):
+            exp = B[i]
+            print 'Image', exp.expnum, 'exptime', exp.exptime, 'seeing', exp.seeing
+            thisdetiv = 1. / (exp.sig1 / exp.galnorm)**2
+            print '  detiv', thisdetiv, '= fraction of target: %.2f' % (thisdetiv / targetiv)
 
-    Nsigma = 5.
-    sig = NanoMaggies.magToNanomaggies(target[band]) / Nsigma
-    targetiv = 1./sig**2
+            maxiv = maxfrac * targetiv
+            if thisdetiv > maxiv:
+                #addnoise = 
+                targetsig1 = exp.sig1 * np.sqrt(thisdetiv / maxiv)
+                addsig1 = np.sqrt(max(0, targetsig1**2 - exp.sig1**2))
+                addnoise.append(addsig1)
+                newiv = 1. / (np.hypot(exp.sig1, addsig1) / exp.galnorm)**2
+                #print 'Adding', addsig1, 'we will get detiv', newiv, 'vs', maxiv
+                print '  adding factor %.2f more noise' % (addsig1/exp.sig1)
+                thisdetiv = maxiv
+            else:
+                addnoise.append(0.)
 
-    detiv = 0.
-    for i in range(len(B)):
-        im = decals.get_image_object(B[i])
-        tim = im.get_tractor_image(pixPsf=True, splinesky=True)
-        thisdetiv = 1. / (tim.sig1 / tim.galnorm)**2
-        detiv += thisdetiv
-        print 'this detiv', thisdetiv, 'vs target', targetiv, '(fraction %.2f)' % (thisdetiv / targetiv)
-        if detiv > targetiv:
+            #thisdetiv = min(thisdetiv, maxfrac * targetiv)
+            detiv += thisdetiv
+            thisset.append(exp.expnum)
+            if detiv > targetiv:
+                gotband = True
+                break
+        if not gotband:
             break
+    if not gotband:
+        break
+
+    Eset = E[np.array([expnum in thisset for expnum in E.expnum])]
+    print 'Cut to', len(E), 'exposures in set'
+
+    Eset.addnoise = np.array(addnoise)
+    #sets.append(thisset)
+    #sets.append(Eset)
+
+    expnoise = dict(zip(Eset.expnum, Eset.addnoise))
+    Cset = C[np.array([expnum in thisset for expnum in C.expnum])]
+    Cset.addnoise = np.array([expnoise[e] for e in Cset.expnum]).astype(np.float32)
+    sets.append(Cset)
+    
+    E.cut(np.array([expnum not in thisset for expnum in E.expnum]))
+    print 'Cut to', len(E), 'remaining exposures'
 
 
     
+print 'Got', len(sets), 'sets of exposures'
+
+for i,C in enumerate(sets):
+    C.writeto('cosmos-ccds-sub%i.fits' % i)
+
+#for i,E in enumerate(sets):
+#    E.writeto('cosmos-subset-%i.fits' % i)
+
