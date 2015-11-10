8d6fafed6251ee1643d3adaa0a236cfdf8cc3095
Dustin Lang <dstndstn@gmail.com>
2015-11-10 16:58:42 -0500

    oversampling nn is basically as good as Lanczos

--- py/legacyanalysis/wise-cutouts.py
+++ py/legacyanalysis/wise-cutouts.py
@@ -49,63 +49,29 @@ def wise_cutouts(ra, dec, radius, ps, pixscale=2.75, tractor_base='.'):
     print('DECaLS resampled shape:', dh,dw)
     tags = ['image', 'model', 'resid']
     coimgs = [np.zeros((dh,dw,3), np.uint8) for t in tags]
-    coimgs2 = [np.zeros((dh,dw,3), np.uint8) for t in tags]
 
     for b in B.brickname:
         fn = os.path.join(tractor_base, 'coadd', b[:3], b,
                           'decals-%s-image-r.fits' % b)
         bwcs = Tan(fn)
-        # try:
-        #     Yo,Xo,Yi,Xi,nil = resample_with_wcs(dwcs, bwcs)
-        # except ResampleError:
-        #     continue
-        # if len(Yo) == 0:
-        #     continue
-        # print('Resampling', len(Yo), 'pixels from', b)
-        # for i,tag in enumerate(tags):
-        #     fn = os.path.join(tractor_base, 'coadd', b[:3], b,
-        #                       'decals-%s-%s.jpg' % (b, tag))
-        #     img = plt.imread(fn)
-        #     img = np.flipud(img)
-        #     coimgs[i][Yo,Xo,:] = img[Yi,Xi,:]
-        ims = []
-        rgbims = []
-        for i,tag in enumerate(tags):
-            fn = os.path.join(tractor_base, 'coadd', b[:3], b,
-                              'decals-%s-%s.jpg' % (b, tag))
-            img = plt.imread(fn)
-            img = np.flipud(img)
-            ims.append(img)
-            rgbims.append(img[:,:,0].astype(np.float32))
-            rgbims.append(img[:,:,1].astype(np.float32))
-            rgbims.append(img[:,:,2].astype(np.float32))
-            
         try:
-            Yo,Xo,Yi,Xi,rims = resample_with_wcs(dwcs, bwcs, rgbims)
+            Yo,Xo,Yi,Xi,nil = resample_with_wcs(dwcs, bwcs)
         except ResampleError:
             continue
         if len(Yo) == 0:
             continue
         print('Resampling', len(Yo), 'pixels from', b)
-        for i,img in enumerate(ims):
+        for i,tag in enumerate(tags):
+            fn = os.path.join(tractor_base, 'coadd', b[:3], b,
+                              'decals-%s-%s.jpg' % (b, tag))
+            img = plt.imread(fn)
+            img = np.flipud(img)
             coimgs[i][Yo,Xo,:] = img[Yi,Xi,:]
-
-        k = 0
-        for i in range(len(coimgs2)):
-            for j in range(3):
-                coimgs2[i][Yo,Xo,j] = np.clip(np.round(rims[k]), 0, 255).astype(np.uint8)
-                k += 1
                 
     for img,tag in zip(coimgs, tags):
         plt.clf()
         dimshow(img, ticks=False)
         ps.savefig()
-
-    for img,tag in zip(coimgs2, tags):
-        plt.clf()
-        dimshow(img, ticks=False)
-        ps.savefig()
-        
     
     # Find unWISE tiles overlapping
 
