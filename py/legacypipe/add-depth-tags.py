f8e90a5ad1541a46c2df6b33645beda7ad49892c
Dustin Lang <dstndstn@gmail.com>
2015-12-15 16:43:01 -0800

    check the depth tags

--- py/legacypipe/add-depth-tags.py
+++ py/legacypipe/add-depth-tags.py
@@ -4,9 +4,9 @@ from legacypipe.common import *
 from astrometry.util.fits import fits_table
 from astrometry.util.file import trymakedirs
 
-def add_depth_tag(decals, brick, outdir):
+def add_depth_tag(decals, brick, outdir, overwrite=False):
     outfn = os.path.join(outdir, 'tractor', brick[:3], 'tractor-%s.fits' % brick)
-    if os.path.exists(outfn):
+    if os.path.exists(outfn) and not overwrite:
         print 'Exists:', outfn
         return
     fn = decals.find_file('tractor', brick=brick)
@@ -69,10 +69,54 @@ def bounce_add_depth_tag(X):
     return add_depth_tag(*X)
 
 if __name__ == '__main__':
+    import sys
     outdir = 'tractor2'
     decals = Decals()
     bricks = decals.get_bricks()
     bricks.cut(bricks.dec > -15)
+
+    # Which bricks are missing the depth tags?
+    if True:
+        for brick in bricks.brickname:
+            #fn = 'tractor2/tractor/%s/tractor-%s.fits' % (brick[:3], brick)
+            fn = '/project/projectdirs/desiproc/dr2/tractor+depth/%s/tractor-%s.fits' % (brick[:3], brick)
+            print 'reading', fn
+            if not os.path.exists(fn):
+                continue
+            T = fits_table(fn)
+            print 'Brick', brick, ':', len(T)
+            if len(T) == 0:
+                continue
+            bad = np.flatnonzero((T.decam_depth[:,1] == 0) * (T.decam_nobs[:,1] > 0))
+            pg = (100*len(bad)/len(T))
+            bad = np.flatnonzero((T.decam_depth[:,2] == 0) * (T.decam_nobs[:,2] > 0))
+            pr = (100*len(bad)/len(T))
+            bad = np.flatnonzero((T.decam_depth[:,4] == 0) * (T.decam_nobs[:,4] > 0))
+            pz = (100*len(bad)/len(T))
+
+            bad = np.flatnonzero((T.decam_galdepth[:,1] == 0) * (T.decam_nobs[:,1] > 0))
+            gg = (100*len(bad)/len(T))
+            bad = np.flatnonzero((T.decam_galdepth[:,2] == 0) * (T.decam_nobs[:,2] > 0))
+            gr = (100*len(bad)/len(T))
+            bad = np.flatnonzero((T.decam_galdepth[:,4] == 0) * (T.decam_nobs[:,4] > 0))
+            gz = (100*len(bad)/len(T))
+            if max(pg, pr, pz, gg, gr, gz) < 10:
+                continue
+            print '%3i' % pg, '% bad g ptsrc'
+            print '%3i' % pr, '% bad r ptsrc'
+            print '%3i' % pz, '% bad z ptsrc'
+            print '%3i' % gg, '% bad g gal'
+            print '%3i' % gr, '% bad r gal'
+            print '%3i' % gz, '% bad z gal'
+
+        # -> p202 through p242
+    sys.exit(0)
+
+    bricks.cut((bricks.dec > 20.1) * (bricks.dec < 24.3))
+    print len(bricks), 'bricks to re-run'
+    for brick in bricks.brickname:
+        add_depth_tag(decals, brick, outdir, overwrite=True)
+
 
     if True:
         for brick in bricks.brickname:
