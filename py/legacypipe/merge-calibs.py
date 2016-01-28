9b7c1e54ff2eb9f54901920fd431568b2aabaa57
Dustin Lang <dstndstn@gmail.com>
2016-01-28 12:39:55 -0800

    make all arrays the same shape

--- py/legacypipe/merge-calibs.py
+++ py/legacypipe/merge-calibs.py
@@ -10,7 +10,31 @@ from astrometry.util.fits import fits_table, merge_tables
 from astrometry.util.file import trymakedirs
 from legacypipe.common import Decals
 
-if __name__ == '__main__':
+def pad_arrays(A):
+    '''
+    Given a list of numpy arrays [a0,a1,...], zero-pads them all to be the same shape.
+    '''
+    maxshape = None
+    for a in A:
+        ashape = np.array(a.shape)
+        if maxshape is None:
+            maxshape = ashape
+        else:
+            maxshape = np.maximum(maxshape, ashape)
+
+    padded = []
+    for a in A:
+        ashape = np.array(a.shape)
+        if np.all(ashape == maxshape):
+            padded.append(a)
+            continue
+        p = np.zeros(maxshape, a.dtype)
+        s = list((slice(s) for s in ashape))
+        p[s] = a
+        padded.append(p)
+    return padded
+
+def main():
     decals = Decals()
     ccds = decals.get_ccds()
     print(len(ccds), 'CCDs')
@@ -56,24 +80,47 @@ if __name__ == '__main__':
             if os.path.exists(fn):
                 T = fits_table(fn)
                 hdr = fitsio.read_header(fn, ext=1)
-                for k in ['LOADED', 'ACCEPTED', 'CHI2', 'POLNAXIS', 'POLGRP1',
-                          'POLNAME1', 'POLZERO1', 'POLSCAL1', 'POLGRP2',
-                          'POLNAME2', 'POLZERO2', 'POLSCAL2', 'POLNGRP',
-                          'POLDEG1', 'PSF_FWHM', 'PSF_SAMP', 'PSFNAXIS',
-                          'PSFAXIS1', 'PSFAXIS2', 'PSFAXIS3']:
+
+                keys = ['LOADED', 'ACCEPTED', 'CHI2', 'POLNAXIS', 
+                        'POLNGRP', 'PSF_FWHM', 'PSF_SAMP', 'PSFNAXIS',
+                        'PSFAXIS1', 'PSFAXIS2', 'PSFAXIS3',]
+
+                if hdr['POLNAXIS'] == 0:
+                    # No polynomials.  Fake it.
+                    T.polgrp1 = np.array([0])
+                    T.polgrp2 = np.array([0])
+                    T.polname1 = np.array(['fake'])
+                    T.polname2 = np.array(['fake'])
+                    T.polzero1 = np.array([0])
+                    T.polzero2 = np.array([0])
+                    T.polscal1 = np.array([1])
+                    T.polscal2 = np.array([1])
+                    T.poldeg1 = np.array([0])
+                    T.poldeg2 = np.array([0])
+                else:
+                    keys.extend([
+                            'POLGRP1', 'POLNAME1', 'POLZERO1', 'POLSCAL1',
+                            'POLGRP2', 'POLNAME2', 'POLZERO2', 'POLSCAL2',
+                            'POLDEG1'])
+
+                for k in keys:
                     T.set(k.lower(), np.array([hdr[k]]))
                 psfex.append(T)
                 #print(fn)
                 #T.about()
     
                 hdr = fitsio.read_header(fn)
-                psfhdrvals.append([hdr[k] for k in [
+                psfhdrvals.append([hdr.get(k,'') for k in [
                     'LEGPIPEV', 'PLVER']] + [expnum, ccd.ccdname])
             else:
                 print('File not found:', fn)
 
         if len(psfex):
-            T = merge_tables(psfex)
+            padded = pad_arrays([p.psf_mask[0] for p in psfex])
+            cols = psfex[0].columns()
+            cols.remove('psf_mask')
+            T = merge_tables(psfex, columns=cols)
+            T.psf_mask = np.concatenate([[p] for p in padded])
             T.legpipev = np.array([h[0] for h in psfhdrvals])
             T.plver    = np.array([h[1] for h in psfhdrvals])
             T.expnum   = np.array([h[2] for h in psfhdrvals])
@@ -84,35 +131,23 @@ if __name__ == '__main__':
             print('Wrote', fn)
 
         if len(splinesky):
-            gridh,gridw = 0,0
-            for t in splinesky:
-                h,w = t.gridvals[0].shape
-                gridh = max(gridh, h)
-                gridw = max(gridw, w)
-            print('Max grid size', gridh,gridw)
-            for t in splinesky:
-                h,w = t.gridvals[0].shape
-                t.gridw = np.array([w])
-                t.gridh = np.array([h])
-                if h == gridh and w == gridw:
-                    continue
-                val = t.gridvals[0]
-                gv = np.zeros((gridh,gridw), val.dtype)
-                gv[:h,:w] = val
-                t.gridvals = np.array([gv])
-                print('Resized gridvals from', (h,w), 'to', (gridh,gridw))
-                val = t.ygrid[0]
-                yg = np.zeros(gridh, val.dtype)
-                yg[:h] = val
-                t.ygrid = np.array([yg])
-                val = t.xgrid[0]
-                xg = np.zeros(gridw, val.dtype)
-                xg[:w] = val
-                t.xgrid = np.array([xg])
-                #print('xgrid', t.xgrid[0].shape)
-                #print('ygrid', t.ygrid[0].shape)
+            T = fits_table()
+            T.gridw = np.array([t.gridvals[0].shape[1] for t in splinesky])
+            T.gridh = np.array([t.gridvals[0].shape[0] for t in splinesky])
+
+            padded = pad_arrays([t.gridvals[0] for t in splinesky])
+            T.gridvals = np.concatenate([[p] for p in padded])
+            padded = pad_arrays([t.xgrid[0] for t in splinesky])
+            T.xgrid = np.concatenate([[p] for p in padded])
+            padded = pad_arrays([t.xgrid[0] for t in splinesky])
+            T.ygrid = np.concatenate([[p] for p in padded])
     
-            T = merge_tables(splinesky)
+            cols = splinesky[0].columns()
+            print('Columns:', cols)
+            for c in ['gridvals', 'xgrid', 'ygrid']:
+                cols.remove(c)
+
+            T.add_columns_from(merge_tables(splinesky, columns=cols))
             T.skyclass = np.array([h[0] for h in skyhdrvals])
             T.legpipev = np.array([h[1] for h in skyhdrvals])
             T.plver    = np.array([h[2] for h in skyhdrvals])
@@ -124,3 +159,5 @@ if __name__ == '__main__':
             print('Wrote', fn)
 
         
+if __name__ == '__main__':
+    main()
