06293349c1d5fa1efac307caf934990586021c13
Dustin Lang <dstndstn@gmail.com>
2016-03-06 19:04:32 -0500

    travis: try recovering corrupted astrometry.net git repo

--- py/legacyanalysis/planet9.py
+++ py/legacyanalysis/planet9.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import os
+
 from astrometry.util.fits import fits_table
 from astrometry.util.plotutils import *
 from astrometry.util.starutil_numpy import *
@@ -128,6 +130,8 @@ if __name__ == '__main__':
     overfn = 'overlapping-ccds.fits'
     if os.path.exists(overfn):
         M = fits_table(overfn)
+        M.rename('i','I')
+        M.rename('j','J')
     else:
         from astrometry.libkd.spherematch import match_radec
         radius = np.hypot(2048, 4096) * 0.262/3600.
@@ -161,7 +165,7 @@ if __name__ == '__main__':
 
     # Find images in the same band with overlapping RA,Dec ranges; plot
     # their overlap area & time lag.
-
+    
     for band in ['g','r','z']:
 
         #I = np.flatnonzero((M.filter_1 == band) * (M.filter_2 == band))
@@ -172,12 +176,12 @@ if __name__ == '__main__':
         overlaps = MI.overlap
         dtimes   = MI.dtime
         
-        plt.clf()
-        plt.plot(overlaps, dtimes, 'k.', alpha=0.1)
-        plt.xlabel('Overlap (sq deg)')
-        plt.ylabel('Delta-Times (days)')
-        plt.title('%s band repeat exposures (by CCD)' % band)
-        ps.savefig()
+        # plt.clf()
+        # plt.plot(overlaps, dtimes, 'k.', alpha=0.1)
+        # plt.xlabel('Overlap (sq deg)')
+        # plt.ylabel('Delta-Times (days)')
+        # plt.title('%s band repeat exposures (by CCD)' % band)
+        # ps.savefig()
 
         plt.clf()
         plt.hist(np.log10(dtimes), bins=100, range=(-3, 3),
@@ -190,5 +194,30 @@ if __name__ == '__main__':
                       (np.log10(5./60 * 1./24), '5 min')]:
             plt.axvline(d, color='k', linestyle='--')
             plt.text(d, (yl+yh)/2, txt, rotation=90, va='top')
+        ps.savefig()
+
+        K = np.flatnonzero((MI.dtime > 0.5) * (MI.dtime < 1.5))
+        MI.cut(K)
+        print(len(MI), 'in', band, 'band with delta-time 0.5 - 1.5 days')
+
+        plt.clf()
+        plt.plot(A.ra_center[MI.I], A.dec_center[MI.I], 'k.', alpha=0.1)
+
+        for rlo,rhi in [(240, 300), (0, 150), (45, 105)]:
+            ilo = np.flatnonzero(ra == rlo)[0]
+            ihi = np.flatnonzero(ra == rhi)[0]
+            sty = dict(lw=3, alpha=0.25)
+            plt.plot([rlo,rlo], [declo[ilo],dechi[ilo]], 'k-', **sty)
+            plt.plot([rhi,rhi], [declo[ihi],dechi[ihi]], 'k-', **sty)
+            plt.plot(ra[ilo:ihi+1], declo[ilo:ihi+1],    'k-', **sty)
+            plt.plot(ra[ilo:ihi+1], dechi[ilo:ihi+1],    'k-', **sty)
+        plt.plot(ra, declo, 'k--')
+        plt.plot(ra, dechi, 'k--')
+
+        plt.axis(ax)
+        plt.title('~ 1 day repeats, %s band' % band)
+        plt.xlabel('RA (deg)')
+        plt.ylabel('Dec (deg)')
+
         ps.savefig()
         
