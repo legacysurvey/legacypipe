160e05db1ab6111a12993cb3ae7ab787c1402d21
Dustin Lang <dstndstn@gmail.com>
2016-04-14 15:59:38 -0400

    travis: don't try to run ceres-requiring test

--- py/test/runbrick_test.py
+++ py/test/runbrick_test.py
@@ -1,19 +1,24 @@
 from __future__ import print_function
 import os
+import sys
 from legacypipe.runbrick import main
 
 if __name__ == '__main__':
+
+    travis = 'travis' in sys.argv
+
     surveydir = os.path.join(os.path.dirname(__file__), 'testcase3')
     main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
                '--no-wise', '--force-all', '--no-write',
                '--survey-dir', surveydir,
                '--outdir', 'out-testcase3'])
 
-    # With ceres
-    main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
-               '--no-wise', '--force-all', '--no-write', '--ceres',
-               '--survey-dir', surveydir,
-               '--outdir', 'out-testcase3-ceres'])
+    if not travis:
+        # With ceres
+        main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
+                   '--no-wise', '--force-all', '--no-write', '--ceres',
+                   '--survey-dir', surveydir,
+                   '--outdir', 'out-testcase3-ceres'])
     
 
     
