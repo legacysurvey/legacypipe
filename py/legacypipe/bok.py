755fa80461280abe69b6d48f7f50f94df421bcb0
Kaylan Burleigh <kburleigh@lbl.gov>
2016-05-02 15:01:39 -0700

    set self.dq_saturation_bits = -1 in BokImage since this attribute required by TractorImage class. Currently the processed bok images come as image, invvar image, and SExtractor/PSFex so no dq image provided, dq defaults to numpy arrays of 0s so -1 above will have no affect

--- py/legacypipe/bok.py
+++ py/legacypipe/bok.py
@@ -18,6 +18,7 @@ from tractor.tractortime import TAITime
 
 from legacypipe.ptf import zeropoint_for_ptf
 
+
 '''
 Code specific to images from the 90prime camera on the Bok telescope.
 '''
@@ -39,6 +40,7 @@ class BokImage(LegacySurveyImage):
         self.sefn = os.path.join(calibdir, 'sextractor', self.calname + '.fits')
         self.psffn = os.path.join(calibdir, 'psfex', self.calname + '.fits')
         #self.skyfn = os.path.join(calibdir, 'sky', self.calname + '.fits')
+        self.dq_saturation_bits = -1 #junk b/c currently don't have dq images for bok so read_dq returns array of 0s
         print('in BokImage init, calibdir=%s,self.calname=%s,self.imgfn=%s, self.whtfn=%s, self.sefn=%s, self.psffn=%s' % (calibdir,self.calname,self.imgfn,self.whtfn,self.sefn, self.psffn))
         
     def __str__(self):
