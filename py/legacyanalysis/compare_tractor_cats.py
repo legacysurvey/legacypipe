b89c681531dd8e3c4d00624907afd0b5cab83f76
Kaylan Burleigh <kburleigh@lbl.gov>
2016-05-22 15:24:32 -0700

    multiple tractor cats combined and matched,unmatched histogram of color object plots from wiki auto generated

--- py/legacyanalysis/compare_tractor_cats.py
+++ py/legacyanalysis/compare_tractor_cats.py
@@ -26,13 +26,16 @@ class Matched_Cats():
     def __init__(self,):
         self.data={}
     def initialize(self,data_1,data_2,m1,m2,m1_unm,m2_unm):
+        #self.data['all_1']= targets.data_extract(data_1,range(len(data_1['ra']))) 
         self.data['m_decam']= targets.data_extract(data_1,m1) 
         self.data['m_bokmos']= targets.data_extract(data_2,m2)
         self.data['u_decam']= targets.data_extract(data_1,m1_unm)
         self.data['u_bokmos']= targets.data_extract(data_2,m2_unm)
-    def add_dict(self,camera,new_data):
-        for key in self.data[camera].keys(): 
-            self.data[camera][key]= np.concatenate([self.data[camera][key],new_data[camera][key]])
+    def add_dict(self,match_type,new_data):
+        '''match_type -- m_decam,m_bokmos,u_decam, etc
+        new data -- data returend from read_from..() to be concatenated with existing m_decam, etc'''
+        for key in self.data[match_type].keys(): 
+            self.data[match_type][key]= np.concatenate([self.data[match_type][key],new_data[key]])
 
 def match_it(cat1,cat2):
     '''cat1,2 are tractor catalogue to match objects between'''
@@ -52,7 +55,51 @@ def read_lines(fn):
     fin.close()
     return list(np.char.strip(lines))
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+
+def plot_SN(obj,m_types=['m_decam','m_bokmos'], index='all'): 
+    '''decam,bokmos -- DECaLS() objects with matched OR unmatched indices
+    index -- "all, ptf, lrg" and just those indices will be plotted'''
+    #matching or unmatched objects?
+    if m_types[0].startswith('m_') and m_types[1].startswith('m_'): matched=True
+    elif m_types[0].startswith('u_') and m_types[1].startswith('u_'): matched=False   
+    else: raise ValueError
+    #set seaborn panel styles
+    #sns.set_style('ticks',{"axes.facecolor": ".97"})
+    #sns.set_palette('colorblind')
+    #setup plot
+    laba=dict(fontweight='bold',fontsize='x-large')
+    kwargs_axtext=dict(fontweight='bold',fontsize='x-large',va='top',ha='left')
+    leg_args=dict(loc=1,frameon=True)
+    fig,ax=plt.subplots(1,3,figsize=(9,3)) #,sharey=True)
+    plt.subplots_adjust(wspace=0.5)
+    #plot
+    color=['b','g']
+    for cnt,val in zip(range(3),['rmag','gmag','zmag']):
+        for ith,camera in zip(range(2),m_types):
+            SN= obj[camera].data[val]*np.sqrt(obj[camera].data[val+'_ivar'])
+            if index == 'all': inds= np.arange(obj[camera].data[val].size)
+            elif index == 'psf': inds= obj[camera].data['type'] == 'PSF'
+            elif index == 'lrg': inds= obj[camera].data['i_lrg']
+            else: raise ValueError
+            ax[cnt].scatter(obj[camera].data[val][inds], SN[ inds ], \
+                            edgecolor=color[ith],c='none',lw=2.,label=camera.split('_')[-1])
+        xlab=ax[cnt].set_xlabel(val, **laba)
+        ax[cnt].set_xlim(20,30)
+        ax[cnt].set_ylim(1e0,1e3)
+        ax[cnt].set_yscale('log')
+    ylab=ax[0].set_ylabel(r'$m / \sigma_m$', **laba)
+    ax[2].legend(**leg_args)
+    if matched: sup= '%s, Matched' % index
+    else: sup= '%s, Unmatched' % index
+    sup=plt.suptitle(sup,**laba)
+    #save
+    #sns.despine()
+    if matched: fout='sn_%s.png' % index
+    else: fout='sn_%s_unmatched.png' % index
+    plt.savefig(fout, bbox_extra_artists=[xlab,ylab,sup], bbox_inches='tight',dpi=150)
+    plt.close()
+
+parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                  description='DECaLS simulations.')
 parser.add_argument('-fn1', type=str, help='process this brick (required input)')
 parser.add_argument('-fn2', type=str, help='object type (STAR, ELG, LRG, BGS)') 
@@ -70,12 +117,18 @@ for cnt,cat1,cat2 in zip(range(len(fns_1)),fns_1,fns_2):
     if cnt == 0:
         a.initialize(data_1,data_2,m1,m2,m1_unm,m2_unm)
     else:  
+        #a.add_dict('all_1', targets.data_extract(data_1,range(len(data_1['ra'])) ))
         a.add_dict('m_decam', targets.data_extract(data_1,m1) )
         a.add_dict('m_bokmos', targets.data_extract(data_2,m2))
         a.add_dict('u_decam', targets.data_extract(data_1,m1_unm))
         a.add_dict('u_bokmos', targets.data_extract(data_2,m2_unm))
 #each key a.data[key] becomes DECaLS() object with grz mags,i_lrg, etc
-
-#unm['decam']= targets.DECaLS(decam, w1=True)
-#unm['bokmos']= targets.DECaLS(bokmos, w1=True)
+b={}
+for match_type in a.data.keys(): b[match_type]= targets.DECaLS(a.data[match_type], w1=True)
+#plots
+plot_SN(b,m_types=['m_decam','m_bokmos'], index='all')
+plot_SN(b,m_types=['m_decam','m_bokmos'], index='psf')
+plot_SN(b,m_types=['m_decam','m_bokmos'], index='lrg')
+plot_SN(b,m_types=['u_decam','u_bokmos'], index='psf')
+plot_SN(b,m_types=['u_decam','u_bokmos'], index='lrg')
 
