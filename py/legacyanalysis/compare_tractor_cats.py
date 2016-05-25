21cc72620c0c8367c0550e5b6b6a7d272125e06d
Kaylan Burleigh <kburleigh@lbl.gov>
2016-05-25 12:26:50 -0700

    made bin_up() function to compute q25,50,75 percentiles and bin centers for a given quantity like S/N using something like gmag to bin by, updated plot_SN() to plot flux/sigma instead of mag/sigma

--- py/legacyanalysis/compare_tractor_cats.py
+++ py/legacyanalysis/compare_tractor_cats.py
@@ -87,47 +87,6 @@ def plot_radec(obj,m_types=['u_decam','u_bokmos']):
     plt.close()
 
 
-
-def plot_SN(obj,m_types=['m_decam','m_bokmos'], index='all'): 
-    '''decam,bokmos -- DECaLS() objects with matched OR unmatched indices
-    index -- "all, ptf, lrg" and just those indices will be plotted'''
-    #matching or unmatched objects?
-    if m_types[0].startswith('m_') and m_types[1].startswith('m_'): matched=True
-    elif m_types[0].startswith('u_') and m_types[1].startswith('u_'): matched=False   
-    else: raise ValueError
-    #set seaborn panel styles
-    #sns.set_style('ticks',{"axes.facecolor": ".97"})
-    #sns.set_palette('colorblind')
-    #setup plot
-    fig,ax=plt.subplots(1,3,figsize=(9,3)) #,sharey=True)
-    plt.subplots_adjust(wspace=0.5)
-    #plot
-    color=['b','g']
-    for cnt,val in zip(range(3),['rmag','gmag','zmag']):
-        for ith,camera in zip(range(2),m_types):
-            SN= obj[camera].data[val]*np.sqrt(obj[camera].data[val+'_ivar'])
-            if index == 'all': inds= np.arange(obj[camera].data[val].size)
-            elif index == 'psf': inds= obj[camera].data['type'] == 'PSF'
-            elif index == 'lrg': inds= obj[camera].data['i_lrg']
-            else: raise ValueError
-            ax[cnt].scatter(obj[camera].data[val][inds], SN[ inds ], \
-                            edgecolor=color[ith],c='none',lw=2.,label=camera.split('_')[-1])
-        xlab=ax[cnt].set_xlabel(val, **laba)
-        ax[cnt].set_xlim(20,30)
-        ax[cnt].set_ylim(1e0,1e3)
-        ax[cnt].set_yscale('log')
-    ylab=ax[0].set_ylabel(r'$m / \sigma_m$', **laba)
-    ax[2].legend(loc=4,**leg_args)
-    if matched: sup= '%s, Matched' % index
-    else: sup= '%s, Unmatched' % index
-    sup=plt.suptitle(sup,**laba)
-    #save
-    #sns.despine()
-    if matched: fout='sn_%s.png' % index
-    else: fout='sn_%s_unmatched.png' % index
-    plt.savefig(fout, bbox_extra_artists=[xlab,ylab,sup], bbox_inches='tight',dpi=150)
-    plt.close()
-
 def plot_HistTypes(obj,m_types=['m_decam','m_bokmos']):
     '''decam,bokmos -- DECaLS() objects with matched OR unmatched indices'''
     #matched or unmatched objects
@@ -190,40 +149,68 @@ def plot_matched_color_color(decam,bokmos, zoom=False):
     plt.savefig(name, bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
     plt.close()
 
-def plot_matched_color_diff_binned(decam,bokmos, zoom=False):
-    '''decam,bokmos are DECaLS() objects matched to decam ra,dec'''
-    #divide into samples of 0.25 mag bins, store q50 of each
-    width=0.25 #in mag
-    low_vals= np.arange(20.,26.,width)
-    med={}
-    for b in ['g','r','z']: med[b]=np.zeros(low_vals.size)-100
+def bin_up(data_bin_by,data_percentile,bL=20., bH=26.,bW=0.25):
+    '''finds indices for 0.25 bins, returns bin centers and q25,50,75 percentiles of data_percentile in each bin
+    bL,bH -- min and max values of data_bin to consider
+    bW -- bin width'''
+    low_vals= np.arange(bL,bH,bW)
+    q25= np.zeros(low_vals.size)+np.nan
+    q50,q75= q25.copy(),q25.copy()
     for i,low in enumerate(low_vals):
+        ind= np.all((low <= data_bin_by,data_bin_by < low+bW),axis=0)
+        if np.where(ind)[0].size > 0:
+            q25[i]= np.percentile(data_percentile[ind],q=25)
+            q50[i]= np.percentile(data_percentile[ind],q=50)
+            q75[i]= np.percentile(data_percentile[ind],q=75)
+        else: 
+            pass #given qs nan, which they already have
+    return low_vals+bW/2,q25,q50,q75
+
+
+def plot_SN(obj, found_by='matched',type='all'):
+    '''obj['m_decam'] is DECaLS() object
+    found_by -- 'matched' or 'unmatched' 
+    type -- all,psf,lrg'''
+    #indices for type == all,psf, or lrg
+    assert(found_by == 'matched' or found_by == 'unmatched')
+    prefix= found_by[0]+'_' # m_ or u_
+    index={}
+    for key in ['decam','bokmos']:
+        if type == 'all': 
+            index[key]= np.arange(obj[prefix+key].data['gflux'].size)
+        elif index == 'psf': 
+            index[key]= obj[prefix+key].data['type'] == 'PSF'
+        elif index == 'lrg': 
+            index[key]= obj[prefix+key].data['i_lrg']
+        else: raise ValueError
+    #bin up SN values
+    bin_SN=dict(decam={},bokmos={})
+    for key in binned.keys():
         for band in ['g','r','z']:
-            ind= np.all((low <= decam[band+'mag'],decam[band+'mag'] < low+width),axis=0)
-            if np.where(ind)[0].size > 0:
-                med[band][i]= np.percentile(bokmos[band+'mag'][ind] - decam[band+'mag'][ind],q=50)
-            else: 
-                med[band][i]= np.nan
-    #make plot
-    #set seaborn panel styles
-    #sns.set_style('ticks',{"axes.facecolor": ".97"})
-    #sns.set_palette('colorblind')
+            bin_SN[key][band]={}
+            i= index[key]
+            bin_SN[key][band]['binc'],bin_SN[key][band]['q25'],bin_SN[key][band]['q50'],bin_SN[key][band]['q75']=\
+                    bin_up(obj['m_'+key].data[band+'mag'][i], obj['m_'+key].data[band+'flux'][i]*np.sqrt(obj['m_'+key].data[band+'flux_ivar'][i]))
     #setup plot
-    fig,ax=plt.subplots(1,3,figsize=(9,3)) #,sharey=True)
-    plt.subplots_adjust(wspace=0.5)
-    #plot
-    for cnt,band in zip(range(3),['r','g','z']):
-        ax[cnt].scatter(low_vals, med[band],\
-                       edgecolor='b',c='none',lw=2.) #,label=m_type.split('_')[-1])
-        xlab=ax[cnt].set_xlabel('bins of %s (decam)' % band, **laba)
-        ylab=ax[cnt].set_ylabel('q50[%s bokmos - decam]' % band, **laba)
-        if zoom: ax[cnt].set_ylim(-0.25,0.25)
-    # sup=plt.suptitle('decam with matching bokmos',**laba)
-    #save
-    #sns.despine()
-    if zoom: name="median_color_diff_zoom.png"
-    else: name="median_color_diff.png"
-    plt.savefig(name, bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
+    fig,ax=plt.subplots(1,3,figsize=(9,3),sharey=True)
+    plt.subplots_adjust(wspace=0)
+    #plot SN
+    for cnt,band in zip(range(3),['g','r','z']):
+        #horiz line at SN = 5
+        ax[cnt].plot([1,40],[5,5],'k--',lw=2,label='S/N = 5')
+        #data
+        for inst,color in zip(['decam','bokmos'],['b','g']):
+            ax[cnt].plot(bin_SN[inst][band]['binc'], bin_SN[inst][band]['q50'],c=color,ls='-',lw=2,label=inst)
+            ax[cnt].fill_between(bin_SN[inst][band]['binc'],bin_SN[inst][band]['q25'],bin_SN[inst][band]['q75'],color=color,alpha=0.25)
+    #labels
+    ax[2].legend(loc=1,**leg_args)
+    for cnt,band in zip(range(3),['g','r','z']):
+    for cnt in range(3):
+        xlab=ax[cnt].set_xlabel('%s' % band, **laba)
+        ax[cnt].set_ylim(1,100)
+        ax[cnt].set_xlim(20.,26.)
+    ylab=ax[0].set_ylabel(r'S/N = f / $\sigma_f$', **laba)
+    plt.savefig('sn_%s_%s.png' % (found_by,type), bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
     plt.close()
 
 def plot_matched_separation_hist(d12, zoom=False):
@@ -282,21 +269,62 @@ def plot_PSF_color(obj):
                 med[val][i]= np.nan
     #plot
     ti=ax[0,0].set_title('Decam PSF')
-    ti=ax[1,0].set_title('Bokmos PSF')
+    ti=ax[0,1].set_title('Bokmos PSF')
     ylab=ax[0,0].set_ylabel('Median g-r')
-    ylab=ax[0,1].set_ylabel('RMS g-r')
+    ylab=ax[1,0].set_ylabel('RMS g-r')
     xlab=ax[1,0].set_xlabel('g binned 0.25')
     xlab=ax[1,1].set_xlabel('g binned 0.25')
-    #add lines
-    ax[0,0].scatter(low_vals,med['m_decam'],edgecolor='b',c='none',lw=2.,label='matched')
-    ax[0,1].scatter(low_vals,rms['m_decam'],edgecolor='b',c='none',lw=2.,label='matched')
+    #decam
+    ax[0,0].scatter(low_vals,med['m_decam'],edgecolor='b',c='none',lw=2.,label='Matched')
     ax[0,0].scatter(low_vals,med['u_decam'],edgecolor='g',c='none',lw=2.,label='Unmatched')
-    ax[0,1].scatter(low_vals,rms['u_decam'],edgecolor='g',c='none',lw=2.,label='Unmatched')
+    ax[1,0].scatter(low_vals,rms['m_decam'],edgecolor='b',c='none',lw=2.)
+    ax[1,0].scatter(low_vals,rms['u_decam'],edgecolor='g',c='none',lw=2.)
+    #bokmos
+    ax[0,0].scatter(low_vals,med['m_bokmos'],edgecolor='b',c='none',lw=2.)
+    ax[0,0].scatter(low_vals,med['u_bokmos'],edgecolor='g',c='none',lw=2.)
+    ax[1,0].scatter(low_vals,rms['m_bokmos'],edgecolor='b',c='none',lw=2.)
+    ax[1,0].scatter(low_vals,rms['u_bokmos'],edgecolor='g',c='none',lw=2.)
     #finish
-    ax[0,0].legend(loc=0,**leg_args)
+    ax[0,0].legend(loc=3,**leg_args)
     #save
     #sns.despine()
     plt.savefig('psf_color_binned.png', bbox_extra_artists=[ti,xlab,ylab], bbox_inches='tight',dpi=150)
+    plt.close()
+
+def plot_psf_hists(decam,bokmos, zoom=False):
+    '''decam,bokmos are DECaLS() objects matched to decam ra,dec'''
+    #divide into samples of 0.25 mag bins, store q50 of each
+    width=0.25 #in mag
+    low_vals= np.arange(20.,26.,width)
+    med={}
+    for b in ['g','r','z']: med[b]=np.zeros(low_vals.size)-100
+    for i,low in enumerate(low_vals):
+        for band in ['g','r','z']:
+            ind= np.all((low <= decam[band+'mag'],decam[band+'mag'] < low+width),axis=0)
+            if np.where(ind)[0].size > 0:
+                med[band][i]= np.percentile(bokmos[band+'mag'][ind] - decam[band+'mag'][ind],q=50)
+            else: 
+                med[band][i]= np.nan
+    #make plot
+    #set seaborn panel styles
+    #sns.set_style('ticks',{"axes.facecolor": ".97"})
+    #sns.set_palette('colorblind')
+    #setup plot
+    fig,ax=plt.subplots(1,3,figsize=(9,3)) #,sharey=True)
+    plt.subplots_adjust(wspace=0.5)
+    #plot
+    for cnt,band in zip(range(3),['r','g','z']):
+        ax[cnt].scatter(low_vals, med[band],\
+                       edgecolor='b',c='none',lw=2.) #,label=m_type.split('_')[-1])
+        xlab=ax[cnt].set_xlabel('bins of %s (decam)' % band, **laba)
+        ylab=ax[cnt].set_ylabel('q50[%s bokmos - decam]' % band, **laba)
+        if zoom: ax[cnt].set_ylim(-0.25,0.25)
+    # sup=plt.suptitle('decam with matching bokmos',**laba)
+    #save
+    #sns.despine()
+    if zoom: name="median_color_diff_zoom.png"
+    else: name="median_color_diff.png"
+    plt.savefig(name, bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
     plt.close()
 
 
@@ -337,6 +365,13 @@ b['m_bokmos'].propogate_new_mask(mask)
 #plots
 plot_radec(b,m_types=['u_decam','u_bokmos'])
 
+plot_SN(b, found_by='matched',type='all')
+#plot_SN(b, found_by='matched',type='psf')
+#plot_SN(b, found_by='unmatched',type='all')
+print('exit')
+sys.exit()
+
+
 plot_matched_color_diff_binned(b['m_decam'].data,b['m_bokmos'].data)
 plot_matched_color_diff_binned(b['m_decam'].data,b['m_bokmos'].data,zoom=True)
 
@@ -346,12 +381,6 @@ plot_PSF_color(b)
 
 print('exiting early')
 sys.exit()
-plot_SN(b,m_types=['m_decam','m_bokmos'], index='all')
-plot_SN(b,m_types=['m_decam','m_bokmos'], index='psf')
-plot_SN(b,m_types=['m_decam','m_bokmos'], index='lrg')
-plot_SN(b,m_types=['u_decam','u_bokmos'], index='psf')
-plot_SN(b,m_types=['u_decam','u_bokmos'], index='lrg')
-
 plot_HistTypes(b,m_types=['m_decam','m_bokmos'])
 plot_HistTypes(b,m_types=['u_decam','u_bokmos'])
 
