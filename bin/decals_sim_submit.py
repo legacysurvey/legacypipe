15082d75a9c38b3124317397470f229b8bda4e53
Kaylan Burleigh <kburleigh@lbl.gov>
2016-06-16 08:33:26 -0700

    now generates batch scripts for 50+ chunks of a given brick

--- bin/decals_sim_submit.py
+++ bin/decals_sim_submit.py
@@ -17,20 +17,25 @@ def ap(cmd):
 
 parser = argparse.ArgumentParser(description="test")
 parser.add_argument("-machine",choices=['cori','edison'],action="store",help='brick to process',required=True)
+parser.add_argument("-istart",type=int,action="store",help='startin chunk',required=True)
+parser.add_argument("-istop",type=int,action="store",help='last chunk',required=True)
+parser.add_argument("-q",choices=['debug','regular'],action="store",help='last chunk',required=True)
 args = parser.parse_args()
 
-bricks=['2523p355','2616p277','1306p247','1931p197','2508p167','2414p142','2493p097','2430p047','3293m002','0364m052']
-for brick in bricks: 
+#bricks=['2523p355','2616p277','1306p247','1931p197','2508p167','2414p142','2493p097','2430p047','3293m002','0364m052']
+#for brick in bricks: 
+brick='2523p355'
+for chunk in range(args.istart,args.istop+1): 
     #bash script
-    fn='submit_%s.sh' % brick
+    fn='submit_%s_chunk_%d.sh' % (brick,chunk)
     if os.path.exists(fn): bash('rm %s' % fn)
     fin=open(fn,'w')
 
     #header
     ap('#!/bin/bash -l\n')
-    ap('#SBATCH -p debug') 
+    ap('#SBATCH -p %s' % args.q) 
     ap('#SBATCH -N 1')  
-    ap('#SBATCH -t 00:30:00')
+    ap('#SBATCH -t 00:20:00')
     ap('#SBATCH -J %s' % brick) 
     pref='#SBATCH -o %s' % brick    
     ap(pref+'.o%j')
@@ -41,8 +46,12 @@ for brick in bricks:
     if args.machine == 'cori': ap('export OMP_NUM_THREADS=32') 
     else: ap('export OMP_NUM_THREADS=24') 
     ap('cd $SLURM_SUBMIT_DIR')
-    ap('export DECALS_SIM_DIR=${SLURM_SUBMIT_DIR}/ten_bricks')
-    ap('srun -n 1 -c ${OMP_NUM_THREADS} python legacyanalysis/decals_sim.py --brick %s -n 500 -o STAR --threads ${OMP_NUM_THREADS}' % brick)
+    #ap('export DECALS_SIM_DIR=${SLURM_SUBMIT_DIR}/ten_bricks')
+    ap('export DECALS_SIM_DIR=/project/projectdirs/desi/users/burleigh/decals_sim/durham')
+    ap('export LEGACY_SURVEY_DIR=/scratch1/scratchdirs/kaylanb/desi/dr3_brick_2523p355')
+    ap('export DUST_DIR=/scratch1/scratchdirs/kaylanb/desi/dr3_brick_2523p355/dust/v0_0')
+    ap('srun -n 1 -c ${OMP_NUM_THREADS} python legacyanalysis/decals_sim.py --brick %s -n 500 -o STAR -ic %d --threads ${OMP_NUM_THREADS}' % (brick,chunk))
     ap('echo DONE')
     fin.close()
+    #ap('sbatch %s' % fn) 
 
