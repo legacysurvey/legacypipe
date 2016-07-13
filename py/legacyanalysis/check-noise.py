6e6eef61ea59f65c141d4fc02bfe18727f625abf
Dustin Lang <dstndstn@gmail.com>
2016-07-13 10:17:19 -0700

    look at MAD error estimates for a flat image

--- py/legacyanalysis/check-noise.py
+++ py/legacyanalysis/check-noise.py
@@ -6,11 +6,211 @@ import pylab as plt
 import fitsio
 from astrometry.util.fits import fits_table, merge_tables
 from astrometry.util.plotutils import PlotSequence, plothist, loghist
+from astrometry.util.ttime import Time
 from legacypipe.common import LegacySurveyData, imsave_jpeg, get_rgb
 
 '''
 A script to examine the pixel distributions vs the CP weight maps.
 '''
+
+
+def plot_correlations(img, ie, sig1, medians=True):
+    corrs = []
+    corrs_x = []
+    corrs_y = []
+
+    mads = []
+    mads_x = []
+    mads_y = []
+
+    rcorrs = []
+    rcorrs_x = []
+    rcorrs_y = []
+    dists = np.arange(1, 51)
+    for dist in dists:
+        offset = dist
+        slice1 = (slice(0,-offset,1),slice(0,-offset,1))
+        slice2 = (slice(offset,None,1),slice(offset,None,1))
+
+        slicex = (slice1[0], slice2[1])
+        slicey = (slice2[0], slice1[1])
+
+        corr = img[slice1] * img[slice2]
+        corr = corr[(ie[slice1] > 0) * (ie[slice2] > 0)]
+
+        diff = img[slice1] - img[slice2]
+        diff = diff[(ie[slice1] > 0) * (ie[slice2] > 0)]
+        sig1 = 1.4826 / np.sqrt(2.) * np.median(np.abs(diff).ravel())
+        mads.append(sig1)
+
+        #print('Dist', dist, '; number of corr pixels', len(corr))
+        #t0 = Time()
+        if medians:
+            rcorr = np.median(corr) / sig1**2
+            rcorrs.append(rcorr)
+        #t1 = Time()
+        corr = np.mean(corr) / sig1**2
+        #t2 = Time()
+        #print('median:', t1-t0)
+        #print('mean  :', t2-t1)
+        corrs.append(corr)
+
+        corr = img[slice1] * img[slicex]
+        corr = corr[(ie[slice1] > 0) * (ie[slicex] > 0)]
+
+        diff = img[slice1] - img[slicex]
+        diff = diff[(ie[slice1] > 0) * (ie[slicex] > 0)]
+        sig1 = 1.4826 / np.sqrt(2.) * np.median(np.abs(diff).ravel())
+        mads_x.append(sig1)
+
+        if medians:
+            rcorr = np.median(corr) / sig1**2
+            rcorrs_x.append(rcorr)
+        corr = np.mean(corr) / sig1**2
+        corrs_x.append(corr)
+
+        corr = img[slice1] * img[slicey]
+        corr = corr[(ie[slice1] > 0) * (ie[slicey] > 0)]
+
+        diff = img[slice1] - img[slicey]
+        diff = diff[(ie[slice1] > 0) * (ie[slicey] > 0)]
+        #Nmad = len(diff)
+        sig1 = 1.4826 / np.sqrt(2.) * np.median(np.abs(diff).ravel())
+        mads_y.append(sig1)
+
+        if medians:
+            rcorr = np.median(corr) / sig1**2
+            rcorrs_y.append(rcorr)
+        corr = np.mean(corr) / sig1**2
+        corrs_y.append(corr)
+
+        #print('Dist', dist, '-> corr', corr, 'X,Y', corrs_x[-1], corrs_y[-1],
+        #      'robust', rcorrs[-1], 'X,Y', rcorrs_x[-1], rcorrs_y[-1])
+
+    pix = img[ie > 0].ravel()
+    Nmad = len(pix) / 2
+    P = np.random.permutation(len(pix))[:(Nmad*2)]
+    diff = pix[P[:Nmad]] - pix[P[Nmad:]]
+    mad_random = 1.4826 / np.sqrt(2.) * np.median(np.abs(diff))
+
+    plt.clf()
+    p1 = plt.plot(dists, corrs, 'b.-')
+    p2 = plt.plot(dists, corrs_x, 'r.-')
+    p3 = plt.plot(dists, corrs_y, 'g.-')
+    if medians:
+        p4 = plt.plot(dists, rcorrs, 'b.--')
+        p5 = plt.plot(dists, rcorrs_x, 'r.--')
+        p6 = plt.plot(dists, rcorrs_y, 'g.--')
+    plt.xlabel('Pixel offset')
+    plt.ylabel('Correlation')
+    plt.axhline(0, color='k', alpha=0.3)
+    plt.legend([p1[0],p2[0],p3[0]], ['Diagonal', 'X', 'Y'], loc='upper right')
+
+    return (dists, corrs, corrs_x, corrs_y, rcorrs, rcorrs_x, rcorrs_y,
+            mads, mads_x, mads_y, mad_random)
+
+ps = PlotSequence('noise')
+
+# fitsgetext -i /global/homes/d/dstn/cosmo/staging/decam/DECam_Raw/20160107/c4d_160107_213046_fri.fits.fz -e 0 -e 1 -o flat.fits
+
+# img = fitsio.read('flat.fits').astype(np.float32)
+# # trim the edges
+# img = img[100:-100, 100:-100]
+# img -= np.median(img)
+
+from obsbot.measure_raw import DECamMeasurer
+from obsbot.decam import DecamNominalCalibration
+
+nom = DecamNominalCalibration()
+fn = 'flat.fits'
+ext = 1
+meas = DECamMeasurer(fn, ext, nom)
+F = fitsio.FITS(fn)
+img,hdr = meas.read_raw(F, ext)
+
+rawimg = F[ext].read()
+plo,phi = np.percentile(rawimg.ravel(), [16,84])
+plt.clf()
+plt.imshow(rawimg, interpolation='nearest', origin='lower', vmin=plo, vmax=phi)
+plt.title('Flat image (raw)')
+plt.colorbar()
+ps.savefig()
+
+plo,phi = np.percentile(img.ravel(), [16,84])
+plt.clf()
+plt.imshow(img, interpolation='nearest', origin='lower', vmin=plo, vmax=phi)
+plt.title('Flat image')
+plt.colorbar()
+ps.savefig()
+
+img,trim_x0,trim_y0 = meas.trim_edges(img)
+sky,sig1 = meas.get_sky_and_sigma(img)
+img -= sky
+meas.remove_sky_gradients(img)
+
+plt.clf()
+plt.imshow(img, interpolation='nearest', origin='lower', vmin=-2.*sig1, vmax=2.*sig1)
+plt.title('Flat image (gradient removed)')
+plt.colorbar()
+ps.savefig()
+
+plt.clf()
+plt.imshow(img, interpolation='nearest', origin='lower', vmin=-4.*sig1, vmax=4.*sig1)
+plt.title('Flat image (gradient removed)')
+plt.colorbar()
+ps.savefig()
+
+plt.clf()
+n,b,p = plt.hist(img.ravel() / sig1, range=(-5,5), bins=100, histtype='step', color='b')
+binc = (b[1:] + b[:-1])/2.
+yy = np.exp(-0.5 * binc**2)
+plt.plot(binc, yy / yy.sum() * np.sum(n), 'k-', alpha=0.5, lw=2)
+plt.xlim(-5,5)
+plt.xlabel('Pixel values / sigma')
+plt.title('Flat image (gradient removed)')
+ps.savefig()
+
+# #binned = (img[0::4, :] + img[1::4, :] + img[2::4, :] + img[3::4, :]) / 4.
+# plt.clf()
+# #plt.plot(np.median(binned, axis=1), 'b-')
+# plt.plot([np.median(img[i*10:(i+1)*10, :]) for i in range(400)], 'b.')
+# plt.ylabel('Row median')
+# ps.savefig()
+
+fitsio.write('flat-grad.fits', img, clobber=True)
+
+X = plot_correlations(img, np.ones_like(img), sig1, medians=False)
+plt.title('Flat image')
+ps.savefig()
+(dists, corrs, corrs_x, corrs_y, rcorrs, rcorrs_x, rcorrs_y,
+ mads, mads_x, mads_y, mad_random) = X
+
+# plt.clf()
+# p4 = plt.plot(dists, rcorrs, 'b.--')
+# p5 = plt.plot(dists, rcorrs_x, 'r.--')
+# p6 = plt.plot(dists, rcorrs_y, 'g.--')
+# plt.xlabel('Pixel offset')
+# plt.ylabel('Correlation')
+# plt.axhline(0, color='k', alpha=0.3)
+# plt.legend([p4[0],p5[0],p6[0]], ['Diagonal (med)', 'X (med)', 'Y (med)'],
+#            loc='upper right')
+# plt.title('Flat image')
+# ps.savefig()
+
+plt.clf()
+p4 = plt.plot(dists, mads, 'b.-')
+p5 = plt.plot(dists, mads_x, 'r.-')
+p6 = plt.plot(dists, mads_y, 'g.-')
+plt.xlabel('Pixel offset')
+plt.ylabel('MAD error estimate')
+#plt.axhline(0, color='k', alpha=0.3)
+p7 = plt.axhline(mad_random, color='k', alpha=0.3)
+plt.legend([p4[0],p5[0],p6[0], p7], ['Diagonal', 'X', 'Y', 'Random'],
+           loc='lower right')
+plt.title('Flat image')
+ps.savefig()
+
+sys.exit(0)
 
 
 survey = LegacySurveyData()
@@ -33,8 +233,6 @@ print(len(I2), 'non-oki images')
 ccds.cut(np.hstack(zip(I1[np.random.permutation(len(I1))],
                        I2[np.random.permutation(len(I2))])))
 #ccds.cut(np.random.permutation(len(ccds)))
-
-ps = PlotSequence('noise')
 
 for ccd in ccds[:1]:
     im = survey.get_image_object(ccd)
@@ -109,61 +307,10 @@ for ccd in ccds[:2]:
     plt.title(tim.name)
     ps.savefig()
 
-    dists = np.arange(1, 51)
-    corrs = []
-    corrs_x = []
-    corrs_y = []
-
-    rcorrs = []
-    rcorrs_x = []
-    rcorrs_y = []
-
-
-    for dist in dists:
-        offset = dist
-        slice1 = (slice(0,-offset,1),slice(0,-offset,1))
-        slice2 = (slice(offset,None,1),slice(offset,None,1))
-
-        slicex = (slice1[0], slice2[1])
-        slicey = (slice2[0], slice1[1])
-
-        corr = img[slice1] * img[slice2]
-        corr = corr[(ie[slice1] > 0) * (ie[slice2] > 0)]
-        print('Dist', dist, '; number of corr pixels', len(corr))
-        rcorr = np.median(corr) / tim.sig1**2
-        corr = np.mean(corr) / tim.sig1**2
-        print('-> corr', corr)
-        corrs.append(corr)
-        rcorrs.append(rcorr)
-
-        corr = img[slice1] * img[slicex]
-        corr = corr[(ie[slice1] > 0) * (ie[slicex] > 0)]
-        rcorr = np.median(corr) / tim.sig1**2
-        corr = np.mean(corr) / tim.sig1**2
-        corrs_x.append(corr)
-        rcorrs_x.append(rcorr)
-
-        corr = img[slice1] * img[slicey]
-        corr = corr[(ie[slice1] > 0) * (ie[slicey] > 0)]
-        rcorr = np.median(corr) / tim.sig1**2
-        corr = np.mean(corr) / tim.sig1**2
-        corrs_y.append(corr)
-        rcorrs_y.append(rcorr)
-
-    plt.clf()
-    p1 = plt.plot(dists, corrs, 'b.-')
-    p2 = plt.plot(dists, corrs_x, 'r.-')
-    p3 = plt.plot(dists, corrs_y, 'g.-')
-    p4 = plt.plot(dists, rcorrs, 'b.--')
-    p5 = plt.plot(dists, rcorrs_x, 'r.--')
-    p6 = plt.plot(dists, rcorrs_y, 'g.--')
-    plt.xlabel('Pixel offset')
-    plt.ylabel('Correlation')
-    plt.legend([p1[0],p2[0],p3[0]], ['Diagonal', 'X', 'Y'], loc='upper right')
+    plot_correlations(img, ie)
     plt.title(tim.name + ' ' + '/'.join(im.imgfn.split('/')[-2:]))
     #plt.ylim(-0.005, 0.02)
     plt.ylim(-0.001, 0.011)
-    plt.axhline(0, color='k', alpha=0.3)
     ps.savefig()
 
 
