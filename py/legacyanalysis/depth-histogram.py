01415dc09524390398a97829e2d3337f4f0973a9
Dustin Lang <dstndstn@gmail.com>
2016-08-29 11:22:13 -0400

    generate depth summary tables

--- py/legacyanalysis/depth-histogram.py
+++ py/legacyanalysis/depth-histogram.py
@@ -1,15 +1,72 @@
-from astrometry.util.fits import *
+from __future__ import print_function
 from glob import glob
 import os
 import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
 import numpy as np
-from astrometry.util.plotutils import *
+from astrometry.util.fits import fits_table, merge_tables
+from astrometry.util.plotutils import PlotSequence
 
-fn = 'dr2-depth.fits'
-if os.path.exists(fn):
+def brickname_from_filename(fn):
+    fn = os.path.basename(fn)
+    words = fn.split('-')
+    assert(len(words) == 3)
+    return words[1]
+
+if __name__ == '__main__':
+    outfn = 'dr3-depth.fits'
+    summaryfn = 'dr3-depth-summary.fits'
+
+    fns = glob('coadd/*/*/*-depth.fits')
+    fns.sort()
+    print(len(fns), 'depth files')
+    
+    fn = fns.pop(0)
+    print('Reading', fn)
+    # We'll keep all files for merging...
+    TT = []
+
     T = fits_table(fn)
+    # Create / upgrade the count columns to int64.
+    for band in 'grz':
+        for pro in ['ptsrc', 'gal']:
+            col = 'counts_%s_%s' % (pro, band)
+            if not col in T.columns():
+                v = np.array(len(T), np.int64)
+            else:
+                v = T.get(col).astype(np.int64)
+            T.set(col, v)
+    T.brickname = np.array([brickname_from_filename(fn)] * len(T))
+    TT.append(T)
+    
+    for ifn,fn in enumerate(fns):
+        print('Reading', ifn, 'of', len(fns), ':', fn)
+        t = fits_table(fn)
+        assert(np.all(t.depthlo == T.depthlo))
+        assert(np.all(t.depthhi == T.depthhi))
+        cols = t.get_columns()
+        t.brickname = np.array([brickname_from_filename(fn)] * len(t))
+        for band in 'grz':
+            col = 'counts_ptsrc_%s' % band
+            if not col in cols:
+                continue
+            C = T.get(col)
+            C += t.get(col)
+            col = 'counts_gal_%s' % band
+            C = T.get(col)
+            C += t.get(col)
+        TT.append(t)
+            
+    T.writeto(summaryfn)
+    print('Wrote', summaryfn)
+    
+    T = merge_tables(TT)
+    T.writeto(outfn)
+    print('Wrote', outfn)
+
+
+    T = fits_table(summaryfn)
     dlo = T.depthlo.copy()
     dd = dlo[2] - dlo[1]
     dlo[0] = dlo[1] - dd
@@ -21,14 +78,14 @@ if os.path.exists(fn):
         plt.bar(dlo, T.get('counts_ptsrc_%s' % band), width=dd)
         plt.xlabel('Depth: %s band' % band)
         plt.ylabel('Number of pixels')
-        plt.title('DECaLS DR2 Depth: Point Sources, %s' % band)
+        plt.title('DECaLS DR3 Depth: Point Sources, %s' % band)
         ps.savefig()
 
         plt.clf()
         plt.bar(dlo, T.get('counts_gal_%s' % band), width=dd)
         plt.xlabel('Depth: %s band' % band)
         plt.ylabel('Number of pixels')
-        plt.title('DECaLS DR2 Depth: Canonical Galaxy, %s' % band)
+        plt.title('DECaLS DR3 Depth: Canonical Galaxy, %s' % band)
         ps.savefig()
 
     for band in 'grz':
@@ -53,42 +110,5 @@ if os.path.exists(fn):
         plt.title('Depth: SIMP Galaxy, %s' % band)
         ps.savefig()
         
-    import sys
-    sys.exit(0)
-        
-        
-if __name__ == '__main__':
-    fns = glob('cosmos-10/coadd/*/*/*-depth.fits')
-    fns.sort()
-    print len(fns), 'depth files'
 
-    fn = fns.pop(0)
-    print 'Reading', fn
-    T = fits_table(fn)
-    # Create / upgrade the count columns to int64.
-    for band in 'grz':
-        for pro in ['ptsrc', 'gal']:
-            col = 'counts_%s_%s' % (pro, band)
-            if not col in T.columns():
-                v = np.array(len(T), np.int64)
-            else:
-                v = T.get(col).astype(np.int64)
-            T.set(col, v)
-                
-    for ifn,fn in enumerate(fns):
-        print 'Reading', ifn, 'of', len(fns), ':', fn
-        t = fits_table(fn)
-        assert(np.all(t.depthlo == T.depthlo))
-        assert(np.all(t.depthhi == T.depthhi))
-        cols = t.get_columns()
-        for band in 'grz':
-            col = 'counts_ptsrc_%s' % band
-            if not col in cols:
-                continue
-            C = T.get(col)
-            C += t.get(col)
-            col = 'counts_gal_%s' % band
-            C = T.get(col)
-            C += t.get(col)
-
-    T.writeto('dr2-depth.fits')
+    
