76176e4222095de19bea8cc20537e5e77302aaae
Kaylan Burleigh <kburleigh@lbl.gov>
2016-11-18 17:47:34 -0800

    instrinsic (dust removed) mags/fluxes for target selection, observed (includind dust) mags for mag distributions to add to image

--- py/legacyanalysis/decals_sim_priors.py
+++ py/legacyanalysis/decals_sim_priors.py
@@ -1,3 +1,10 @@
+'''
+Uses intrinsic (dust removed) AB fluxes/mags to select ELGs,LRGs,QSOs,STARs
+Makes color-color plots that reproduce the FDR
+Single band, mag distributions, plotted using "as observed" AB fluxes/mags,
+    since these are what one adds into a CP image
+'''
+
 if __name__ == '__main__':
     import matplotlib
     matplotlib.use('Agg')
@@ -10,6 +17,8 @@ from astrometry.util.fits import fits_table, merge_tables
 import os
 import sys
 from scipy.optimize import newton
+
+from theValidator.catalogues import CatalogueFuncs 
 
 class _GaussianMixtureModel(object):
     """Read and sample from a pre-defined Gaussian mixture model.
@@ -172,8 +181,8 @@ def get_rgb_cols():
             (255,0,0),(0,255,0),(0,0,255),\
             (0,0,0)]
 
-def flux2mag(nanoflux):
-    return 22.5-2.5*np.log10(nanoflux)
+#def flux2mag(nanoflux):
+#    return 22.5-2.5*np.log10(nanoflux)
 
 # Globals
 xyrange=dict(x_star=[-0.5,2.2],\
@@ -255,28 +264,28 @@ class TSBox(object):
 
 
 
-def elg_data():
-    '''Use DEEP2 ELGs whose SEDs have been modeled.'''
-    elgs = fits.getdata('/project/projectdirs/desi/spectro/templates/basis_templates/v2.2/elg_templates_v2.0.fits', 1)
-    # Colors
-    gg = elgs['DECAM_G']
-    rr = elgs['DECAM_R']
-    zz = elgs['DECAM_Z']
-    gr = gg - rr
-    rz = rr - zz
-    Xall = np.array([rz, gr]).T
-    # Cuts
-    has_morph = elgs['radius_halflight'] > 0
-    cuts= dict(has_morph=has_morph) 
-    print('%d/%d of Fit Template Spectra ELGs have morphologies' % (len(elgs[has_morph]), len(elgs)))
-    # Morphology
-    morph= {}
-    morph['rz'] = rz[has_morph]
-    morph['gr'] = gr[has_morph]
-    morph['r50'] = elgs['RADIUS_HALFLIGHT'][has_morph] #arcsec
-    morph['n'] = elgs['SERSICN'][has_morph]                            
-    morph['ba'] = elgs['AXIS_RATIO'][has_morph] #minor/major
-    return Xall,cuts,morph                            
+#def elg_data():
+#    '''Use DEEP2 ELGs whose SEDs have been modeled.'''
+#    elgs = fits.getdata('/project/projectdirs/desi/spectro/templates/basis_templates/v2.2/elg_templates_v2.0.fits', 1)
+#    # Colors
+#    gg = elgs['DECAM_G']
+#    rr = elgs['DECAM_R']
+#    zz = elgs['DECAM_Z']
+#    gr = gg - rr
+#    rz = rr - zz
+#    Xall = np.array([rz, gr]).T
+#    # Cuts
+#    has_morph = elgs['radius_halflight'] > 0
+#    cuts= dict(has_morph=has_morph) 
+#    print('%d/%d of Fit Template Spectra ELGs have morphologies' % (len(elgs[has_morph]), len(elgs)))
+#    # Morphology
+#    morph= {}
+#    morph['rz'] = rz[has_morph]
+#    morph['gr'] = gr[has_morph]
+#    morph['r50'] = elgs['RADIUS_HALFLIGHT'][has_morph] #arcsec
+#    morph['n'] = elgs['SERSICN'][has_morph]                            
+#    morph['ba'] = elgs['AXIS_RATIO'][has_morph] #minor/major
+#    return Xall,cuts,morph                            
 
 
 class ReadWrite(object):
@@ -325,14 +334,10 @@ class CommonInit(ReadWrite):
         '''See: https://desi.lbl.gov/trac/wiki/TargetSelectionWG/TargetSelection#SpectrophotometricStandardStarsFSTD
            data is a fits_table object with Tractor Catalogue columns
         '''
-        # Remove whitespaces 'PSF ' --> 'PSF'
-        if not 'decam_mag' in data.get_columns():
-            from theValidator.catalogues import CatalogueFuncs 
-            CatalogueFuncs().set_extra_data(data)
         RFLUX_obs = data.get('decam_flux')[:,2]
-        GFLUX = data.get('decam_flux')[:,1] / data.get('decam_mw_transmission')[:,1]
-        RFLUX = data.get('decam_flux')[:,2] / data.get('decam_mw_transmission')[:,2]
-        ZFLUX = data.get('decam_flux')[:,4] / data.get('decam_mw_transmission')[:,4]
+        GFLUX = data.get('decam_flux_nodust')[:,1]
+        RFLUX = data.get('decam_flux_nodust')[:,2]
+        ZFLUX = data.get('decam_flux_nodust')[:,4]
         GRZSN = data.get('decam_flux')[:,[1,2,4]] * np.sqrt(data.get('decam_flux_ivar')[:,[1,2,4]])
         GRCOLOR = 2.5 * np.log10(RFLUX / GFLUX)
         RZCOLOR = 2.5 * np.log10(ZFLUX / RFLUX)
@@ -349,9 +354,7 @@ class CommonInit(ReadWrite):
 class ELG(CommonInit):
     def __init__(self,**kwargs):
         super(ELG, self).__init__(**kwargs)
-        self.rlimit= 23.4
-        if 'rlimit' in kwargs:
-            self.rlimit= kwargs['rlimit']
+        self.rlimit= kwargs.get('rlimit',23.4)
         print('ELGs, self.rlimit= ',self.rlimit)
     
     def get_elgs_FDR_cuts(self):
@@ -359,18 +362,14 @@ class ELG(CommonInit):
         https://desi.lbl.gov/DocDB/cgi-bin/private/RetrieveFile?docid=912'''
         if self.DR == 2:
             zcat = self.read_fits(os.path.join(self.truth_dir,'../deep2/v3.0/','deep2-field1-oii.fits.gz'))
-            G_MAG= zcat.get('cfhtls_g')
             R_MAG= zcat.get('cfhtls_r')
-            Z_MAG= zcat.get('cfhtls_z')
             rmag_cut= R_MAG<self.rlimit 
         elif self.DR == 3:
             zcat = self.read_fits(os.path.join(self.truth_dir,'deep2f234-dr3matched.fits'))
             decals = self.read_fits(os.path.join(self.truth_dir,'dr3-deep2f234matched.fits'))
             # Add mag data 
-            G_MAG= decals.get('decam_mag')[:,1]
-            R_MAG= decals.get('decam_mag')[:,2]
-            Z_MAG= decals.get('decam_mag')[:,4]
-            rflux= decals.get('decam_flux')[:,2]/decals.get('decam_mw_transmission')[:,2]
+            CatalogueFuncs().set_mags(decals)
+            rflux= decals.get('decam_flux_nodust')[:,2]
             rmag_cut= rflux > 10**((22.5-self.rlimit)/2.5)
             rmag_cut*= self.imaging_cut(decals) 
         # Cuts
@@ -397,13 +396,23 @@ class ELG(CommonInit):
                                    zcat.get('oii_3727')>oiicut1),axis=0)
 
         # color data
-        rz,gr,r={},{},{}
+        if self.DR == 2:
+            G_MAG= zcat.get('cfhtls_g')
+            R_MAG= zcat.get('cfhtls_r')
+            Z_MAG= zcat.get('cfhtls_z')
+        elif self.DR == 3:
+            G_MAG= decals.get('decam_mag_nodust')[:,1]
+            R_MAG= decals.get('decam_mag_nodust')[:,2]
+            Z_MAG= decals.get('decam_mag_nodust')[:,4]
+            R_MAG_wdust= decals.get('decam_mag_wdust')[:,2]
+        rz,gr,r_nodust,r_wdust={},{},{},{}
         for key,cut in zip(['loz','oiifaint','oiibright_loz','oiibright_hiz','med2hiz_oiibright'],\
                            [loz,oiifaint,oiibright_loz,oiibright_hiz,med2hiz_oiibright]):
             rz[key]= (R_MAG - Z_MAG)[cut]
             gr[key]= (G_MAG - R_MAG)[cut]
-            r[key]= R_MAG[cut]
-        return rz,gr,r
+            r_nodust[key]= R_MAG[cut]
+            r_wdust[key]= R_MAG_wdust[cut]
+        return rz,gr,r_nodust,r_wdust
 
     def plot_FDR(self):
         rz,gr,r= self.get_elgs_FDR_cuts()
@@ -442,7 +451,7 @@ class ELG(CommonInit):
             print('Wrote {}'.format(name))
  
     def plot_FDR_multipanel(self):
-        rz,gr,r= self.get_elgs_FDR_cuts()
+        rz,gr,r_nodust,r_wdust= self.get_elgs_FDR_cuts()
         ts= TSBox(src='ELG')
         xrange,yrange= xyrange['x_elg'],xyrange['y_elg']
         # Plot
@@ -484,9 +493,7 @@ class ELG(CommonInit):
 class LRG(CommonInit):
     def __init__(self,**kwargs):
         super(LRG, self).__init__(**kwargs)
-        self.zlimit= 20.46
-        if 'zlimit' in  kwargs:
-            self.zlimit= kwargs['zlimit']
+        self.zlimit= kwargs.get('zlimit',20.46)
         print('LRGs, self.zlimit= ',self.zlimit)
     
     def get_lrgs_FDR_cuts(self):
@@ -497,8 +504,9 @@ class LRG(CommonInit):
             decals=self.read_fits( os.path.join(self.truth_dir,'dr3-cosmoszphotmatched.fits') )
             spec=self.read_fits( os.path.join(self.truth_dir,'cosmos-zphot-dr3matched.fits') )
         # DECaLS
-        Z_FLUX = decals.get('decam_flux'.lower())[:,4] / decals.get('decam_mw_transmission'.lower())[:,4]
-        W1_FLUX = decals.get('wise_flux'.lower())[:,0] / decals.get('wise_mw_transmission'.lower())[:,0]
+        CatalogueFuncs().set_mags(decals)
+        Z_FLUX = decals.get('decam_flux_nodust')[:,4]
+        W1_FLUX = decals.get('wise_flux_nodust')[:,0]
         index={}
         # BUG!!
         #index['decals']= np.all((Z_FLUX < 10**((22.5-20.46)/2.5),\
@@ -527,17 +535,19 @@ class LRG(CommonInit):
                                     spec.get('mod_gal') <= 8,\
                                     spec.get('zp_gal') > 0.6),axis=0)
         # return Mags
-        rz,rW1,r={},{},{}
-        R_FLUX = decals.get('decam_flux')[:,2] / decals.get('decam_mw_transmission')[:,2]
+        rz,rW1,r_nodust,r_wdust,z_nodust,z_wdust={},{},{},{},{},{}
         for key in ['star','red_galaxy_lowz','red_galaxy_hiz','blue_galaxy']:
             cut= index[key]
-            rz[key]= flux2mag(R_FLUX[cut]) - flux2mag(Z_FLUX[cut])
-            rW1[key]= flux2mag(R_FLUX[cut]) - flux2mag(W1_FLUX[cut])
-            r[key]= flux2mag(R_FLUX[cut])
-        return rz,rW1,r
+            rz[key]= decals.get('decam_mag_nodust')[:,2][cut] - decals.get('decam_mag_nodust')[:,4][cut]
+            rW1[key]= decals.get('decam_mag_nodust')[:,2][cut] - decals.get('wise_mag_nodust')[:,0][cut]
+            r_nodust[key]= decals.get('decam_mag_nodust')[:,2][cut]
+            z_nodust[key]= decals.get('decam_mag_nodust')[:,4][cut]
+            r_wdust[key]= decals.get('decam_mag_wdust')[:,2][cut]
+            z_wdust[key]= decals.get('decam_mag_wdust')[:,4][cut]
+        return rz,rW1,r_nodust,r_wdust,z_nodust,z_wdust
                                  
     def plot_FDR(self):
-        rz,rW1,r= self.get_lrgs_FDR_cuts()
+        rz,rW1,r_nodust,r_wdust,z_nodust,z_wdust= self.get_lrgs_FDR_cuts()
         fig,ax = plt.subplots()
         rgb_cols=get_rgb_cols()
         xrange,yrange= xyrange['x_lrg'],xyrange['y_lrg']
@@ -566,7 +576,7 @@ class LRG(CommonInit):
             print('Wrote {}'.format(name))
 
     def plot_FDR_multipanel(self):
-        rz,rW1,r= self.get_lrgs_FDR_cuts()
+        rz,rW1,r_nodust,r_wdust,z_nodust,z_wdust= self.get_lrgs_FDR_cuts()
         fig,ax = plt.subplots(1,4,sharex=True,sharey=True,figsize=(16,4))
         plt.subplots_adjust(wspace=0.1,hspace=0)
         rgb_cols=get_rgb_cols()
@@ -602,8 +612,9 @@ class LRG(CommonInit):
         elif self.DR == 3:
             decals = self.read_fits(os.path.join(self.truth_dir,'dr3-vipersw1w4matched.fits'))
             vip = self.read_fits(os.path.join(self.truth_dir,'vipersw1w4-dr3matched.fits'))
-        Z_FLUX = decals.get('decam_flux')[:,4] / decals.get('decam_mw_transmission')[:,4]
-        W1_FLUX = decals.get('wise_flux')[:,0] / decals.get('wise_mw_transmission')[:,0]
+        CatalogueFuncs().set_mags(decals)
+        Z_FLUX = decals.get('decam_flux_nodust')[:,4]
+        W1_FLUX = decals.get('wise_flux_nodust')[:,0]
         index={}
         index['decals']= np.all((Z_FLUX > 10**((22.5-self.zlimit)/2.5),\
                                  W1_FLUX > 0.),axis=0)
@@ -617,11 +628,10 @@ class LRG(CommonInit):
                                  vip.get('zspec') < 9.9),axis=0) 
         # return Mags
         rz,rW1={},{}
-        R_FLUX = decals.get('decam_flux')[:,2] / decals.get('decam_mw_transmission')[:,2]
         cut= np.all((index['decals'],\
                      index['good_z']),axis=0)
-        rz= flux2mag(R_FLUX[cut]) - flux2mag(Z_FLUX[cut])
-        rW1= flux2mag(R_FLUX[cut]) - flux2mag(W1_FLUX[cut])
+        rz= decals.get('decam_mag_nodust')[:,2][cut] - decals.get('decam_mag_nodust')[:,4][cut]
+        rW1= decals.get('decam_mag_nodust')[:,2][cut] - decals.get('wise_mag_nodust')[:,0][cut]
         return rz,rW1
     
     def plot_vipers(self):
@@ -712,6 +722,7 @@ class STAR(CommonInit):
         # https://desi.lbl.gov/trac/wiki/TargetSelectionWG/TargetSelection#SpectrophotometricStandardStarsFSTD
         if self.DR == 2:
             stars=fits_table(os.path.join(self.truth_dir,'Stars_str82_355_4.DECaLS.dr2.fits'))
+            CatalogueFuncs().set_mags(stars)
             stars.cut( self.std_star_cut(stars) )
             return stars
         elif self.DR == 3:
@@ -726,22 +737,19 @@ class STAR(CommonInit):
 class QSO(CommonInit):
     def __init__(self,**kwargs):
         super(QSO,self).__init__(**kwargs)
-        self.rlimit= 22.7
-        if 'rlimit' in  kwargs:
-            self.rlimit= kwargs['rlimit']
+        self.rlimit= kwargs.get('rlimit',22.7)
         print('QSOs, self.rlimit= ',self.rlimit)
   
     def get_qsos(self):
-        from theValidator.catalogues import CatalogueFuncs 
         if self.DR == 2:        
             qsos= self.read_fits( os.path.join(self.truth_dir,'AllQSO.DECaLS.dr2.fits') )
             # Add AB mags
-            CatalogueFuncs().set_extra_data(qsos)
+            CatalogueFuncs().set_mags(qsos)
             qsos.cut( self.imaging_cut(qsos) )
             # r < 22.7, grz > 17
-            GFLUX = qsos.get('decam_flux')[:,1] / qsos.get('decam_mw_transmission')[:,1]
-            RFLUX = qsos.get('decam_flux')[:,2] / qsos.get('decam_mw_transmission')[:,2]
-            ZFLUX = qsos.get('decam_flux')[:,4] / qsos.get('decam_mw_transmission')[:,4]
+            GFLUX = qsos.get('decam_flux_nodust')[:,1] 
+            RFLUX = qsos.get('decam_flux_nodust')[:,2]
+            ZFLUX = qsos.get('decam_flux_nodust')[:,4] 
             GRZFLUX = (GFLUX + 0.8* RFLUX + 0.5* ZFLUX ) / 2.3
             cut= np.all((RFLUX > 10**((22.5-self.rlimit)/2.5),\
                          GRZFLUX < 10**((22.5-17.0)/2.5)),axis=0)
@@ -750,8 +758,8 @@ class QSO(CommonInit):
         elif self.DR == 3:
             qsos=self.read_fits( os.path.join(self.truth_dir,'qso-dr3sweepmatched.fits') )
             decals=self.read_fits( os.path.join(self.truth_dir,'dr3-qsosweepmatched.fits') )
+            CatalogueFuncs().set_mags(decals)
             decals.set('z',qsos.get('z'))
-            CatalogueFuncs().set_extra_data(decals)
             decals.cuts( self.imaging_cut(decals) )
             return decals
         #G_FLUX= decals.get('decam_flux')[:,1]/decals.get('decam_mw_transmission')[:,1]
@@ -782,22 +790,22 @@ class QSO(CommonInit):
         fig,ax = plt.subplots(1,2,figsize=(10,4))
         plt.subplots_adjust(wspace=0.1,hspace=0)
         # Stars
-        ax[0].scatter(stars.get('decam_mag')[:,2]-stars.get('decam_mag')[:,4],\
-                      stars.get('decam_mag')[:,1]-stars.get('decam_mag')[:,2],\
+        ax[0].scatter(stars.get('decam_mag_nodust')[:,2]-stars.get('decam_mag_nodust')[:,4],\
+                      stars.get('decam_mag_nodust')[:,1]-stars.get('decam_mag_nodust')[:,2],\
                       c='b',edgecolors='none',marker='o',s=10.,rasterized=True, label='stars',alpha=self.alpha)
-        W= 0.75*stars.get('wise_mag')[:,0]+ 0.25*stars.get('wise_mag')[:,1]
-        ax[1].scatter(stars.get('decam_mag')[:,1]-stars.get('decam_mag')[:,4],\
-                      stars.get('decam_mag')[:,2]-W,\
+        W= 0.75*stars.get('wise_mag_nodust')[:,0]+ 0.25*stars.get('wise_mag_nodust')[:,1]
+        ax[1].scatter(stars.get('decam_mag_nodust')[:,1]-stars.get('decam_mag_nodust')[:,4],\
+                      stars.get('decam_mag_nodust')[:,2]-W,\
                       c='b',edgecolors='none',marker='o',s=10.,rasterized=True, label='stars',alpha=self.alpha)
         # QSOs
         for key,lab,col in zip(['loz','hiz'],['(z < 2.1)','(z > 2.1)'],['magenta','red']):
             i= index[key]
-            ax[0].scatter(qsos.get('decam_mag')[:,2][i]-qsos.get('decam_mag')[:,4][i],\
-                          qsos.get('decam_mag')[:,1][i]-qsos.get('decam_mag')[:,2][i],\
+            ax[0].scatter(qsos.get('decam_mag_nodust')[:,2][i]-qsos.get('decam_mag_nodust')[:,4][i],\
+                          qsos.get('decam_mag_nodust')[:,1][i]-qsos.get('decam_mag_nodust')[:,2][i],\
                           c=col,edgecolors='none',marker='o',s=10.,rasterized=True, label='qso '+lab,alpha=self.alpha)
-            W= 0.75*qsos.get('wise_mag')[:,0]+ 0.25*qsos.get('wise_mag')[:,1]
-            ax[1].scatter(qsos.get('decam_mag')[:,1][i]-qsos.get('decam_mag')[:,4][i],\
-                          qsos.get('decam_mag')[:,2][i]-W[i],\
+            W= 0.75*qsos.get('wise_mag_nodust')[:,0]+ 0.25*qsos.get('wise_mag_nodust')[:,1]
+            ax[1].scatter(qsos.get('decam_mag_nodust')[:,1][i]-qsos.get('decam_mag_nodust')[:,4][i],\
+                          qsos.get('decam_mag_nodust')[:,2][i]-W[i],\
                           c=col,edgecolors='none',marker='o',s=10.,rasterized=True, label='qso '+lab,alpha=self.alpha)
         
         #for xlim,ylim,x_lab,y_lab in ax[0].set_xlim([-0.5,3.])
