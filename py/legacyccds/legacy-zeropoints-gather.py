back to TPV wcs for 90prime and fixed sign error in 90prime to ps1 color coefficient

py/legacyccds/legacy-zeropoints-gather.py
@@ -32,7 +32,10 @@ if __name__ == "__main__":
         fn_split= np.array_split(fns, comm.size)
         cats=[]
         for fn in fn_split[comm.rank]:
-            cats.append( fits_table(fn) )
+            try:
+                cats.append( fits_table(fn) )
+            except IOError:
+                print('File is bad, skipping: %s' % fn)
         cats= merge_tables(cats, columns='fillzero')
         # Gather
         all_cats = comm.gather( cats, root=0 )
@@ -45,7 +48,8 @@ if __name__ == "__main__":
             print("Done")
     else:
         cats=[]
-        for fn in fns:
+        for cnt,fn in enumerate(fns):
+            print('Reading %d/%d' % (cnt,len(fns)))
             cats.append( fits_table(fn) )
         cats= merge_tables(cats, columns='fillzero')
         if os.path.exists(opt.outname):

py/legacyccds/legacy-zeropoints-stars-gain1.8noCorr.py
@@ -704,10 +704,10 @@ class Measurer(object):
             colorterm = np.zeros(nmatch)
         else:
             colorterm = self.colorterm_ps1_to_observed(ps1.median[m2, :], self.band)
-        from legacyanalysis.ps1cat import ps1_to_mosaic,ps1_to_decam,ps1_to_90prime
+        #from legacyanalysis.ps1cat import ps1_to_mosaic,ps1_to_decam,ps1_to_90prime
         #stars['mzls_colorterm']= ps1_to_mosaic(ps1.median[m2, :], self.band)
-        stars['decam_colorterm']= ps1_to_decam(ps1.median[m2, :], self.band)
-        stars['90prime_colorterm']= ps1_to_90prime(ps1.median[m2, :], self.band)
+        #stars['decam_colorterm']= ps1_to_decam(ps1.median[m2, :], self.band)
+        #stars['90prime_colorterm']= ps1_to_90prime(ps1.median[m2, :], self.band)
         
         # Compute the astrometric residuals relative to PS1.
         #radiff = (stars['ra'] - stars['ps1_ra']) * np.cos(np.deg2rad(ccddec)) * 3600.0
@@ -944,10 +944,11 @@ class NinetyPrimeMeasurer(Measurer):
         return mask
   
     def get_wcs(self):
-        # Bok_CP WCS coeffs are unusuable, use TAN for now
-        self.hdr['CTYPE1'] = 'RA---TAN'
-        self.hdr['CTYPE2'] = 'DEC--TAN'
-        return wcs_pv2sip_hdr(self.hdr)
+        return wcs_pv2sip_hdr(self.hdr) # PV distortion
+    #    # Bok_CP WCS coeffs are unusuable, use TAN for now
+    #    self.hdr['CTYPE1'] = 'RA---TAN'
+    #    self.hdr['CTYPE2'] = 'DEC--TAN'
+    #    return wcs_pv2sip_hdr(self.hdr)
 
 
 def camera_name(primhdr):