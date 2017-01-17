ba8842f90760783effe39a8d714905c79e1f7d0d
Dustin Lang <dstndstn@gmail.com>
2017-01-17 16:49:47 -0500

    Eliminate NaNs in rchi2, fracflux, fracmasked

--- py/legacypipe/oneblob.py
+++ py/legacypipe/oneblob.py
@@ -1391,8 +1391,12 @@ def _compute_source_metrics(srcs, tims, bands, tr):
     rchi2      = rchi2_num      / rchi2_den
     fracmasked = fracmasked_num / fracmasked_den
 
-    #print('Fracflux:', B.fracflux)
-    
+    # Eliminate NaNs (these happen when, eg, we have no coverage in one band but
+    # sources detected in another band, hence denominator is zero)
+    fracflux  [  fracflux_den == 0] = 0.
+    rchi2     [     rchi2_den == 0] = 0.
+    fracmasked[fracmasked_den == 0] = 0.
+
     # fracin_{num,den} are in flux * nimages units
     tinyflux = 1e-9
     fracin     = fracin_num     / np.maximum(tinyflux, fracin_den)
