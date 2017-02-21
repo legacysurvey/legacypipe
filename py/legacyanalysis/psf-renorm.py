30229c212c76b7a30bdc12dfbcbb49a105daf759
Dustin Lang <dstndstn@gmail.com>
2017-02-21 11:10:54 -0800

    look at PsfEx normalization

--- py/legacyanalysis/psf-renorm.py
+++ py/legacyanalysis/psf-renorm.py
@@ -19,6 +19,8 @@ import os
 
 if __name__ == '__main__':
 
+    ps = PlotSequence('psfnorm')
+
     survey = get_survey('dr4v2')
     ccds = survey.get_ccds_readonly()
     print(len(ccds), 'CCDs')
@@ -47,8 +49,12 @@ if __name__ == '__main__':
             traceback.print_exc()
             continue
 
+        tt = 'BASS expnum %i, ccd %s' % (ccd.expnum, ccd.ccdname)
+
         h,w = im.get_image_shape()
 
+        plt.clf()
+        
         for x,y in [(0,0), (0,h), (w,0), (w,h), (w/2, h/2)]:
             psfimg = psf.getImage(x, y)
             print('PSF image at', (x,y))#, ':', psfimg)
@@ -57,12 +63,13 @@ if __name__ == '__main__':
             ph,pw = psfimg.shape
             print('psf img shape', psfimg.shape)
 
-            apertures = [1., 3., 3.75, 5., 6., 7., 7.5, 8., 9., 10., 12.5, 15.]
+            apertures = [1., 3., 3.5, 3.75, 4., 6., 8., 10.]
 
             #apxy = np.array([[pw/2., ph/2.]])
 
-            for apxy in [np.array([[pw/2., ph/2.]]),
-                         np.array([[pw/2, ph/2]])]:
+            #for ioff,apxy in enumerate([np.array([[pw/2., ph/2.]]),
+            #                            np.array([[pw/2, ph/2]])]):
+            for ioff,apxy in enumerate([np.array([[pw/2, ph/2]])]):
                 print('apxy', apxy)
 
                 ap = []
@@ -72,4 +79,49 @@ if __name__ == '__main__':
                     ap.append(p.field('aperture_sum'))
 
                 ap = np.vstack(ap).T
+                ap = ap[0,:]
                 print('Aperture fluxes:', ap)
+
+                #plt.plot(apertures, ap, '.-', color='br'[ioff], label='%i,%i + %.1f,%.1f' % (x, y, apxy[0,0], apxy[0,1]))
+                plt.plot(apertures, ap, '.-', label='%i,%i' % (x, y))
+        plt.legend(loc='lower right')
+        plt.xlabel('Aperture')
+        plt.ylabel('Flux of PSF model')
+        plt.ylim(0.8, 1.2)
+        plt.xlim(2, 6)
+        plt.axhline(1.0, color='k', alpha=0.25)
+        plt.axvline(3.75, color='k', alpha=0.25)
+        plt.title(tt)
+        ps.savefig()
+
+        plt.ylim(0.99, 1.01)
+        plt.xlim(3.6, 3.9)
+        ps.savefig()
+
+
+        plt.clf()
+        ap0 = None
+        for ibase,psfimg in enumerate(psf.psfex.psfbases):
+            ph,pw = psfimg.shape
+            apxy = np.array([[pw/2, ph/2]])
+            ap = []
+            for rad in apertures:
+                aper = photutils.CircularAperture(apxy, rad)
+                p = photutils.aperture_photometry(psfimg, aper)
+                ap.append(p.field('aperture_sum'))
+            ap = np.vstack(ap).T
+            ap = ap[0,:]
+            if ibase == 0:
+                ap0 = ap
+                continue
+            plt.plot(apertures, ap, '.-', label='Basis %i' % ibase)
+        ax = plt.axis()
+        plt.plot(apertures, ap0 - 1., '.-', label='Basis 0, flux-1')
+        plt.axis(ax)
+        plt.legend(loc='lower right')
+        plt.xlabel('Aperture')
+        plt.ylabel('Flux of PSF basis image')
+        plt.axhline(0.0, color='k', alpha=0.25)
+        plt.axvline(3.75, color='k', alpha=0.25)
+        plt.title(tt)
+        ps.savefig()
