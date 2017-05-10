0a7c0aceca2db4a298244da3c0f7a743e77e24a4
Marc Manera <manera@cori08-bond0.224>
2017-05-10 09:06:25 -0700

    Add deph histogram subroutine for DR2, DR3 and DR4

--- validationtests/DESIccdManera.py
+++ validationtests/DESIccdManera.py
@@ -10,11 +10,12 @@ import matplotlib.pyplot as plt
 from quicksipManera import *
 
 dir = '$HOME/' # obviously needs to be changed
-inputdir = '/project/projectdirs/cosmo/data/legacysurvey/dr3/' # where I get my data 
-#inputdir= '/global/projecta/projectdirs/cosmo/work/dr4/'
+#inputdir = '/project/projectdirs/cosmo/data/legacysurvey/dr3/' # where I get my data 
+inputdir= '/global/projecta/projectdirs/cosmo/work/dr4/'
 localdir = '/global/homes/m/manera/DESI/validation-outputs/' #place for local DESI stuff
 
 extmap = np.loadtxt('/global/homes/m/manera/DESI/validation-outputs/healSFD_r_256_fullsky.dat') # extintion map remove it 
+
 
 
 ### A couple of useful conversions
@@ -72,14 +73,14 @@ def plotdepthfromIvar(band,depthmin=23.8,mjdmax='',prop='ivar',op='total',survey
 	print len(ral)/(float(nside)**2.*12)*360*360./pi		
 	#print min(val),max(val)	
 	#map = plt.scatter(ral,decl,c=magvalgmin,cmap=cm.rainbow,lw=0,s=.1)
-	map = plt.scatter(ral,decl,c=magvalgmin, cmap=cm.rainbow,s=2., vmin=21.0, vmax=23.5, lw=0,edgecolors='none')
+	map = plt.scatter(ral,decl,c=magvalgmin, cmap=cm.rainbow,s=2., vmin=21.5, vmax=23.5, lw=0,edgecolors='none')
 	cbar = plt.colorbar(map)
-	cbar.set_label(r'5$\sigma$ galaxy depth', rotation=270)
+	cbar.set_label(r'5$\sigma$ galaxy depth', rotation=270,labelpad=1)
 	plt.xlabel('r.a. (degrees)')
 	plt.ylabel('declination (degrees)')
 	plt.title('Map of depth' +' for '+survey+' '+band+'-band')
 	plt.xlim(0,360)
-	plt.ylim(-90,90)
+	plt.ylim(-30,90)
 	#plt.show()
 	plt.savefig(localdir+'depth_'+band+'_'+survey+str(nside)+'.png')
 	#plt.xscale('log')
@@ -89,106 +90,248 @@ def plotdepthfromIvar(band,depthmin=23.8,mjdmax='',prop='ivar',op='total',survey
 		
 
 
-# ------------------------------------------------------
-# AJR hack of Boris' code. Many original things from Boris are just commented out
-# DESI_RUN edited from Boris' DES example
-# ------------------------------------------------------
+def depthfromIvar():
+    
+    # ------------------------------------------------------
+    # AJR hack of Boris' code. Many original things from Boris are just commented out
+    # DESI_RUN edited from Boris' DES example
+    # ------------------------------------------------------
+    
+    nside = 1024 # Resolution of output maps
+    nsidesout = None # if you want full sky degraded maps to be written
+    ratiores = 1 # Superresolution/oversampling ratio, simp mode doesn't allow anything other than 1
+    mode = 1 # 1: fully sequential, 2: parallel then sequential, 3: fully parallel
+    
+    mjd_max = 10e10
+    #mjd_max = 57000
+    mjdw = ''
+    if mjd_max != 10e10:
+    	mjdw += 'mjdmax'+str(mjd_max)
+    #catalogue_name = 'DECaLS_DR2'+mjdw
+    #catalogue_name = 'DECaLS_DR3'+mjdw
+    #catalogue_name = 'DECaLS_DR4'+mjdw
+    catalogue_name = '90prime_DR4'+mjdw
+    #catalogue_name = 'MZLS_DR4'+mjdw
+    pixoffset = 0 # How many pixels are being removed on the edge of each CCD? 15 for DES.
+    #fname = inputdir+'ccds-annotated-decals.fits.gz'
+    fname = inputdir+'ccds-annotated-dr4-90prime.fits.gz'
+    #fname = inputdir+'ccds-annotated-dr4-mzls.fits.gz'
+    
+    #fname = localdir+'decals-ccds-annotated.fits'
+    # Where to write the maps ? Make sure directory exists.
+    outroot = localdir
+    
+    tbdata = pyfits.open(fname)[1].data
+    print tbdata.dtype
+    
+    # ------------------------------------------------------
+    # Read data
+    
+    #def invnoisesq here
+    nmag = Magtonanomaggies(tbdata['galdepth'])/5.
+    ivar = 1./nmag**2.
+    # Select the five bands
+    indg = np.where((tbdata['filter'] == 'g') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True)) 
+    #indr = np.where((tbdata['filter'] == 'r') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True))
+    #indz = np.where((tbdata['filter'] == 'z') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True))
+    #sample_names = ['band_g', 'band_r', 'band_z']
+    sample_names = ['band_g']
+    #sample_names = ['band_r']
+    #sample_names = ['band_z']
+    #inds = [indg, indr, indz]
+    inds = [indg]
+    #inds = [indr]
+    #inds = [indz]
+    
+    # What properties do you want mapped?
+    # Each each tuple, the first element is the quantity to be projected,
+    # the second is the weighting scheme, and the third is the operation.
+    propertiesandoperations = [
+        #('count', '', 'fracdet'),  # CCD count with fractional values in pixels partially observed.
+        #('EXPTIME', '', 'total'), # Total exposure time, constant weighting.
+        #('maglimit3', '', ''), # Magnitude limit (3rd method, correct)
+        #('FWHM_MEAN', '', 'mean'), # Mean FWHM (called FWHM_MEAN because it's already the mean per CCD)
+        #('FWHM_MEAN', 'coaddweights3', 'mean'), # Same with with weighting corresponding to CDD noise
+        #('mjd_obs', '', 'max'), #
+        ('ivar', '', 'total'),
+        #('FWHM_FROMFLUXRADIUS_MEAN', '', 'mean'),
+        #('FWHM_FROMFLUXRADIUS_MEAN', 'coaddweights3', 'mean'),
+        #('NSTARS_ACCEPTED_MEAN', '', 'mean'),
+        #('NSTARS_ACCEPTED_MEAN', 'coaddweights3', 'mean'),
+        #('FWHM', '', 'mean'),
+        #('FWHM', '', 'min'),
+        #('AIRMASS', '', 'mean'),
+        #('AIRMASS', 'coaddweights3', 'mean'),
+        #('SKYBRITE', '', 'mean'),
+        #('SKYBRITE', 'coaddweights3', 'mean'),
+        #('SKYSIGMA', '', 'mean'),
+        #('SKYSIGMA', 'coaddweights3', 'mean')
+        ]
+    
+    # What properties to keep when reading the images? Should at least contain propertiesandoperations and the image corners.
+    #propertiesToKeep = ['COADD_ID', 'ID', 'TILENAME', 'BAND', 'AIRMASS', 'SKYBRITE', 'SKYSIGMA', 'EXPTIME'] \
+    #	+ ['FWHM', 'FWHM_MEAN', 'FWHM_PIXELFREE_MEAN', 'FWHM_FROMFLUXRADIUS_MEAN', 'NSTARS_ACCEPTED_MEAN'] \
+    propertiesToKeep = [ 'filter', 'AIRMASS', 'FWHM','mjd_obs'] \
+    	+ ['RA', 'DEC', 'crval1', 'crval2', 'crpix1', 'crpix2', 'cd1_1', 'cd1_2', 'cd2_1', 'cd2_2','width','height'] \
+        + ['ra0','ra1','ra2','ra3','dec0','dec1','dec2','dec3','ra','dec'] #, 'NAXIS1', 'NAXIS2']  \
+    	 #\
+    #    + ['PV1_'+str(i) for i in range(11)] + ['PV2_'+str(i) for i in range(11)] \
+    #   + ['RALL', 'RAUL', 'RAUR', 'RALR', 'DECLL', 'DECUL', 'DECUR', 'DECLR', 'URALL', 'UDECLL', 'URAUR', 'UDECUR', 'COADD_RA', 'COADD_DEC'] \
+    #    + ['COADD_MAGZP', 'MAGZP']
+    
+    # Create big table with all relevant properties. We will send it to the Quicksip library, which will do its magic.
+    #tbdata = np.core.records.fromarrays([tbdata[prop] for prop in propertiesToKeep] + [URAUL, UDECUL, URALR, UDECLR,ivar], names = propertiesToKeep + ['URAUL', 'UDECUL', 'URALR', 'UDECLR', 'ivar'])
+    tbdata = np.core.records.fromarrays([tbdata[prop] for prop in propertiesToKeep] + [ivar], names = propertiesToKeep + [ 'ivar'])
+    
+    # Do the magic! Read the table, create Healtree, project it into healpix maps, and write these maps.
+    project_and_write_maps(mode, propertiesandoperations, tbdata, catalogue_name, outroot, sample_names, inds, nside, ratiores, pixoffset, nsidesout)
+    #project_and_write_maps_simp(mode, propertiesandoperations, tbdata, catalogue_name, outroot, sample_names, inds, nside)
+    
+    # ------------------------------------------------------
+    #plot depth maps
+    # __________________
+    
+    depthminl = [21.5] #if doing multiple bands, add numbers there
+    for i in range(0,len(sample_names)):
+    	band = sample_names[i].split('_')[-1] 
+    	depthmin = depthminl[i]
+    	plotdepthfromIvar(band,depthmin=depthmin,mjdmax=mjdw,survey=catalogue_name)
 
-nside = 1024 # Resolution of output maps
-nsidesout = None # if you want full sky degraded maps to be written
-ratiores = 1 # Superresolution/oversampling ratio, simp mode doesn't allow anything other than 1
-mode = 1 # 1: fully sequential, 2: parallel then sequential, 3: fully parallel
 
-mjd_max = 10e10
-#mjd_max = 57000
-mjdw = ''
-if mjd_max != 10e10:
-	mjdw += 'mjdmax'+str(mjd_max)
-#catalogue_name = 'DECaLS_DR2'+mjdw
-catalogue_name = 'DECaLS_DR3'+mjdw
-#catalogue_name = 'DECaLS_DR4'+mjdw
-#catalogue_name = '90prime_DR4'+mjdw
-#catalogue_name = 'MZLS_DR4'+mjdw
-pixoffset = 0 # How many pixels are being removed on the edge of each CCD? 15 for DES.
-fname = inputdir+'ccds-annotated-decals.fits.gz'
-#fname = inputdir+'ccds-annotated-dr4-90prime.fits.gz'
-#fname = inputdir+'ccds-annotated-dr4-mzls.fits.gz'
+def plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR3',survey='decals'):
+	#this produces the histogram for Dustin's processed galaxy depth
+	import fitsio
+	from matplotlib import pyplot as plt
+	from numpy import zeros,array
+	from random import random
+	if rel == 'DR2':
+		f = fitsio.read(inputdir+'decals-ccds-annotated.fits')
+	if rel == 'DR3':
+		f = fitsio.read(inputdir+'ccds-annotated-'+survey+'.fits.gz')
+        if rel == 'DR4':
+            if (band == 'g' or band == 'r'):
+                f = fitsio.read(inputdir+'ccds-annotated-dr4-90prime.fits.gz')
+            if band == 'z' :
+                f = fitsio.read(inputdir+'ccds-annotated-dr4-mzls.fits.gz') 
+	
+	nl = []
+	nd = 0
+	nbr = 0	
+		
+	if band == 'g':
+		be = 1
+		zp0 = 25.08
+		recm = 24.
+	if band == 'r':
+		be = 2
+		zp0 = 25.29
+		recm = 23.4
+	if band == 'z':
+		be = 4
+		zp0 = 24.92
+		recm = 22.5
 
-#fname = localdir+'decals-ccds-annotated.fits'
-# Where to write the maps ? Make sure directory exists.
-outroot = localdir
+	n = 0
+	for i in range(0,len(f)):
+		pid = f[i]['propid']
+		#if DS == '2014B-0404' or DS == '2013A-0741': #enforce DECaLS only
+		#	DS = 1
+		DS = 0
+		year = int(f[i]['date_obs'].split('-')[0])
+		if year > 2014:
+			#if pid == '2014B-0404' or pid == '2013A-0741':
+			DS = 1 #enforce 2015 data
 
-tbdata = pyfits.open(fname)[1].data
-print tbdata.dtype
+		if f[i]['filter'] == band:
+			
+			if DS == 1:
+				n += 1				
+				if f[i]['dec'] > -20 and f[i]['photometric'] == True and f[i]['blacklist_ok'] == True :   
+				
+				#if f[i]['seeing'] != 99 and f[i]['ccdzpt'] != 99 and f[i]['fwhm'] != 99 and DS == 1:
+					#if f[i]['dec'] > -20 and f[i]['exptime'] >=30 and f[i]['ccdnmatch'] >= 20 and abs(f[i]['zpt'] - f[i]['ccdzpt']) <= 0.1 and f[i]['zpt'] >= zp0-.5 and f[i]['zpt'] <=zp0+.25:   
+					ext = f[i]['decam_extinction'][be]
+					detsig1 = Magtonanomaggies(f[i]['galdepth'])/5. #total noise
+					signalext = 1./10.**(-ext/2.5)
+					nl.append(detsig1*signalext)
+			
+	print n	
+	print len(nl)
+	ng = len(nl)
+	NTl = []
+	for nd in range(0,int(ndraw)):
+		detsigtoti = 0
+		for i in range(0,Nexp):
+			ind = int(random()*ng)
+			detsig1 = nl[ind]
+			detsigtoti += 1./detsig1**2.
+		detsigtot = sqrt(1./detsigtoti)
+		m = nanomaggiesToMag(detsigtot * 5.)
+		if m > recm:
+			nbr += 1.	
+		NTl.append(m)
+		n += 1.
+	minN = min(NTl)
+	maxN = max(NTl)+.0001
+	print minN,maxN
+	hl = zeros((nbin))
+	for i in range(0,len(NTl)):
+		bin = int(nbin*(NTl[i]-minN)/(maxN-minN))
+		hl[bin] += 1
+	Nl = []
+	for i in range(0,len(hl)):
+		Nl.append(minN+i*(maxN-minN)/float(nbin)+0.5*(maxN-minN)/float(nbin))
+	NTl = array(NTl)
+	mean = sum(NTl)/float(len(NTl))
+	std = sqrt(sum(NTl**2.)/float(len(NTl))-mean**2.)
+	NTl.sort()
+	if len(NTl)/2. != len(NTl)/2:
+		med = NTl[len(NTl)/2+1]
+	else:
+		med = (NTl[len(NTl)/2+1]+NTl[len(NTl)/2])/2.
+	print mean,med,std
+	print 'percentage better than requirements '+str(nbr/float(nd))
+	from matplotlib.backends.backend_pdf import PdfPages
+	plt.clf()
+	pp = PdfPages(localdir+'validationplots/'+rel+survey+band+str(Nexp)+'exposures.pdf')	
 
-# ------------------------------------------------------
-# Read data
+	plt.plot(Nl,hl,'k-')
+	plt.xlabel(r'5$\sigma$ '+band+ ' depth')
+	plt.ylabel('# of ccds')
+	plt.title('MC '+str(Nexp)+' exposure depth '+str(mean)[:5]+r'$\pm$'+str(std)[:4]+r', $f_{\rm pass}=$'+str(nbr/float(nd))[:5]+'\n '+rel+' '+survey)
+	#plt.xscale('log')
+	pp.savefig()
+	pp.close()
+	return True
 
-#def invnoisesq here
-nmag = Magtonanomaggies(tbdata['galdepth'])/5.
-ivar = 1./nmag**2.
-# Select the five bands
-indg = np.where((tbdata['filter'] == 'g') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True)) 
-#indr = np.where((tbdata['filter'] == 'r') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True))
-#indz = np.where((tbdata['filter'] == 'z') & (tbdata['photometric'] == True) & (tbdata['blacklist_ok'] == True))
-#sample_names = ['band_g', 'band_r', 'band_z']
-sample_names = ['band_g']
-#sample_names = ['band_r']
-#sample_names = ['band_z']
-#inds = [indg, indr, indz]
-inds = [indg]
-#inds = [indr]
-#inds = [indz]
 
-# What properties do you want mapped?
-# Each each tuple, the first element is the quantity to be projected,
-# the second is the weighting scheme, and the third is the operation.
-propertiesandoperations = [
-    #('count', '', 'fracdet'),  # CCD count with fractional values in pixels partially observed.
-    #('EXPTIME', '', 'total'), # Total exposure time, constant weighting.
-    #('maglimit3', '', ''), # Magnitude limit (3rd method, correct)
-    #('FWHM_MEAN', '', 'mean'), # Mean FWHM (called FWHM_MEAN because it's already the mean per CCD)
-    #('FWHM_MEAN', 'coaddweights3', 'mean'), # Same with with weighting corresponding to CDD noise
-    #('mjd_obs', '', 'max'), #
-    ('ivar', '', 'total'),
-    #('FWHM_FROMFLUXRADIUS_MEAN', '', 'mean'),
-    #('FWHM_FROMFLUXRADIUS_MEAN', 'coaddweights3', 'mean'),
-    #('NSTARS_ACCEPTED_MEAN', '', 'mean'),
-    #('NSTARS_ACCEPTED_MEAN', 'coaddweights3', 'mean'),
-    #('FWHM', '', 'mean'),
-    #('FWHM', '', 'min'),
-    #('AIRMASS', '', 'mean'),
-    #('AIRMASS', 'coaddweights3', 'mean'),
-    #('SKYBRITE', '', 'mean'),
-    #('SKYBRITE', 'coaddweights3', 'mean'),
-    #('SKYSIGMA', '', 'mean'),
-    #('SKYSIGMA', 'coaddweights3', 'mean')
-    ]
+# --- run depth maps 
+# depthfromIvar()
 
-# What properties to keep when reading the images? Should at least contain propertiesandoperations and the image corners.
-#propertiesToKeep = ['COADD_ID', 'ID', 'TILENAME', 'BAND', 'AIRMASS', 'SKYBRITE', 'SKYSIGMA', 'EXPTIME'] \
-#	+ ['FWHM', 'FWHM_MEAN', 'FWHM_PIXELFREE_MEAN', 'FWHM_FROMFLUXRADIUS_MEAN', 'NSTARS_ACCEPTED_MEAN'] \
-propertiesToKeep = [ 'filter', 'AIRMASS', 'FWHM','mjd_obs'] \
-	+ ['RA', 'DEC', 'crval1', 'crval2', 'crpix1', 'crpix2', 'cd1_1', 'cd1_2', 'cd2_1', 'cd2_2','width','height'] + ['ra0','ra1','ra2','ra3','dec0','dec1','dec2','dec3','ra','dec'] #, 'NAXIS1', 'NAXIS2']  \
-	 #\
-#    + ['PV1_'+str(i) for i in range(11)] + ['PV2_'+str(i) for i in range(11)] \
-#   + ['RALL', 'RAUL', 'RAUR', 'RALR', 'DECLL', 'DECUL', 'DECUR', 'DECLR', 'URALL', 'UDECLL', 'URAUR', 'UDECUR', 'COADD_RA', 'COADD_DEC'] \
-#    + ['COADD_MAGZP', 'MAGZP']
+# --- run histogram deph peredictions
+#Nexp=3
+#band='g'
+#plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR3',survey='decals')
+#
+#Nexp=3
+#band='r'
+#plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR3',survey='decals')
+#
+#Nexp=3
+#band='z'
+#plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR3',survey='decals')
 
-# Create big table with all relevant properties. We will send it to the Quicksip library, which will do its magic.
-#tbdata = np.core.records.fromarrays([tbdata[prop] for prop in propertiesToKeep] + [URAUL, UDECUL, URALR, UDECLR,ivar], names = propertiesToKeep + ['URAUL', 'UDECUL', 'URALR', 'UDECLR', 'ivar'])
-tbdata = np.core.records.fromarrays([tbdata[prop] for prop in propertiesToKeep] + [ivar], names = propertiesToKeep + [ 'ivar'])
+Nexp=3
+band='g'
+plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR4',survey='90prime')
 
-# Do the magic! Read the table, create Healtree, project it into healpix maps, and write these maps.
-project_and_write_maps(mode, propertiesandoperations, tbdata, catalogue_name, outroot, sample_names, inds, nside, ratiores, pixoffset, nsidesout)
-#project_and_write_maps_simp(mode, propertiesandoperations, tbdata, catalogue_name, outroot, sample_names, inds, nside)
+Nexp=3
+band='r'
+plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR4',survey='90prime')
 
-# ------------------------------------------------------
-#plot depth maps
-# __________________
+Nexp=3
+band='z'
+plotMaghist_proc_Nexp(band,Nexp,ndraw = 1e5,nbin=100,rel='DR4',survey='MLZ')
 
-depthminl = [22.] #if doing multiple bands, add numbers there
-for i in range(0,len(sample_names)):
-	band = sample_names[i].split('_')[-1] 
-	depthmin = depthminl[i]
-	plotdepthfromIvar(band,depthmin=depthmin,mjdmax=mjdw,survey=catalogue_name)
+
+
