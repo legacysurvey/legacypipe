3a901d30fb4d8e8e97971cd17950009b35e1d456
Dustin Lang <dstndstn@gmail.com>
2017-06-11 06:51:06 -0700

    comment out galaxy-norm debugging

--- py/legacypipe/image.py
+++ py/legacypipe/image.py
@@ -454,185 +454,185 @@ class LegacySurveyImage(object):
         galmod /= galmod.sum()
         galnorm = np.sqrt(np.sum(galmod**2))
 
-        h,w = galmod.shape
-        import pylab as plt
-        # plt.clf()
-        # plt.imshow(galmod, interpolation='nearest', origin='lower',
-        #            vmin=0, vmax=0.06)
-        # # zoom in on 15x15 center
-        # plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        # plt.colorbar()
-        # plt.title('galmod: %s expnum %i, band %s, galnorm %.3f' % (self.camera, self.expnum, tim.band, galnorm))
-        # psgalnorm.savefig()
-
-        from tractor import PointSource
-
-        print('galaxy_norm: getting PointSource patch')
-        psf = PointSource(pos, NanoMaggies(**{band:1.}))
-        psfmod = psf.getModelPatch(tim, modelMask=mm).patch
-
-        orig_psfmod = psfmod.copy()
-
-        print('orig galmod range:', orig_galmod.min(), orig_galmod.max())
-        print('orig psfmod range:', orig_psfmod.min(), orig_psfmod.max())
-
-        print('Orig psfmod sum:', orig_psfmod.sum())
-        print('Orig galmod sum:', orig_galmod.sum())
-
-        mn = min(np.min(orig_galmod), np.min(orig_psfmod))
-        mx = max(np.max(orig_galmod), np.max(orig_psfmod))
-
-        psfmod = np.maximum(0, psfmod)
-        print('PSF sum after clamping up to zero:', psfmod.sum())
-        psfmod /= psfmod.sum()
-        psfnorm = np.sqrt(np.sum(psfmod**2))
-
-        slc = (slice(h//2-7, h//2+8), slice(w//2-7, w//2+8))
-        print('Norm of central galaxy slice:', np.sqrt(np.sum((galmod[slc] / galmod[slc].sum())**2)))
-        print('Norm of central PSF slice:', np.sqrt(np.sum((psfmod[slc] / psfmod[slc].sum())**2)))
-
-        from scipy.ndimage.filters import gaussian_filter
-        psfconv = gaussian_filter(orig_psfmod, 1.2)
-        psfconv = np.maximum(0, psfconv)
-        print('PSF sum after clamping up to zero:', psfmod.sum())
-        psfconv /= psfconv.sum()
-        nm = np.sqrt(np.sum(psfconv**2))
-        print('Norm of PSF convolved by Gaussian:', nm)
-
-        plt.clf()
-        plt.subplot(2,2,1)
-        plt.imshow(orig_galmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('orig gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-        plt.subplot(2,2,2)
-        plt.imshow(orig_psfmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('orig psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-        plt.subplot(2,2,3)
-        diff = orig_galmod - orig_psfmod
-        dmx = np.max(np.abs(diff))
-        plt.imshow(diff, interpolation='nearest', origin='lower',
-                   vmin=-dmx, vmax=dmx)
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('galmod - psfmod')
-        plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
-        psgalnorm.savefig()
-
-
-
-        plt.clf()
-        plt.subplot(2,2,1)
-        plt.imshow(orig_galmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        plt.title('orig gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-        plt.subplot(2,2,2)
-        plt.imshow(orig_psfmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        plt.title('orig psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-        plt.subplot(2,2,3)
-        diff = orig_galmod - orig_psfmod
-        dmx = np.max(np.abs(diff))
-        plt.imshow(diff, interpolation='nearest', origin='lower',
-                   vmin=-dmx, vmax=dmx)
-        plt.title('galmod - psfmod')
-        plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
-        psgalnorm.savefig()
-
-
-        mx = max(np.max(galmod), np.max(psfmod))
-
-
-        plt.clf()
-        plt.subplot(2,2,1)
-        plt.imshow(galmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-
-        plt.subplot(2,2,2)
-        plt.imshow(psfmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-
-        plt.subplot(2,2,3)
-        diff = galmod - psfmod
-        mx = np.max(np.abs(diff))
-        plt.imshow(diff, interpolation='nearest', origin='lower',
-                   vmin=-mx, vmax=mx)
-        #plt.colorbar()
-        # zoom in on 15x15 center
-        plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
-        plt.title('galmod - psfmod')
-
-        plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
-
-        psgalnorm.savefig()
-
-
-        plt.clf()
-        plt.subplot(2,2,1)
-        plt.imshow(galmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-        plt.subplot(2,2,2)
-        plt.imshow(psfmod, interpolation='nearest', origin='lower',
-                   vmin=mn, vmax=mx)
-        plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-        plt.subplot(2,2,3)
-        diff = galmod - psfmod
-        mx = np.max(np.abs(diff))
-        plt.imshow(diff, interpolation='nearest', origin='lower',
-                   vmin=-mx, vmax=mx)
-        plt.title('galmod - psfmod')
-        plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
-        psgalnorm.savefig()
-
-
-
-        #ima = dict(interpolation='nearest', origin='lower', vmin=-0.001*mx,
-        #           vmax=0.001*mx, cmap='RdBu')
-        # ima = dict(interpolation='nearest', origin='lower', vmin=-1,
-        #            vmax=1, cmap='RdBu')
-        # 
-        # plt.clf()
-        # plt.subplot(1,2,1)
-        # plt.imshow(np.sign(orig_galmod), **ima)
-        # plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-        # 
-        # plt.subplot(1,2,2)
-        # plt.imshow(np.sign(orig_psfmod), **ima)
-        # plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-        # 
-        # plt.suptitle('%s expnum %i, band %s: sign' % (self.camera, self.expnum, tim.band))
-        # 
-        # psgalnorm.savefig()
-
-
-        mx = max(np.max(orig_galmod), np.max(orig_psfmod))
-        ima = dict(interpolation='nearest', origin='lower',
-                   vmin=-6 + np.log10(mx),
-                   vmax=np.log10(mx))
-
-        plt.clf()
-        plt.subplot(1,2,1)
-        plt.imshow(np.log10(orig_galmod), **ima)
-        plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
-
-        plt.subplot(1,2,2)
-        plt.imshow(np.log10(orig_psfmod), **ima)
-        plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
-
-        plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
-
-        psgalnorm.savefig()
+        #  h,w = galmod.shape
+        #  import pylab as plt
+        #  # plt.clf()
+        #  # plt.imshow(galmod, interpolation='nearest', origin='lower',
+        #  #            vmin=0, vmax=0.06)
+        #  # # zoom in on 15x15 center
+        #  # plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  # plt.colorbar()
+        #  # plt.title('galmod: %s expnum %i, band %s, galnorm %.3f' % (self.camera, self.expnum, tim.band, galnorm))
+        #  # psgalnorm.savefig()
+        #  
+        #  from tractor import PointSource
+        #  
+        #  print('galaxy_norm: getting PointSource patch')
+        #  psf = PointSource(pos, NanoMaggies(**{band:1.}))
+        #  psfmod = psf.getModelPatch(tim, modelMask=mm).patch
+        #  
+        #  orig_psfmod = psfmod.copy()
+        #  
+        #  print('orig galmod range:', orig_galmod.min(), orig_galmod.max())
+        #  print('orig psfmod range:', orig_psfmod.min(), orig_psfmod.max())
+        #  
+        #  print('Orig psfmod sum:', orig_psfmod.sum())
+        #  print('Orig galmod sum:', orig_galmod.sum())
+        #  
+        #  mn = min(np.min(orig_galmod), np.min(orig_psfmod))
+        #  mx = max(np.max(orig_galmod), np.max(orig_psfmod))
+        #  
+        #  psfmod = np.maximum(0, psfmod)
+        #  print('PSF sum after clamping up to zero:', psfmod.sum())
+        #  psfmod /= psfmod.sum()
+        #  psfnorm = np.sqrt(np.sum(psfmod**2))
+        #  
+        #  slc = (slice(h//2-7, h//2+8), slice(w//2-7, w//2+8))
+        #  print('Norm of central galaxy slice:', np.sqrt(np.sum((galmod[slc] / galmod[slc].sum())**2)))
+        #  print('Norm of central PSF slice:', np.sqrt(np.sum((psfmod[slc] / psfmod[slc].sum())**2)))
+        #  
+        #  from scipy.ndimage.filters import gaussian_filter
+        #  psfconv = gaussian_filter(orig_psfmod, 1.2)
+        #  psfconv = np.maximum(0, psfconv)
+        #  print('PSF sum after clamping up to zero:', psfmod.sum())
+        #  psfconv /= psfconv.sum()
+        #  nm = np.sqrt(np.sum(psfconv**2))
+        #  print('Norm of PSF convolved by Gaussian:', nm)
+        #  
+        #  plt.clf()
+        #  plt.subplot(2,2,1)
+        #  plt.imshow(orig_galmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('orig gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  plt.subplot(2,2,2)
+        #  plt.imshow(orig_psfmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('orig psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  plt.subplot(2,2,3)
+        #  diff = orig_galmod - orig_psfmod
+        #  dmx = np.max(np.abs(diff))
+        #  plt.imshow(diff, interpolation='nearest', origin='lower',
+        #             vmin=-dmx, vmax=dmx)
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('galmod - psfmod')
+        #  plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
+        #  psgalnorm.savefig()
+        #  
+        #  
+        #  
+        #  plt.clf()
+        #  plt.subplot(2,2,1)
+        #  plt.imshow(orig_galmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  plt.title('orig gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  plt.subplot(2,2,2)
+        #  plt.imshow(orig_psfmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  plt.title('orig psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  plt.subplot(2,2,3)
+        #  diff = orig_galmod - orig_psfmod
+        #  dmx = np.max(np.abs(diff))
+        #  plt.imshow(diff, interpolation='nearest', origin='lower',
+        #             vmin=-dmx, vmax=dmx)
+        #  plt.title('galmod - psfmod')
+        #  plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
+        #  psgalnorm.savefig()
+        #  
+        #  
+        #  mx = max(np.max(galmod), np.max(psfmod))
+        #  
+        #  
+        #  plt.clf()
+        #  plt.subplot(2,2,1)
+        #  plt.imshow(galmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  
+        #  plt.subplot(2,2,2)
+        #  plt.imshow(psfmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  
+        #  plt.subplot(2,2,3)
+        #  diff = galmod - psfmod
+        #  mx = np.max(np.abs(diff))
+        #  plt.imshow(diff, interpolation='nearest', origin='lower',
+        #             vmin=-mx, vmax=mx)
+        #  #plt.colorbar()
+        #  # zoom in on 15x15 center
+        #  plt.axis([w//2-7, w//2+7, h//2-7, h//2+7])
+        #  plt.title('galmod - psfmod')
+        #  
+        #  plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
+        #  
+        #  psgalnorm.savefig()
+        #  
+        #  
+        #  plt.clf()
+        #  plt.subplot(2,2,1)
+        #  plt.imshow(galmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  plt.subplot(2,2,2)
+        #  plt.imshow(psfmod, interpolation='nearest', origin='lower',
+        #             vmin=mn, vmax=mx)
+        #  plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  plt.subplot(2,2,3)
+        #  diff = galmod - psfmod
+        #  mx = np.max(np.abs(diff))
+        #  plt.imshow(diff, interpolation='nearest', origin='lower',
+        #             vmin=-mx, vmax=mx)
+        #  plt.title('galmod - psfmod')
+        #  plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
+        #  psgalnorm.savefig()
+        #  
+        #  
+        #  
+        #  #ima = dict(interpolation='nearest', origin='lower', vmin=-0.001*mx,
+        #  #           vmax=0.001*mx, cmap='RdBu')
+        #  # ima = dict(interpolation='nearest', origin='lower', vmin=-1,
+        #  #            vmax=1, cmap='RdBu')
+        #  # 
+        #  # plt.clf()
+        #  # plt.subplot(1,2,1)
+        #  # plt.imshow(np.sign(orig_galmod), **ima)
+        #  # plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  # 
+        #  # plt.subplot(1,2,2)
+        #  # plt.imshow(np.sign(orig_psfmod), **ima)
+        #  # plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  # 
+        #  # plt.suptitle('%s expnum %i, band %s: sign' % (self.camera, self.expnum, tim.band))
+        #  # 
+        #  # psgalnorm.savefig()
+        #  
+        #  
+        #  mx = max(np.max(orig_galmod), np.max(orig_psfmod))
+        #  ima = dict(interpolation='nearest', origin='lower',
+        #             vmin=-6 + np.log10(mx),
+        #             vmax=np.log10(mx))
+        #  
+        #  plt.clf()
+        #  plt.subplot(1,2,1)
+        #  plt.imshow(np.log10(orig_galmod), **ima)
+        #  plt.title('gal: norm %.3f, pk %.3f' % (galnorm, np.max(galmod)))
+        #  
+        #  plt.subplot(1,2,2)
+        #  plt.imshow(np.log10(orig_psfmod), **ima)
+        #  plt.title('psf: norm %.3f, pk %.3f' % (psfnorm, np.max(psfmod)))
+        #  
+        #  plt.suptitle('%s expnum %i, band %s' % (self.camera, self.expnum, tim.band))
+        #  
+        #  psgalnorm.savefig()
 
 
 
