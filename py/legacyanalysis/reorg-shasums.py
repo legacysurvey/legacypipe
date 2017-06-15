ea157e14bb15b51f1d42ad6057068c24d8b7a455
Dustin Lang <dstndstn@gmail.com>
2017-06-15 10:10:22 -0700

    sha1sum files need two spaces between sha1sum and filename

--- py/legacyanalysis/reorg-shasums.py
+++ py/legacyanalysis/reorg-shasums.py
@@ -3,11 +3,13 @@ from glob import glob
 import os
 
 if __name__ == '__main__':
-    basedir = '/global/projecta/projectdirs/cosmo/work/dr4c'
+    #basedir = '/global/projecta/projectdirs/cosmo/work/dr4c'
+    basedir = '/global/projecta/projectdirs/cosmo/work/dr4_backup/brick_sha1sums'
 
     outdir = 'sha1sums'
 
-    pat = os.path.join(basedir, 'tractor', '*', 'brick*.sha*')
+    pat = os.path.join(basedir, '*', 'brick*.sha*')
+    #pat = os.path.join(basedir, 'tractor', '*', 'brick*.sha*')
     #pat = os.path.join(basedir, 'tractor', '055', 'brick*.sha*')
     print('Pattern:', pat)
     fns = glob(pat)
@@ -38,7 +40,7 @@ if __name__ == '__main__':
         shafn = os.path.join(shadir, 'legacysurvey_dr4_' + dirnm.replace('/','_') + '.sha1sum')
         f = open(shafn, 'w')
         for fn,sha in files:
-            f.write('%s %s\n' % (sha, fn))
+            f.write('%s  %s\n' % (sha, fn))
         f.close()
         print('  wrote', shafn)
 
