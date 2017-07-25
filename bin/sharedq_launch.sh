17a88304591efea8a937c3f13a834ccf603c445d
Kaylan Burleigh <kburleigh@lbl.gov>
2017-07-25 10:36:42 -0700

    moved dr4 bash scripts in legacypipe/bin/ to legacypipe/bin/dr4

--- bin/sharedq_launch.sh
+++ bin/sharedq_launch.sh
@@ -1,24 +1,109 @@
 #!/bin/bash 
 
-#export bricklist=$SCRATCH/test/legacypipe/py/coadd_dirs.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/dr4_bricks.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/tractor_fns.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/tractor_i_fns.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/tractors_proja_dr4c.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/dr3_bricks.txt
-#export bricklist=/global/projecta/projectdirs/cosmo/work/dr4/dr4_bricks.txt
+# Python stack, env vars, etc
+setEnv() {
+    local fn=$1
+    echo "#Environment" >> $fn
+    echo "source ~/.bashrc_desiconda" >> $fn
+    echo "export LEGACY_SURVEY_DIR=/global/cscratch1/sd/kaylanb/dr5_bricks/dr5-new" >> $fn
+}
 
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/fns_for_25bricks.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/zpts_remain2.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/decals_fns.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/decals_fns_1000.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/zpts_oom.txt
-export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/err_timeout.txt
+getLogDir() {
+    local outdir=$1
+    local dmy=`date|awk '{print $3$2$NF}'`
+    echo ${outdir}/logs/${NERSC_HOST}_${dmy}
+}
 
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/fns_for_25bricks_rerun.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/tractor_fns.txt
-#export bricklist=/global/cscratch1/sd/kaylanb/test/legacypipe/py/tractor_i_fns.txt
+getLogName() {
+    local logdir=$1
+    local brick=$2
+    local hm=`date|awk '{print $4}'|awk -F ":" '{print $1$2}'`
+    echo "${logdir}/log.${brick}_${hm}"
+}
 
+# configures MPI/OpenMP for cori haswell
+setOMP() {
+    local fn=$1
+    local pythreads=$2
+    # number of OpenMP threads per MPI task
+    echo "#Configure MPI/OpenMP for cori haswell" >> $fn
+    echo "export OMP_PLACES=cores" >> $fn
+    echo "export OMP_NUM_THREADS=$pythreads" >> $fn
+    # stays 1 no matter what
+    # https://software.intel.com/en-us/articles/using-threaded-intel-mkl-in-multi-thread-application
+    echo "export MKL_NUM_THREADS=1" >> $fn
+    # check mpi/openmp settings
+    echo "export KMP_AFFINITY=verbose" >> $fn
+}
+
+# 0 for true, 1 for false
+isShared() {
+    local part=$1
+    if [ ${part} == "shared" ];then
+        return 0
+    else
+        return 1
+    fi
+}
+
+isCori() {
+    if [ "$NERSC_HOST" = "cori" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Limit memory to avoid 1 srun killing whole node
+# ...Shared queue only
+limMemory() {
+    local fn=$1
+    local part=$2
+    local usecores=$3
+    if isShared $part; then 
+        if isCori;then
+            # 128 GB / node = 1345000000 kbytes
+            local maxmem=134000000
+            let usemem=${maxmem}*${usecores}/32
+        else
+            # 62 GB / node = 65000000 kbytes
+            local maxmem=65000000
+            let usemem=${maxmem}*${usecores}/24
+        fi
+        echo "#Limit memory for Shared Queue jobs" >> $fn
+        echo "ulimit -S -v $usemem" >> $fn
+        echo "ulimit -a" >> $fn
+    fi
+}
+
+
+getSrun() {
+    local part=$1
+    local brick=$2
+    local outdir=$3 
+    local hypercores=$4 
+    local pythreads=$5 
+    local logfn=$6
+    local cmd="python legacypipe/runbrick.py --brick ${brick} --outdir ${outdir} --threads ${pythreads} --run dr5 --no-write --skip --skip-calibs >> ${logfn} 2>&1"
+    if isShared ${part};then
+        echo "srun -n 1 -c ${hypercores} ${cmd}"
+    else
+        echo "srun -n 1 -c ${hypercores} --cpu_bind=cores ${cmd} &"
+    fi
+}
+
+getJobsPerNode() {
+    local part=$1
+    local usecores=$2
+    if isShared ${part};then
+        echo 1
+    else
+        let a=32/${usecores}
+        echo $a
+    fi
+}
+
+export bricklist=/global/cscratch1/sd/kaylanb/dr5_bricks/legacypipe/py/dr5_bricks25.txt
 
 echo bricklist=$bricklist
 if [ ! -e "$bricklist" ]; then
@@ -26,26 +111,27 @@ if [ ! -e "$bricklist" ]; then
     exit 999
 fi
 
-#
-export need_project=yes
-export need_projecta=no
-export set_plus_x=yes
-
-export proj_dir=/global/projecta/projectdirs/cosmo/work/dr4
-export dr4c_dir=/global/cscratch1/sd/desiproc/dr4/data_release/dr4c
-export run=dr3
-#export run=legacy
-export outdir=/global/cscratch1/sd/kaylanb/dr5_zpts
-export walltime=01:00:00
-export jobname=zpts
-export usecores=1
-export threads=1
+#export part=shared
+export part=debug
+export outdir=/global/cscratch1/sd/kaylanb/dr5_bricks/runs
+export logdir=$(getLogDir ${outdir})
+mkdir -p $logdir
+export walltime=00:30:00
+export jobname=dr5
+#export need_project=no
+#export need_projecta=yes
+# MPI taks must be divisible by 64!!
+# if isn't set --cpu_bind=cores in srun
+# and -c to (32/4)*2 in srun
+export usecores=4
+export pythreads=4
 let hypercores=${usecores}*2
 export hypercores=${hypercores}
 # Loop over a star and end
-step=1
+step=$(getJobsPerNode $part $usecores)
 beg=1
 max=`wc -l ${bricklist} |awk '{print $1}'`
+max=1
 ###
 cnt=0
 # Make 1 script for each i of loop
@@ -56,49 +142,47 @@ for star in `seq ${beg} ${step} ${max}`;do
     # Make job script with N = step srun commands
     export fn=../bin/sharedq_job_${start_brick}_${end_brick}.sh
     cp ../bin/sharedq_job_template.sh $fn
+    # KEY
+    setEnv $fn
+    setOMP $fn $pythreads
+    limMemory $fn $part $usecores
     # 2 Options
     # 1) Multiple sruns, 1 per line in bricklist
     while read aline; do
         brick=`echo $aline|awk '{print $1}'`
-        ####
-        #cmd="srun -n 1 -c $usecores python legacypipe/format_headers.py --brick $brick --dr4c_dir ${dr4c_dir}"
-        #echo $cmd >> $fn 
-        #echo "source /global/homes/k/kaylanb/.bashrc_desiconda" >> $fn 
-        ####
-        #echo "srun -n 1 -c $usecores python legacypipe/runbrick.py --run ${run} --brick ${brick} --hybrid-psf --outdir ${outdir} --nsigma 6 --no-write --threads ${threads} --skip-calibs" >> $fn
-        #### 
-        #export bri=`echo $brick|head -c 3`
-        #echo "mkdir -p ${proj_dir}/logs2/${bri}" >> $fn
-        #echo "export logfn=\`find ${proj_dir}/logs/ -name \"log.${brick}*\"|xargs ls -l|tail -n 1|awk '{print \$NF}'\`" >> $fn
-        #echo "echo $brick \${logfn}" >> $fn
-        #echo "cp \${logfn} ${proj_dir}/logs2/${bri}/" >> $fn
-        ###
-        export log=`echo $brick|sed s#/project/projectdirs/cosmo/staging#${outdir}#g|sed s#.fits.fz#.log#g`
-        echo "echo Logging to: ${log}" >> $fn
-        echo "mkdir -p $(dirname $log)" >> $fn
-        cmd="srun -n 1 -c $usecores python legacyccds/legacy_zeropoints.py --camera decam --image ${brick} --outdir ${outdir} >> ${log} 2>&1"
-        echo $cmd >> $fn 
+        echo "#Run" >> $fn
+        logfn=$(getLogName ${logdir} ${brick})
+        echo "echo Logging to: $logfn" >> $fn
+        cmd=$(getSrun $part $brick $outdir $hypercores $pythreads $logfn)
+        echo $cmd >> $fn
+    #    #export bri=`echo $brick|head -c 3`
+    #    #echo "mkdir -p ${proj_dir}/logs2/${bri}" >> $fn
+    #    #echo "export logfn=\`find ${proj_dir}/logs/ -name \"log.${brick}*\"|xargs ls -l|tail -n 1|awk '{print \$NF}'\`" >> $fn
     done <<< "$(sed -n ${start_brick},${end_brick}p $bricklist)"
+    if ! isShared $part; then
+        echo "wait" >> $fn
+    fi
     # 2) One srun, using args start and end
     #cmd="srun -n 1 -c $usecores python ../bin/job_accounting.py --dowhat blobs --fn ${bricklist} --line_start ${start_brick} --line_end ${end_brick}"
     #echo $cmd >> $fn 
     # Modify N cores, nodes, walltime, etc
-    sed -i "s#SBATCH\ -n\ 2#SBATCH\ -n\ ${hypercores}#g" $fn
+    if isShared $part;then
+        sed -i "s#SBATCH\ -n\ 2#SBATCH\ -n\ ${hypercores}#g" $fn
+    else
+        sed -i "s#SBATCH\ -p\ shared#SBATCH\ -p\ ${part}#g" $fn
+        sed -i "s#SBATCH\ -n\ 2#SBATCH\ -N\ 1#g" $fn
+    fi
     sed -i "s#SBATCH\ -t\ 00:10:00#SBATCH\ -t\ ${walltime}#g" $fn
     sed -i "s#SBATCH\ -J\ repack#SBATCH\ -J\ ${jobname}#g" $fn
     sed -i "s#usecores=1#usecores=${usecores}#g" $fn 
-    sed -i "s#threads=1#threads=${threads}#g" $fn 
     if [ "$NERSC_HOST" = "edison" ]; then
         sed -i "s?SBATCH\ -C\ haswell?#SBATCH\ -C\ haswell?g" $fn
     fi
-    if [ ${need_project} == "yes" ];then
-        sed -i "s#SBATCH\ -L\ SCRATCH#SBATCH\ -L\ SCRATCH,project#g" $fn
-    elif [ ${need_projecta} == "yes" ];then
-        sed -i "s#SBATCH\ -L\ SCRATCH#SBATCH\ -L\ SCRATCH,projecta#g" $fn
-    fi
-    if [ ${set_plus_x} == "yes" ];then
-        sed -i "s#set\ -x#set\ +x#g" $fn
-    fi
+    #if [ ${need_project} == "yes" ];then
+    #    sed -i "s#SBATCH\ -L\ SCRATCH#SBATCH\ -L\ SCRATCH,project#g" $fn
+    #elif [ ${need_projecta} == "yes" ];then
+    #    sed -i "s#SBATCH\ -L\ SCRATCH#SBATCH\ -L\ SCRATCH,projecta#g" $fn
+    #fi
     # Submit script created above
     echo Created batch script: $fn 
     sbatch $fn 
