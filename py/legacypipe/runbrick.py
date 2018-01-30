55fdf633983f7a5f3fc34c8e1c3764277291664d
Dustin Lang <dstndstn@gmail.com>
2018-01-30 13:34:01 -0800

    sdss_rgb: handle fields with subset of grz coverage

--- py/legacypipe/runbrick.py
+++ py/legacypipe/runbrick.py
@@ -968,9 +968,8 @@ def stage_image_coadds(survey=None, targetwcs=None, bands=None, tims=None,
         coadd_list.append(('simscoadd', sims_coadd, rgbkwargs))
 
     for name,ims,rgbkw in coadd_list:
-        #rgb = get_rgb(ims, bands, **rgbkw)
-
-        rgb = sdss_rgb(ims, bands, scales=dict(g=6.0, r=3.4, z=2.2), m=0.03)
+        rgb = get_rgb(ims, bands, **rgbkw)
+        #rgb = sdss_rgb(ims, bands)
 
         kwa = {}
         if coadd_bw and len(bands) == 1:
@@ -1015,34 +1014,36 @@ def stage_image_coadds(survey=None, targetwcs=None, bands=None, tims=None,
         del rgb
     return None
 
-def sdss_rgb(rimgs, bands, scales=None, m = 0.02):
+def sdss_rgb(imgs, bands, scales=None, m = 0.03):
     import numpy as np
-    rgbscales = {'u': 1.5, #1.0,
-                 'g': 2.5,
-                 'r': 1.5,
-                 'i': 1.0,
-                 'z': 0.4, #0.3
-                 }
+
+    rgbscales=dict(g=(2, 6.0),
+                   r=(1, 3.4),
+                   z=(0, 2.2))
+
+    # rgbscales = {'u': 1.5, #1.0,
+    #              'g': 2.5,
+    #              'r': 1.5,
+    #              'i': 1.0,
+    #              'z': 0.4, #0.3
+    #              }
     if scales is not None:
         rgbscales.update(scales)
-    b,g,r = [rimg * rgbscales[b] for rimg,b in zip(rimgs, bands)]
-    r = np.maximum(0, r + m)
-    g = np.maximum(0, g + m)
-    b = np.maximum(0, b + m)
-    I = (r+g+b)/3.
+
+    I = 0
+    for img,band in zip(imgs, bands):
+        plane,scale = rgbscales[band]
+        img = np.maximum(0, img * scale + m)
+        I = I + img
+    I /= len(bands)
     Q = 20
     fI = np.arcsinh(Q * I) / np.sqrt(Q)
     I += (I == 0.) * 1e-6
-    R = fI * r / I
-    G = fI * g / I
-    B = fI * b / I
-    # maxrgb = reduce(np.maximum, [R,G,B])
-    # J = (maxrgb > 1.)
-    # R[J] = R[J]/maxrgb[J]
-    # G[J] = G[J]/maxrgb[J]
-    # B[J] = B[J]/maxrgb[J]
-    rgb = np.dstack((R,G,B))
-    rgb = np.clip(rgb, 0, 1)
+    H,W = I.shape
+    rgb = np.zeros((H,W,3), np.float32)
+    for img,band in zip(imgs, bands):
+        plane,scale = rgbscales[band]
+        rgb[:,:,plane] = np.clip((img * scale + m) * fI / I, 0, 1)
     return rgb
 
 def _median_smooth_detmap(X):
