3ce626d29dee55d761415e58980e038801006a15
Dustin Lang <dstndstn@gmail.com>
2018-05-29 09:15:27 -0700

    updated bashrc and pipebrick.sh scripts

--- bin/pipebrick.sh
+++ bin/pipebrick.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-# For modules loaded, see "bashrc" in this directory.
+# This script is run (via qdo) to process a single brick in a Legacy
+# Surveys reduction.
+
+# Assume we are running in the legacypipe/py directory.
+
+source ../bin/bashrc
 
 export PYTHONPATH=${PYTHONPATH}:.
 
@@ -9,19 +14,17 @@ export PYTHONPATH=${PYTHONPATH}:.
 export MKL_NUM_THREADS=1
 
 # Try limiting memory to avoid killing the whole MPI job...
-#ulimit -S -v 15000000
 ulimit -S -v 30000000
 ulimit -a
 
-export LEGACY_SURVEY_DIR=$CSCRATCH/dr5-new-sky
 outdir=$LEGACY_SURVEY_DIR
 
+# First (only) command-line arg is the brick name.
 brick="$1"
-bri=$(echo $brick | head -c 3)
 
+bri=$(echo $brick | head -c 3)
 mkdir -p $outdir/logs/$bri
 log="$outdir/logs/$bri/$brick.log"
-
 echo Logging to: $log
 echo Running on ${NERSC_HOST} $(hostname)
 
@@ -41,17 +44,18 @@ echo -e "\nStarting on ${NERSC_HOST} $(hostname)\n" >> $log
 echo "-----------------------------------------------------------------------------------------" >> $log
 
 python -u legacypipe/runbrick.py \
-    --depth-cut \
     --max-blobsize  250000 \
-    --cache-dir /global/cscratch1/sd/dstn/dr5-new-sky/cache/ \
+    --normalize-psf \
+    --gaia \
+    --skip-calibs \
     --checkpoint $outdir/checkpoints/${bri}/checkpoint-${brick}.pickle \
     --pickle "$outdir/pickles/${bri}/runbrick-%(brick)s-%%(stage)s.pickle" \
     --write-stage srcs \
     --threads 8 \
     --brick $brick --outdir $outdir >> $log 2>&1
 
-    #--skip-calibs \
-    #--force-all \
+#--cache-dir /global/cscratch1/sd/dstn/dr5-new-sky/cache/ \
+#--force-all \
 #--write-stage coadds \
 
 # qdo launch dr2n 16 --cores_per_worker 8 --walltime=24:00:00 --script ../bin/pipebrick.sh --batchqueue regular --verbose
