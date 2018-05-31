ecb517d8733d7d4a7349c653c96f9577cd1ba55b
Dustin Lang <dstndstn@gmail.com>
2018-05-31 09:50:58 -0400

    Bok: use the supplemental static mask provided by Frank

--- py/legacypipe/bok.py
+++ py/legacypipe/bok.py
@@ -17,6 +17,23 @@ class BokImage(LegacySurveyImage):
     def read_invvar(self, **kwargs):
         return self.read_invvar_clipped(**kwargs)
 
+    def read_dq(self, slice=None, **kwargs):
+        # Add supplemental static mask.
+        import os
+        from pkg_resources import resource_filename
+        dq = super(BokImage, self).read_dq(slice=slice, **kwargs)
+        dirname = resource_filename('legacypipe', 'config')
+        fn = os.path.join(dirname, 'ksb_staticmask_ood_v1.fits.fz')
+        F = fitsio.FITS(fn)[self.hdu]
+        if slice is not None:
+            mask = F[slice]
+        else:
+            mask = F.read()
+        # Pixels where the mask==1 that are not already masked get marked
+        # with code 1 ("bad").
+        dq[(mask == 1) * (dq == 0)] = 1
+        return dq
+
     def remap_invvar(self, invvar, primhdr, img, dq):
         return self.remap_invvar_shotnoise(invvar, primhdr, img, dq)
 
