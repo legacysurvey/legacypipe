4712b3f4aff1daeb2d38c4f1c5723aa695a20b6d
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-13 11:16:38 -0700

    remove test_ztfclass

--- py/test_ztfclass.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-
-from legacypipe.decam import DecamImage
-from legacypipe.survey import *
-from legacypipe.ztf import ZTFImage
-import astropy
-tim = super(DecamImagePlusNoise, self)
