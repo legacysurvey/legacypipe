c62b52a3ba588315a7b42d2bbd7159335258fd9b
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-13 11:14:30 -0700

    first clone of ztf class from ptf class

---
+++ py/test_ztfclass.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+
+from legacypipe.decam import DecamImage
+from legacypipe.survey import *
+from legacypipe.ztf import ZTFImage
+import astropy
+tim = super(DecamImagePlusNoise, self)
