80b16532cbef82f901a24911038e00f858902d13
John Moustakas <john.moustakas@gmail.com>
2018-11-29 07:20:37 -0800

    ensure H,W are integers when making detection maps

--- py/legacypipe/detection.py
+++ py/legacypipe/detection.py
@@ -33,6 +33,7 @@ def _detmap(X):
 def detection_maps(tims, targetwcs, bands, mp):
     # Render the detection maps
     H,W = targetwcs.shape
+    H,W = np.int(H), np.int(W)
     ibands = dict([(b,i) for i,b in enumerate(bands)])
 
     detmaps = [np.zeros((H,W), np.float32) for b in bands]
