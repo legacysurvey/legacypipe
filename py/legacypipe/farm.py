00f29a318c56d55aac121a965cfc25919ade3fd5
Dustin Lang <dstndstn@gmail.com>
2019-02-13 13:55:51 -0800

    farm: refactor into multi-threaded app: input thread

--- py/legacypipe/farm.py
+++ py/legacypipe/farm.py
@@ -25,6 +25,180 @@ def main():
               **kwargs)
 
 
+def main2():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('queue', help='QDO queue name to get brick names from')
+    parser.add_argument('--pickle', default='pickles/runbrick-%(brick)s-srcs.pickle',
+                        help='Pickle pattern for "srcs" (source detection) stage pickles')
+    opt = parser.parse_args()
+
+    queuename = opt.queue
+
+
+    import threading
+    import queue
+
+    inqueue = queue.PriorityQueue(maxsize=1000)
+
+    blobsizes = BlobSizeDict()
+
+    inthread = threading.Thread(target=input_thread, args=(queuename, inqueue, blobsizes, opt),
+                                daemon=True)
+    inthread.start()
+
+    while True:
+        import time
+        time.sleep(1)
+
+
+
+def get_blob_iter(T=None,
+                   brickname=None,
+                   brickid=None,
+                   brick=None,
+                   version_header=None,
+                   blobsrcs=None, blobslices=None, blobs=None,
+                   cat=None,
+                   targetwcs=None,
+                   W=None,H=None,
+                   bands=None, ps=None, tims=None,
+                   survey=None,
+                   plots=False, plots2=False,
+                   nblobs=None, blob0=None, blobxy=None, blobradec=None, blobid=None,
+                   max_blobsize=None,
+                   simul_opt=False, use_ceres=True, mp=None,
+                   checkpoint_filename=None,
+                   checkpoint_period=600,
+                   write_pickle_filename=None,
+                   write_metrics=True,
+                   get_all_models=False,
+                   refstars=None,
+                   rex=False,
+                   bailout=False,
+                   record_event=None,
+                   custom_brick=False,
+                   **kwargs):
+    T.orig_ra  = T.ra.copy()
+    T.orig_dec = T.dec.copy()
+    # drop any cached data before we start pickling/multiprocessing
+    survey.drop_cache()
+
+    if refstars:
+        from legacypipe.oneblob import get_inblob_map
+        refstars.radius_pix = np.ceil(refstars.radius * 3600. / targetwcs.pixel_scale()).astype(int)
+        refmap = get_inblob_map(targetwcs, refstars)
+    else:
+        HH, WW = targetwcs.shape
+        refmap = np.zeros((int(HH), int(WW)), np.uint8)
+
+    # Create the iterator over blobs to process
+    blobiter = _blob_iter(blobslices, blobsrcs, blobs, targetwcs, tims,
+                          cat, bands, plots, ps, simul_opt, use_ceres,
+                          refmap, brick, rex,
+                          max_blobsize=max_blobsize, custom_brick=custom_brick)
+    return blobiter
+
+
+# python3.7 foncy
+# from dataclasses import dataclass, field
+# from typing import Any
+# @dataclass(order=True)
+# class PrioritizedItem:
+#     priority: int
+#     item: Any=field(compare=False)
+
+class PrioritizedItem(object):
+    def __init__(self, priority=0, item=None):
+        self.priority = priority
+        self.item = item
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+class BlobSizeDict(object):
+    def __init__(self):
+        import threading
+        self.d = dict()
+        self.lock = threading.Lock()
+    def set(self, brickname, nblobs):
+        with self.lock:
+            self.d[brickname] = nblobs
+    def get(self, brickname):
+        with self.lock:
+            return self.d.get(brickname)
+
+
+
+def queue_work(brickname, inqueue, opt):
+    from astrometry.util.file import unpickle_from_file
+
+    pickle_fn = opt.pickle % dict(brick=brickname)
+    print('Looking for', pickle_fn)
+    if not os.path.exists(pickle_fn):
+        raise RuntimeError('Input pickle does not exist: ' + pickle_fn)
+    kwargs = unpickle_from_file(pickle_fn)
+    print('Unpickled:', kwargs.keys())
+
+    blobiter = get_blob_iter(**kwargs)
+
+    k = 0
+    for arg in blobiter:
+        if arg is None:
+            continue
+
+        # HACK -- reach into args to get blob id...
+        iblob = arg[1]
+        blobw = arg[6]
+        blobh = arg[7]
+        arg = (brickname, iblob, arg)
+        priority = -(blobw*blobh)
+
+        picl = pickle.dumps(arg, -1)
+
+        qitem = PrioritizedItem(priority=priority, item=picl)
+        print('Putting blob', (k+1), 'for brick', brickname)
+        k += 1
+        inqueue.put(qitem)
+
+    # Finished queuing all blobs for this brick -- record how many blobs we sent out.
+    return k
+
+
+def input_thread(queuename, inqueue, blobsizes, opt):
+
+    import qdo
+    q = qdo.connect(queuename)
+
+    while True:
+        task = q.get(timeout=10)
+
+        if task is not None:
+            try:
+                brickname = task.task
+                print('Brick', brickname)
+                # WORK
+                nblobs = queue_work(brickname, inqueue, opt)
+                blobsizes.set(brickname, nblobs)
+                #
+                print('Finished', brickname, 'with', nblobs, 'blobs')
+                #task.set_state(qdo.Task.SUCCEEDED)
+            except:
+                print('Oops')
+                import traceback
+                traceback.print_exc()
+                task.set_state(qdo.Task.FAILED, err=1)
+        else:
+            #- no more tasks in queue so break
+            break        
+
+
+
+
+
+
+
+
 def stage_farmblobs(T=None,
                    brickname=None,
                    brickid=None,
@@ -228,4 +402,5 @@ def stage_farmblobs(T=None,
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
