f8db48b853b5f9994e5908f4550e08228c2f3653
Dustin Lang <dstndstn@gmail.com>
2019-04-06 20:57:28 -0400

    use cat>>, not mv, to move log file into place

--- bin/runcalibs-shifter.sh
+++ bin/runcalibs-shifter.sh
@@ -78,5 +78,5 @@ python /src/legacypipe/py/legacyzpts/legacy_zeropoints.py \
 # Save the return value from the python command -- otherwise we exit 0 because the
 # mv succeeds!
 status=$?
-mv $tmplog $log
+cat $tmplog >> $log
 exit $status
