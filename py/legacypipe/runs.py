8be6d2726d01eadf6b20f24a30f40e8aec8c2c6b
Dustin Lang <dstndstn@gmail.com>
2019-04-12 20:23:21 -0400

    replace old --run entries with decam / 90prime-mosaic ones

--- py/legacypipe/runs.py
+++ py/legacypipe/runs.py
@@ -3,102 +3,17 @@ import numpy as np
 
 from legacypipe.survey import LegacySurveyData
 
-class Dr3DecalsSurvey(LegacySurveyData):
-    # Do we want/need this cut?
-    # def filter_ccds_files(self, fns):
-    #     return [fn for fn in fns if
-    #             ('survey-ccds-decals.fits.gz' in fn or
-    #              'survey-ccds-nondecals.fits.gz' in fn or
-    #              'survey-ccds-extra.fits.gz' in fn)]
-
-    def ccds_for_fitting(self, brick, ccds):
-        return np.flatnonzero(ccds.camera == 'decam')
-
+class DecamSurvey(LegacySurveyData):
     def filter_ccd_kd_files(self, fns):
-        return []
+        return [fn for fn in fns if 'decam' in fn]
 
-class Dr4Survey(LegacySurveyData):
-    def ccds_for_fitting(survey, brick, ccds):
-        return np.flatnonzero(np.logical_or(ccds.camera == 'mosaic',
-                                            ccds.camera == '90prime'))
-
-class Dr4v2(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-90prime.fits.gz' in fn or
-                  'survey-ccds-dr4-mzlsv2.fits.gz' in fn)]
-
-class Dr4v3(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-90prime.fits.gz' in fn or
-                  'survey-ccds-dr4-mzlsv3.fits.gz' in fn)]
-
-
-class Thirdpixv2(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-thirdpix-v2.fits.gz' in fn)]
-                 #('survey-ccds-thirdpix-len2-v2.fits.gz' in fn)]
-                 #('survey-ccds-dr4-mzlsv2.fits.gz' in fn)]
-
-class Thirdpixv3(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-thirdpix-v3.fits.gz' in fn)]
-                 #('survey-ccds-thirdpix-len2-v3.fits.gz' in fn)]
-
-class Dr4Mzlsv2(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-mzlsv2.fits.gz' in fn)]
-
-class Dr4Mzlsv3(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-mzlsv3.fits.gz' in fn)]
-
-class Dr490prime(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-90prime.fits.gz' in fn)]
-
-class Dr4Bootes(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-90prime-bootes.fits.gz' in fn or
-                  'survey-ccds-mzlsv2thruMarch19.fits.gz' in fn)]
-
-class Dr4Bootes90Prime(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-90prime-bootes.fits.gz' in fn)]
-
-class Dr4BootesMzls(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-mzlsv2thruMarch19.fits.gz' in fn)]
-
-class Dr4Plus(Dr4Survey):
-    def filter_ccds_files(self, fns):
-         return [fn for fn in fns if
-                 ('survey-ccds-dr4-90prime.fits.gz' in fn or
-                  'survey-ccds-dr4-mzlsv2.fits.gz' in fn or
-                  'survey-ccds-mzls-runs-16-to-21a.fits.gz' in fn)]
+class NinetyPrimeMosaic(LegacySurveyData):
+    def filter_ccd_kd_files(self, fns):
+        return [fn for fn in fns if ('90prime' in fn) or ('mosaic' in fn)]
 
 runs = {
-    'dr3': Dr3DecalsSurvey,
-    'thirdpix-v2': Thirdpixv2,
-    'thirdpix-v3': Thirdpixv3,
-    'dr4v2': Dr4v2,
-    'dr4v3': Dr4v3,
-    'dr490prime': Dr490prime,
-    'dr4-mzlsv2': Dr4Mzlsv2,
-    'dr4-mzlsv3': Dr4Mzlsv3,
-    'dr4-bootes': Dr4Bootes,
-    'bootes-90prime': Dr4Bootes90Prime,
-    'bootes-mzlsv2thruMarch19': Dr4BootesMzls,
-    'dr4+': Dr4Plus,
+    'decam': DecamSurvey,
+    '90prime-mosaic': NinetyPrimeMosaic,
     None: LegacySurveyData,
 }
 
