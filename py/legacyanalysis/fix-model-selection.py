807921c2233ed87663acde014c280cb4172230be
Dustin Lang <dstndstn@gmail.com>
2019-06-04 11:03:10 -0700

    tidy

--- py/legacyanalysis/fix-model-selection.py
+++ py/legacyanalysis/fix-model-selection.py
@@ -36,7 +36,6 @@ def patch_one(X):
     assert(len(A) == len(np.flatnonzero(T8.type != 'DUP ')))
     typemap = dict(ptsrc='PSF', rex='REX', dev='DEV', exp='EXP', comp='COMP')
     Tnew = T8.copy()
-    #II = []
     npatched = 0
     for i,(d,ttype) in enumerate(zip(A.dchisq, T8.type)):
         dchisqs = dict(zip(['ptsrc','rex','dev','exp','comp'], d))
@@ -47,24 +46,10 @@ def patch_one(X):
         assert(ttype != 'DUP')
         newtype = typemap[mod]
 
-        #if ttype in ['PSF', 'REX']:
-        #    #assert(newtype == ttype)
-        #    #if newtyp
-        #    continue
-        # Can have PSF->EXP, eg, if the REX model fit fails
-
-        # exp->exp, dev->dev, comp->comp
+        # type unchanged
         if ttype == newtype:
             continue
     
-        # exp,dev,comp -> ptsrc
-        #if (ttype in ['DEV', 'EXP', 'COMP']) and newtype == 'PSF':
-        #    II.append(i)
-        #    continue
-
-        # Rarer cases.
-        #print(ttype, '->', newtype)
-
         # Copy fit values from the "newtype" entries in all-models
         cols = ['ra', 'dec', 'ra_ivar', 'dec_ivar']
         nt = newtype.lower()
@@ -113,37 +98,6 @@ def patch_one(X):
 
 
     print('%i of %i: %s patching %i sources' % (ifn+1, Nfns, os.path.basename(fn), npatched))
-    # if len(II) > 0:
-    #     II = np.array(II)
-    #     print('%i of %i: %s patching %i sources' % (ifn+1, len(fns), os.path.basename(fn), len(II)))
-    #     Tnew.type[II] = 'PSF '
-    #     # Copy fit values from the "psf" entries in all-models
-    #     cols = ['ra', 'dec', 'ra_ivar', 'dec_ivar']
-    #     for c in cols:
-    #         Tnew.get(c)[II] = A.get('psf_%s' % c)[II]
-    #     # expand flux, flux_ivar
-    #     for c in ['flux', 'flux_ivar']:
-    #         flux = A.get('psf_%s' % (c))[II]
-    #         if len(abands) == 1:
-    #             Tnew.get('%s_%s' % (c,abands[0]))[i] = flux
-    #         else:
-    #             for ib,band in enumerate(abands):
-    #                 Tnew.get('%s_%s' % (c,band))[i] = flux[ib]
-    #     # zero out shape{dev,exp}_{e1,e2,r}{,_ivar} (recall that the sources are all PSFs)
-    #     for c1 in ['dev','exp']:
-    #         for c2 in ['e1','e2','r']:
-    #             for c3 in ['', '_ivar']:
-    #                 c = 'shape%s_%s%s' % (c1, c2, c3)
-    #                 Tnew.get(c)[II] = 0.
-    #     Tnew.fracdev[II] = 0.
-    #     Tnew.fracdev_ivar[II] = 0.
-    # 
-    #     # recompute bx,by, brick_primary
-    #     ok,x,y = brickwcs.radec2pixelxy(Tnew.ra[II], Tnew.dec[II])
-    #     Tnew.bx[II] = x-1.
-    #     Tnew.by[II] = y-1.
-    #     Tnew.brick_primary[II] = ((Tnew.ra[II] >= brick.ra1 ) * (Tnew.ra[II] < brick.ra2) *
-    #                               (Tnew.dec[II] >= brick.dec1) * (Tnew.dec[II] < brick.dec2))
 
     outfn = fn.replace('/global/project/projectdirs/cosmo/work/legacysurvey/dr8/decam/tractor/',
                        'patched/')
