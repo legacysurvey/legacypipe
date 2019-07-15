3dba07eaf453eb9962c3cb514102e914c639c16c
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-15 14:40:31 -0700

    Add runbrick-shifter-dr8-postfarm.sh

---
+++ doc/runbrick-shifter-dr8-postfarm.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Script for running the legacypipe code within a Shifter container at NERSC
+# with burst buffer!
+
+# Burst-buffer!
+if [ x$DW_PERSISTENT_STRIPED_DR8 == x ]; then
+  # No burst buffer -- use scratch
+  if [ "$NERSC_HOST" = "edison" ]; then
+     BB=${SCRATCH}/
+  else
+     BB=${CSCRATCH}/
+  fi
+else
+  # Use "DR8" burst buffer.
+  BB=$DW_PERSISTENT_STRIPED_DR8
+fi
+outdir=${BB}farm-post-prod
+
+if [ "$NERSC_HOST" = "edison" ]; then
+    export LEGACY_SURVEY_DIR=/scratch1/scratchdirs/desiproc/dr8
+else
+    export LEGACY_SURVEY_DIR=/global/cscratch1/sd/landriau/dr8
+fi
+
+export DUST_DIR=/global/project/projectdirs/cosmo/data/dust/v0_1
+export UNWISE_COADDS_DIR=/global/project/projectdirs/cosmo/work/wise/outputs/merge/neo4/fulldepth:/global/project/projectdirs/cosmo/data/unwise/allwise/unwise-coadds/fulldepth
+export UNWISE_COADDS_TIMERESOLVED_DIR=/global/projecta/projectdirs/cosmo/work/wise/outputs/merge/neo4
+export GAIA_CAT_DIR=/global/project/projectdirs/cosmo/work/gaia/chunks-gaia-dr2-astrom-2
+export GAIA_CAT_VER=2
+export TYCHO2_KD_DIR=/global/project/projectdirs/cosmo/staging/tycho2
+export LARGEGALAXIES_DIR=/global/project/projectdirs/cosmo/staging/largegalaxies/v2.0
+export PS1CAT_DIR=/global/project/projectdirs/cosmo/work/ps1/cats/chunks-qz-star-v3
+
+# Use the unwise_psf version inside the container
+UNWISE_PSF_DIR=/src/unwise_psf
+export WISE_PSF_DIR=${UNWISE_PSF_DIR}/etc
+
+export PYTHONPATH=/usr/local/lib/python:/usr/local/lib/python3.6/dist-packages:.:${UNWISE_PSF_DIR}/py
+
+# Don't add ~/.local/ to Python's sys.path
+export PYTHONNOUSERSITE=1
+
+# Force MKL single-threaded
+# https://software.intel.com/en-us/articles/using-threaded-intel-mkl-in-multi-thread-application
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+# To avoid problems with MPI and Python multiprocessing
+export MPICH_GNI_FORK_MODE=FULLCOPY
+export KMP_AFFINITY=disabled
+
+# Limit memory to avoid killing the whole MPI job...
+ncores=8
+if [ -n "$(grep avx512 /proc/cpuinfo)" ]; then
+    # 64 GB / Edison node = 67108864 kbytes
+    # 96 GB / KNL node >= 95839232 KB
+    maxmem=88839232
+    let usemem=${maxmem}*${ncores}/68
+else
+    # 128 GB / Cori Haswell node = 134217728 kbytes
+    maxmem=100217728
+    let usemem=${maxmem}*${ncores}/64
+
+    # Can detect Cori KNL node (96 GB) via:
+    # grep -q "Xeon Phi" /proc/cpuinfo && echo Yes
+fi
+ulimit -Sv $usemem
+
+cd /src/legacypipe/py
+
+brick="$1"
+
+bri=$(echo $brick | head -c 3)
+mkdir -p $outdir/logs/$bri
+log="$outdir/logs/$bri/$brick.log"
+
+mkdir -p $outdir/metrics/$bri
+
+echo Logging to: $log
+echo Running on $(hostname)
+
+echo -e "\n\n\n" >> $log
+echo "-----------------------------------------------------------------------------------------" >> $log
+echo "PWD: $(pwd)" >> $log
+echo >> $log
+ulimit -a >> $log
+echo >> $log
+
+echo -e "\nStarting on $(hostname)\n" >> $log
+echo "-----------------------------------------------------------------------------------------" >> $log
+
+python -O legacypipe/runbrick.py \
+     --brick $brick \
+     --skip \
+     --skip-calibs \
+     --threads ${ncores} \
+     --checkpoint "/global/cscratch1/sd/ziyaoz/farm-checkpoints-prod-pt4/checkpoint-${1}.pickle" \
+     --pickle "/global/cscratch1/sd/landriau/dr8/decam-des/pickles/%(brick).3s/runbrick-%(brick)s-%%(stage)s.pickle" \
+     --unwise-coadds \
+     --outdir $outdir \
+     --ps "${outdir}/metrics/${bri}/ps-${brick}-${SLURM_JOB_ID}.fits" \
+     --ps-t0 $(date "+%s") \
+     --write-stage srcs \
+     --run decam \
+     >> $log 2>&1
+
+# Need to add either:
+# --run 90prime-mosaic
+# --run decam
+
+
+# QDO_BATCH_PROFILE=cori-shifter qdo launch -v tst 1 --cores_per_worker 8 --walltime=30:00 --batchqueue=debug --keep_env --batchopts "--image=docker:dstndstn/legacypipe:intel" --script "/src/legacypipe/bin/runbrick-shifter.sh"
