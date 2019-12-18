9f20d664b2d16471f1e14ba98dd1569322fdc78d
Dustin Lang <dstndstn@gmail.com>
2019-12-18 09:44:51 -0800

    drop read_primary_header & non-working warning filters

--- py/legacypipe/utils.py
+++ py/legacypipe/utils.py
@@ -158,49 +158,9 @@ def find_unique_pixels(wcs, W, H, unique, ra1,ra2,dec1,dec2):
     return unique
 
 def read_primary_header(fn):
-    '''
-    Reads the FITS primary header (HDU 0) from the given filename.
-    This is just a faster version of fitsio.read_header(fn).
-    '''
+    # fitsio 1.0.1 sped up header-reading, so we don't need to do it ourselves any more.
     import fitsio
-    if fn.endswith('.gz'):
-        return fitsio.read_header(fn)
-
-    # Weirdly, this can be MUCH faster than letting fitsio do it...
-    hdr = fitsio.FITSHDR()
-    foundEnd = False
-    ff = open(fn, 'rb')
-    h = b''
-    while True:
-        hnew = ff.read(32768)
-        if len(hnew) == 0:
-            # EOF
-            ff.close()
-            raise RuntimeError('Reached end-of-file in "%s" before finding end of FITS header.' % fn)
-        h = h + hnew
-        while True:
-            line = h[:80]
-            h = h[80:]
-            #print('Header line "%s"' % line)
-            # HACK -- fitsio apparently can't handle CONTINUE.
-            # It also has issues with slightly malformed cards, like
-            # KEYWORD  =      / no value
-            if line[:8] != b'CONTINUE':
-                try:
-                    hdr.add_record(line.decode())
-                except OSError as err:
-                    print('Warning: failed to parse FITS header line: ' +
-                          ('"%s"; error "%s"; skipped' % (line.strip(), str(err))))
-
-            if line == (b'END' + b' '*77):
-                foundEnd = True
-                break
-            if len(h) < 80:
-                break
-        if foundEnd:
-            break
-    ff.close()
-    return hdr
+    return fitsio.read_header(fn)
 
 def run_ps_thread(parent_pid, parent_ppid, fn, shutdown, event_queue):
     from astrometry.util.run_command import run_command
