f593a35f25cb8304028e80aad1c8e6ebb282cba5
Rongpu Zhou <rongpu.zhou@gmail.com>
2020-03-04 15:53:41 -0800

    update the psfex patching script to handle errors

--- py/legacyzpts/modify_psfex_profiles.py
+++ py/legacyzpts/modify_psfex_profiles.py
@@ -42,10 +42,10 @@ def get_sb_double_moffat(r, alpha1, beta1, alpha2, beta2, weight2):
     return i
 
 
-test_q = True  # only process a small number of exposures
+test_q = False  # only process a small number of exposures
 
-output_dir = '/global/project/projectdirs/desi/users/rongpu/dr9/new-psfex-v2'
-surveyccd_path = '/global/project/projectdirs/cosmo/work/legacysurvey/dr9/survey-ccds-decam-dr9-cut.fits.gz'
+output_dir = '/global/cfs/projectdirs/cosmo/work/legacysurvey/dr9/calib/patched-psfex'
+surveyccd_path = '/global/cfs/projectdirs/cosmo/work/legacysurvey/dr9/survey-ccds-decam-dr9-cut.fits.gz'
 
 radius_lim1, radius_lim2 = 5.0, 6.0
 radius_lim3, radius_lim4 = 7., 8.
@@ -63,12 +63,14 @@ pixscale = 0.262
 ccd = fitsio.read(surveyccd_path)
 ccd = Table(ccd)
 
+"""
 mask = ccd['ccd_cuts']==0
 print(np.sum(mask)/len(mask))
 ccd = ccd[mask]
+"""
 
 unique_expnum = np.unique(ccd['expnum'])
-print(len(unique_expnum))
+#print(len(unique_expnum))
 
 # randomly select exposures for testing
 if test_q:
@@ -83,29 +85,30 @@ for exp_index in exp_index_list:
 
     mask = ccd['expnum']==unique_expnum[exp_index]
     band = ccd['filter'][mask][0]
-    print('band = {}'.format(band))
+    #print('band = {}'.format(band))
 
     image_filename = ccd['image_filename'][mask][0]
     psfex_filename = image_filename[:image_filename.find('.fits.fz')]+'-psfex.fits'
-    psfex_filename_new = image_filename[:image_filename.find('.fits.fz')]+'-psfex-patched.fits'
-    psfex_path = os.path.join('/project/projectdirs/cosmo/work/legacysurvey/dr9/calib', psfex_filename)
+    psfex_filename_new = image_filename[:image_filename.find('.fits.fz')]+'-psfex.fits'
+    psfex_path = os.path.join('/global/cfs/projectdirs/cosmo/work/legacysurvey/dr9/calib/psfex', psfex_filename)
 
     output_path = os.path.join(output_dir, psfex_filename_new)
     if os.path.isfile(output_path):
-        raise ValueError
-        # continue
+        #raise ValueError
+        continue
 
     hdu = fits.open(psfex_path)
     data = Table(hdu[1].data)
-    print(len(data))
+    #print(len(data))
 
-    data['psf_patch_ver'] = 'psf_patch_ver'
+    data['psf_patch_ver'] = 'd683d99'
     data['moffat_alpha'] = 0.
     data['moffat_beta'] = 0.
     # sum of the difference between the original and new PSF model (first eigen-image)
     data['sum_diff'] = 0.
     # the factor from fitting the new PSF with the original one; should be close to unity
     data['fit_original'] = 0.
+    data['failure'] = False
 
     # loop over the CCDs in each exposure (note: not all CCDs meet ccd_cuts)
     for ccd_index in range(len(data)):
@@ -130,6 +133,11 @@ for exp_index in exp_index_list:
         psfi_original = data['psf_mask'][ccd_index, psf_index].copy()
         # normalize to a 22.5 magnitude star
         norm_factor = np.sum(psfi_original)
+
+        # catch problematic psfex images
+        if norm_factor==0 or np.isnan(norm_factor) or np.isinf(norm_factor):
+            norm_factor = 1.
+
         psfi = psfi_original/norm_factor
 
         # vrange = 0.0002
@@ -150,10 +158,16 @@ for exp_index in exp_index_list:
             warnings.simplefilter("ignore")
             # popt, pcov =  curve_fit(get_sb_moffat, radius, psfi_flat/(pixscale**2))
             mask = (radius>radius_min) & (radius<radius_max)
-            popt, pcov = curve_fit(get_sb_moffat, radius[mask], psfi_flat[mask]/(pixscale**2), bounds=((0, 1.8), np.inf))
-
-        alpha, beta = popt
-        print('{} {} alpha, beta = {:.3f}, {:.3f}'.format(ccdname, band, alpha, beta))
+            try:
+                popt, pcov = curve_fit(get_sb_moffat, radius[mask], psfi_flat[mask]/(pixscale**2), bounds=((0, 1.8), np.inf))
+                alpha, beta = popt
+            except:
+                print("Error: "+image_filename+", "+ccdname+".")
+                print("Error: fit failed to converge.")
+                alpha, beta = 0.8, 2.2 # using default values
+                data['failure'][ccd_index] = True
+                
+        #print('{} {} alpha, beta = {:.3f}, {:.3f}'.format(ccdname, band, alpha, beta))
 
         # save the Moffat parameters
         data['moffat_alpha'][ccd_index] = alpha
