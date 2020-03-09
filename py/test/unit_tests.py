3e8343a2e038b1e9fae0d8f8224a0377e6d1cfe8
Dustin Lang <dstndstn@gmail.com>
2020-03-09 13:34:38 -0400

    oops, fix unit tests for oneblob ptsrc->psf renaming

--- py/test/unit_tests.py
+++ py/test/unit_tests.py
@@ -5,55 +5,55 @@ class TestOneblob(unittest.TestCase):
     def test_modelsel(self):
         from legacypipe.oneblob import _select_model
 
-        nparams = dict(ptsrc=2, rex=3, exp=5, dev=5, comp=9)
-        galaxy_margin = 3.**2 + (nparams['exp'] - nparams['ptsrc'])
+        nparams = dict(psf=2, rex=3, exp=5, dev=5, comp=9)
+        galaxy_margin = 3.**2 + (nparams['exp'] - nparams['psf'])
         rex = True
 
-        chisqs = dict(ptsrc=0, rex=0)
+        chisqs = dict(psf=0, rex=0)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'none')
 
-        chisqs = dict(ptsrc=500, rex=0)
+        chisqs = dict(psf=500, rex=0)
         mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'ptsrc')
+        self.assertTrue(mod == 'psf')
 
-        chisqs = dict(ptsrc=0, rex=500)
-        mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'rex')
-
-        chisqs = dict(ptsrc=500, rex=501)
-        mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'ptsrc')
-
-        chisqs = dict(ptsrc=500, rex=503)
-        mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'ptsrc')
-
-        chisqs = dict(ptsrc=500, rex=510)
+        chisqs = dict(psf=0, rex=500)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'rex')
 
-        chisqs = dict(ptsrc=500, rex=504, exp=505)
+        chisqs = dict(psf=500, rex=501)
         mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'ptsrc')
+        self.assertTrue(mod == 'psf')
 
-        chisqs = dict(ptsrc=500, rex=505, exp=505)
+        chisqs = dict(psf=500, rex=503)
+        mod = _select_model(chisqs, nparams, galaxy_margin)
+        self.assertTrue(mod == 'psf')
+
+        chisqs = dict(psf=500, rex=510)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'rex')
 
-        chisqs = dict(ptsrc=500, rex=505, exp=520)
+        chisqs = dict(psf=500, rex=504, exp=505)
+        mod = _select_model(chisqs, nparams, galaxy_margin)
+        self.assertTrue(mod == 'psf')
+
+        chisqs = dict(psf=500, rex=505, exp=505)
+        mod = _select_model(chisqs, nparams, galaxy_margin)
+        self.assertTrue(mod == 'rex')
+
+        chisqs = dict(psf=500, rex=505, exp=520)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'exp')
 
-        chisqs = dict(ptsrc=5000, rex=5005, exp=5020)
+        chisqs = dict(psf=5000, rex=5005, exp=5020)
         mod = _select_model(chisqs, nparams, galaxy_margin)
-        self.assertTrue(mod == 'ptsrc')
+        self.assertTrue(mod == 'psf')
 
-        chisqs = dict(ptsrc=5000, rex=5005, exp=5051)
+        chisqs = dict(psf=5000, rex=5005, exp=5051)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'exp')
 
-        chisqs = dict(ptsrc=5000, rex=5005, exp=5051, dev=5052)
+        chisqs = dict(psf=5000, rex=5005, exp=5051, dev=5052)
         mod = _select_model(chisqs, nparams, galaxy_margin)
         self.assertTrue(mod == 'dev')
 
