47442502e5ef20f544e630bef875a6f95e3f764e
John Moustakas <john.moustakas@gmail.com>
2020-03-10 10:29:25 -0700

    use the full ccd footprint

--- py/legacypipe/largegalaxies.py
+++ py/legacypipe/largegalaxies.py
@@ -90,50 +90,95 @@ def get_sky(survey, targetwcs, tim):
     overlapimg[Yo, Xo] = skymedian
 
     return overlapimg
-        
-def stage_largegalaxies(
-        survey=None, targetwcs=None, bands=None, tims=None,
-        brickname=None, version_header=None,
-        apodize=True,
-        plots=False, ps=None, coadd_bw=False, W=None, H=None,
-        brick=None, blobs=None, lanczos=True, ccds=None,
-        write_metrics=True,
-        mp=None, record_event=None,
-        **kwargs):
 
+def largegalaxy_sky(tims, targetwcs, survey, brickname):
+    
     from astrometry.util.starutil_numpy import degrees_between
-
-    from legacypipe.coadds import make_coadds, write_coadd_images
-    from legacypipe.bits import DQ_BITS
-    from legacypipe.survey import get_rgb, imsave_jpeg, LegacySurveyWcs
-
-    from tractor.image import Image
-    from tractor.basics import NanoMaggies, LinearPhotoCal
-    from tractor.sky import ConstantSky
-    from tractor.psf import PixelizedPSF
-    from tractor.tractortime import TAITime
-    import astropy.time
-    import fitsio
-
     from astrometry.util.resample import resample_with_wcs
     from legacypipe.reference import get_reference_sources
     from legacypipe.oneblob import get_inblob_map
-    
-    # Custom sky-subtraction.
+
+    if False:
+        import os
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as patches
+
+        refs, _ = get_reference_sources(survey, targetwcs, targetwcs.pixel_scale(), ['r'],
+                                        tycho_stars=False, gaia_stars=False,
+                                        large_galaxies=True, star_clusters=False)
+        
+        pixscale = targetwcs.pixel_scale()
+        width, height = targetwcs.get_width() * pixscale / 3600, targetwcs.get_height() * pixscale / 3600 # [degrees]
+        bb, bbcc = targetwcs.radec_bounds(), targetwcs.radec_center() # [degrees]
+        pad = 0.5 * width # [degrees]
+
+        delta = np.max( (np.diff(bb[0:2]), np.diff(bb[2:4])) ) / 2 + pad / 2
+        xlim = bbcc[0] - delta, bbcc[0] + delta
+        ylim = bbcc[1] - delta, bbcc[1] + delta
+
+        fig, allax = plt.subplots(1, 3, figsize=(12, 5), sharey=True, sharex=True)
+        for ax, band in zip(allax, ('g', 'r', 'z')):
+            ax.set_xlabel('RA (deg)')
+            ax.text(0.9, 0.05, band, ha='center', va='bottom',
+                    transform=ax.transAxes, fontsize=18)
+
+            if band == 'g':
+                ax.set_ylabel('Dec (deg)')
+            ax.get_xaxis().get_major_formatter().set_useOffset(False)
+            for gal in refs:
+                ax.add_patch(patches.Circle((gal.ra, gal.dec), gal.radius, fill=False, edgecolor='black', lw=2))
+                #ax.add_patch(patches.Circle((bbcc[0], bbcc[1]), 2*radius * pixscale / 3600, # inner sky annulus
+                #                            fill=False, edgecolor='black', lw=1))
+                #ax.add_patch(patches.Circle((bbcc[0], bbcc[1]), 5*radius * pixscale / 3600, # outer sky annulus
+                #                            fill=False, edgecolor='black', lw=1))
+
+            these = np.where([tim.band == band for tim in tims])[0]
+            col = plt.cm.Set1(np.linspace(0, 1, len(tims)))
+            for ii, indx in enumerate(these):
+                tim = tims[indx]
+                #wcs = tim.imobj.get_wcs()
+                wcs = tim.subwcs
+                cc = wcs.radec_bounds()
+                ax.add_patch(patches.Rectangle((cc[0], cc[2]), cc[1]-cc[0],
+                                               cc[3]-cc[2], fill=False, lw=2, 
+                                               edgecolor=col[these[ii]],
+                                               label='ccd{:02d}'.format(these[ii])))
+                ax.legend(ncol=2, frameon=False, loc='upper left', fontsize=10)
+
+            ax.set_ylim(ylim)
+            ax.set_xlim(xlim)
+            ax.invert_xaxis()
+            ax.set_aspect('equal')
+            #print(ax.get_xlim(), ax.get_ylim())
+
+        plt.subplots_adjust(bottom=0.12, wspace=0.05, left=0.12, right=0.97, top=0.95)
+        pngfile = survey.write_output('image-jpeg', brick=brickname).fn.replace('.jpg', '-ccdpos.jpg')
+        os.rename(pngfile, pngfile.replace('tmp-', ''))
+        print('Writing {}'.format(pngfile))
+        fig.savefig(pngfile)
+        plt.close(fig)
+        
     #overlapimg = np.stack(mp.map(_get_sky, [(survey, targetwcs, tim) for tim in tims]))
 
     # Add the original sky background back into the tim.
+    #fullimg, fullwcs = [], []
+    fullwcs = []
     for tim in tims:
-        img = tim.getImage()
-        origsky = np.zeros_like(img)
-        tim.origsky.addTo(origsky)
-        tim.setImage(img + origsky)
+        #img = tim.getImage()
+        #origsky = np.zeros_like(img)
+        #tim.origsky.addTo(origsky)
+        #tim.setImage(img + origsky)
+        #fullimg.append(tim.imobj.read_image())
+        fullwcs.append(tim.imobj.get_wcs())
+        #tims[0].imobj.read_sky_model()
 
-    radec = np.array([tim.subwcs.crval for tim in tims])
+    radec = np.array([wcs.crval for wcs in fullwcs])
+    #radec = np.array([tim.subwcs.crval for tim in tims])
     ddeg = degrees_between(radec[:, 0], radec[:, 1], targetwcs.crval[0], targetwcs.crval[1])
     
     allbands = np.array([tim.band for tim in tims])
     for band in sorted(set(allbands)):
+        print('Working on band {}'.format(band))
         I = np.where(allbands == band)[0]
         nn = len(I)
 
@@ -143,14 +188,45 @@ def stage_largegalaxies(
         for ii in indx:
             for jj in indx[ii:]:
                 try:
-                    Yo, Xo, Yi, Xi, _ = resample_with_wcs(tims[I[ii]].subwcs, tims[I[jj]].subwcs)
+                    Yo, Xo, Yi, Xi, _ = resample_with_wcs(fullwcs[I[ii]], fullwcs[I[jj]])
+                    #Yo, Xo, Yi, Xi, _ = resample_with_wcs(tims[I[ii]].subwcs, tims[I[jj]].subwcs)
                     noverlap[jj, ii] = len(Yo)
                     #print(ii, jj, len(Yo), len(Xo))
                 except:
                     pass
 
+        print(noverlap)
         # Work from the inside out.
         JJ = np.argsort(ddeg[I])
+
+        for J in JJ:
+            # Sort the images that overlap with this image by increasing overlap
+            # (but skip the image itself).
+            print(noverlap[J, :])
+            KK = np.argsort(noverlap[J, :])[::-1]
+            KK = KK[noverlap[J, KK] > 0]
+            for K in KK:
+                try:
+                    YJ, XJ, YK, XK, _ = resample_with_wcs(fullwcs[I[J]], fullwcs[I[K]])
+                    #Yo, Xo, Yi, Xi, _ = resample_with_wcs(tims[I[J]].subwcs, tims[I[K]].subwcs)
+                except:
+                    import pdb ; pdb.set_trace()
+
+                #slcJ = slice(YJ.min(), YJ.max() + 1), slice(XJ.min(), XJ.max() + 1)
+                #slcK = slice(YK.min(), YK.max() + 1), slice(XK.min(), XK.max() + 1)
+
+                imgK = tims[I[K]].imobj.read_image()[YK, XK]
+                imgJ = tims[I[J]].imobj.read_image()[YJ, XJ]
+                invJ = tims[I[J]].imobj.read_invvar()[YJ, XJ]
+                
+                #imgK = tims[I[K]].imobj.read_image(slc=slcK) # [Yi, Xi]
+                #imgJ = tims[I[J]].imobj.read_image(slc=slcJ) # [Yo, Xo]
+                #invJ = tims[I[J]].imobj.read_invvar(slc=slcJ) # [Yo, Xo]
+                
+                delta = np.sum(invJ * (imgJ - imgK)) / np.sum(invJ)
+                #delta = np.sum(tims[I[J]].getInvvar()[Yo, Xo]*(tims[I[J]].getImage()[Yo, Xo] - tims[I[K]].getImage()[Yi, Xi])) / np.sum(tims[I[J]].getInvvar()[Yo, Xo])
+                print(J, K, noverlap[J, K], delta)
+                tims[I[K]].setImage(tims[I[K]].getImage() + delta)
 
         # Get the median sky background from the CCD that's furthest from the
         # center of the field.
@@ -160,30 +236,16 @@ def stage_largegalaxies(
         imh, imw = skytim.imobj.get_image_shape()
         refsky = np.zeros((imh, imw)).astype('f4')
         skyimg.addTo(refsky)
-        
+
         refs, _ = get_reference_sources(survey, skywcs, skywcs.pixel_scale(), ['r'],
                                         tycho_stars=True, gaia_stars=True,
                                         large_galaxies=True, star_clusters=True)
         skymask = get_inblob_map(skywcs, refs) != 0 # True=unmasked
         medsky = np.median(refsky[skymask])
-        
-        for J in JJ:
-            # Sort the images that overlap with this image by increasing overlap
-            # (but skip the image itself).
-            #print(noverlap[J, :])
-            KK = np.argsort(noverlap[J, :])[::-1]
-            KK = KK[noverlap[J, KK] > 0]
-            for K in KK:
-                try:
-                    Yo, Xo, Yi, Xi, _ = resample_with_wcs(tims[I[J]].subwcs, tims[I[K]].subwcs)
-                except:
-                    import pdb ; pdb.set_trace()
-                delta = np.sum(tims[I[J]].getInvvar()[Yo, Xo]*(tims[I[J]].getImage()[Yo, Xo] - tims[I[K]].getImage()[Yi, Xi])) / np.sum(tims[I[J]].getInvvar()[Yo, Xo])
-                print(J, K, delta, medsky)
-                tims[I[K]].setImage(tims[I[K]].getImage() + delta - medsky)
-                
-        #import pdb ; pdb.set_trace()
 
+        for J in JJ:
+            tims[I[J]].setImage(tims[I[J]].getImage() - medsky)
+            
     #    # Choose the reference image, read the full tim, and render the original
     #    # sky image.
     #    indx = np.argsort(radec[I][:, 0])
@@ -216,7 +278,35 @@ def stage_largegalaxies(
     #        ff.write(overlapimg[w, :, :])
 
     #import pdb ; pdb.set_trace()
-       
+
+    return tims
+
+def stage_largegalaxies(
+        survey=None, targetwcs=None, bands=None, tims=None,
+        brickname=None, version_header=None,
+        apodize=True,
+        plots=False, ps=None, coadd_bw=False, W=None, H=None,
+        brick=None, blobs=None, lanczos=True, ccds=None,
+        write_metrics=True,
+        mp=None, record_event=None,
+        **kwargs):
+
+    from legacypipe.coadds import make_coadds, write_coadd_images
+    from legacypipe.bits import DQ_BITS
+    from legacypipe.survey import get_rgb, imsave_jpeg, LegacySurveyWcs
+
+    from tractor.image import Image
+    from tractor.basics import NanoMaggies, LinearPhotoCal
+    from tractor.sky import ConstantSky
+    from tractor.psf import PixelizedPSF
+    from tractor.tractortime import TAITime
+    import astropy.time
+    import fitsio
+
+    # Custom sky-subtraction for large galaxies.
+    tims = largegalaxy_sky(tims, targetwcs, survey, brickname)
+    #import pdb ; pdb.set_trace()
+    
     # Create coadds and then build custom tims from them.
 
     # We tried setting the invvars constant per tim -- this makes things worse, since we *remove*
