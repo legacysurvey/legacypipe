780b34d4cba937518c32527eef38ad9ffb9a2390
Dustin Lang <dstndstn@gmail.com>
2020-03-27 11:56:35 -0700

    add test case with DUP gaia star & LSLGA galaxy

--- py/legacypipe/runbrick.py
+++ py/legacypipe/runbrick.py
@@ -691,7 +691,7 @@ def stage_srcs(targetrd=None, pixscale=None, targetwcs=None,
         SEDs, bands, detmaps, detivs, (avoid_x,avoid_y,avoid_r), targetwcs,
         nsigma=nsigma, saddle_fraction=saddle_fraction, saddle_min=saddle_min,
         saturated_pix=saturated_pix, plots=plots, ps=ps, mp=mp)
-    if Tnew is None and not bailout_sources:
+    if Tnew is None:
         raise NothingToDoError('No sources detected.')
     assert(len(Tnew) == len(newcat))
     if Tnew is not None:
