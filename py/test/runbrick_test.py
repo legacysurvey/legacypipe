bf730ca2f6fe6369b8abebb6d24fabb0d67acf63
Dustin Lang <dstndstn@gmail.com>
2020-04-08 15:55:09 -0400

    remove 'extra_args' from test cases; add 'ceres' option

--- py/test/runbrick_test.py
+++ py/test/runbrick_test.py
@@ -13,14 +13,11 @@ from astrometry.util.fits import fits_table
 
 def rbmain():
     travis = 'travis' in sys.argv
-    psfex = 'psfex' in sys.argv
+    ceres  = 'ceres'  in sys.argv
+    psfex  = 'psfex'  in sys.argv
     
-    extra_args = ['--old-calibs-ok',
-    #'--verbose',
-        ]
-    if travis:
-        extra_args.extend(['--no-wise-ceres', '--no-gaia', '--no-large-galaxies'])
-
+    if 'LARGEGALAXIES_CAT' in os.environ:
+        del os.environ['LARGEGALAXIES_CAT']
 
     surveydir = os.path.join(os.path.dirname(__file__), 'testcase12')
     os.environ['GAIA_CAT_DIR'] = os.path.join(surveydir, 'gaia')
@@ -114,14 +111,13 @@ def rbmain():
     del os.environ['GAIA_CAT_VER']
     del os.environ['LARGEGALAXIES_CAT']
     
-    if 'ceres' in sys.argv:
-        surveydir = os.path.join(os.path.dirname(__file__), 'testcase3')
-        main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
-                   '--no-wise', '--force-all', '--no-write', '--ceres',
-                   '--survey-dir', surveydir,
-                   '--outdir', 'out-testcase3-ceres',
-                   '--no-depth-cut'])
-        sys.exit(0)
+    # if ceres:
+    #     surveydir = os.path.join(os.path.dirname(__file__), 'testcase3')
+    #     main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
+    #                '--no-wise', '--force-all', '--no-write', '--ceres',
+    #                '--survey-dir', surveydir,
+    #                '--outdir', 'out-testcase3-ceres',
+    #                '--no-depth-cut'])
     
     # demo RexGalaxy, with plots
     if False:
@@ -164,11 +160,10 @@ def rbmain():
     surveydir2 = os.path.join(os.path.dirname(__file__), 'mzlsbass2')
     os.environ['GAIA_CAT_DIR'] = os.path.join(surveydir2, 'gaia')
     os.environ['GAIA_CAT_VER'] = '2'
-    my_extra_args = [a for a in extra_args if a != '--no-gaia']
     main(args=['--brick', '1773p595', '--zoom', '1300', '1500', '700', '900',
                '--no-wise', '--force-all', '--no-write',
                '--survey-dir', surveydir2,
-               '--outdir', 'out-mzlsbass2'] + my_extra_args)
+               '--outdir', 'out-mzlsbass2'])
 
     T = fits_table('out-mzlsbass2/tractor/177/tractor-1773p595.fits')
     assert(np.sum(T.ref_cat == 'G2') == 3)
@@ -186,7 +181,7 @@ def rbmain():
                '--survey-dir', surveydir2,
                '--outdir', outdir,
                '--checkpoint', chk,
-               '--nblobs', '3'] + my_extra_args)
+               '--nblobs', '3'])
     # err... --max-blobsize does not result in bailed-out blobs masked,
     # because it treats large blobs as *completed*...
     #'--max-blobsize', '3000',
@@ -196,8 +191,7 @@ def rbmain():
                '--no-wise', '--force-all',
                '--survey-dir', surveydir2,
                '--outdir', outdir, '--bail-out', '--checkpoint', chk,
-               '--no-write'] +
-               my_extra_args)
+               '--no-write'])
 
     del os.environ['GAIA_CAT_DIR']
     del os.environ['GAIA_CAT_VER']
@@ -215,7 +209,7 @@ def rbmain():
                 '--skip-calibs',
     #'--rex', #'--plots',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + extra_args
+               '--outdir', outdir]
     print('python legacypipe/runbrick.py', ' '.join(the_args))
     os.environ['GAIA_CAT_DIR'] = os.path.join(surveydir, 'gaia')
     os.environ['GAIA_CAT_VER'] = '2'
@@ -240,7 +234,7 @@ def rbmain():
     main(args=['--brick', '1102p240', '--zoom', '500', '600', '650', '750',
                '--force-all', '--no-write', '--no-wise',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + extra_args)
+               '--outdir', outdir])
     fn = os.path.join(outdir, 'tractor', '110', 'tractor-1102p240.fits')
     assert(os.path.exists(fn))
     T = fits_table(fn)
@@ -274,10 +268,19 @@ def rbmain():
                '--unwise-coadds',
                '--blob-image', '--no-hybrid-psf',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + extra_args + ['-v'])
+               '--outdir', outdir, '-v', '--no-wise-ceres'])
     print('Checking for calib file', fn)
     assert(os.path.exists(fn))
 
+    if ceres:    
+        main(args=['--brick', '1867p255', '--zoom', '2050', '2300', '1150', '1400',
+                   '--force-all', '--no-write', '--coadd-bw',
+                   '--unwise-dir', os.path.join(surveydir, 'images', 'unwise'),
+                   '--unwise-tr-dir', os.path.join(surveydir,'images','unwise-tr'),
+                   '--unwise-coadds',
+                   '--survey-dir', surveydir,
+                   '--outdir', outdir])
+    
     if psfex:
         # Check that we can regenerate PsfEx files if necessary.
         fn = os.path.join(surveydir, 'calib', 'psfex', 'decam', 'CP', 'V4.8.2',
@@ -292,7 +295,7 @@ def rbmain():
                    '--unwise-coadds',
                    '--blob-image',
                    '--survey-dir', surveydir,
-                   '--outdir', outdir] + extra_args + ['-v'])
+                   '--outdir', outdir, '-v'])
         print('After generating PsfEx calib:')
         os.system('find %s' % (os.path.join(surveydir, 'calib')))
 
@@ -306,20 +309,18 @@ def rbmain():
     main(args=['--brick', '1209p050', '--zoom', '720', '1095', '3220', '3500',
                '--force-all', '--no-write', '--no-wise', #'--plots',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + extra_args)
+               '--outdir', outdir])
     
     # Test with a Tycho-2 star + another saturated star in the blob.
 
     surveydir = os.path.join(os.path.dirname(__file__), 'testcase7')
     outdir = 'out-testcase7'
-    # remove --no-gaia
-    my_extra_args = [a for a in extra_args if a != '--no-gaia']
     os.environ['GAIA_CAT_DIR'] = os.path.join(surveydir, 'gaia')
     os.environ['GAIA_CAT_VER'] = '2'
     main(args=['--brick', '1102p240', '--zoom', '250', '350', '1550', '1650',
                '--force-all', '--no-write', '--no-wise', #'--plots',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + my_extra_args)
+               '--outdir', outdir])
     del os.environ['GAIA_CAT_DIR']
     del os.environ['GAIA_CAT_VER']
     fn = os.path.join(outdir, 'tractor', '110', 'tractor-1102p240.fits')
@@ -360,7 +361,7 @@ def rbmain():
                '--force-all', '--no-write', '--no-wise',
                '--blobradec', '186.740369', '25.453855',
                '--survey-dir', surveydir,
-               '--outdir', outdir] + extra_args)
+               '--outdir', outdir])
 
     assert(os.path.exists(fn))
     T = fits_table(fn)
@@ -373,14 +374,13 @@ def rbmain():
     checkpoint_fn = os.path.join(outdir, 'checkpoint.pickle')
     if os.path.exists(checkpoint_fn):
         os.unlink(checkpoint_fn)
-    my_extra_args = [a for a in extra_args if a != '--no-gaia']
     main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
                '--no-wise', '--force-all', '--no-write',
                '--survey-dir', surveydir,
                '--outdir', outdir,
                '--checkpoint', checkpoint_fn,
                '--checkpoint-period', '1',
-               '--threads', '2'] + my_extra_args)
+               '--threads', '2'])
 
     # Read catalog into Tractor sources to test read_fits_catalog
     from legacypipe.catalog import read_fits_catalog
@@ -416,7 +416,7 @@ def rbmain():
                '--outdir', outdir,
                '--checkpoint', checkpoint_fn,
                '--checkpoint-period', '1',
-               '--threads', '2'] + extra_args)
+               '--threads', '2'])
     # Assert...... something?
 
     # Test --checkpoint without --threads
@@ -425,7 +425,7 @@ def rbmain():
                '--survey-dir', surveydir,
                '--outdir', outdir,
                '--checkpoint', checkpoint_fn,
-               '--checkpoint-period', '1' ] + extra_args)
+               '--checkpoint-period', '1' ])
     
     # From Kaylan's Bootes pre-DR4 run
     # surveydir2 = os.path.join(os.path.dirname(__file__), 'mzlsbass3')
@@ -439,7 +439,7 @@ def rbmain():
                '--no-wise', '--force-all', '--no-write',
                '--survey-dir', surveydir,
                '--outdir', 'out-testcase3', '--plots',
-               '--nblobs', '1'] + extra_args)
+               '--nblobs', '1'])
 
     # Decals Image Simulations
     # Uncomment WHEN galsim build for Travis
@@ -456,12 +456,12 @@ def rbmain():
     #for fn in ['image','model','resid','simscoadd']: 
     #    assert( os.path.exists(os.path.join(rt_dir,'qa-'+brick+'-star-'+fn+'-01.jpg')) )
 
-    if not travis:
-        # With ceres
-        main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
-                   '--no-wise', '--force-all', '--no-write', '--ceres',
-                   '--survey-dir', surveydir,
-                   '--outdir', 'out-testcase3-ceres'] + extra_args)
+    # if ceres:
+    #     # With ceres
+    #     main(args=['--brick', '2447p120', '--zoom', '1020', '1070', '2775', '2815',
+    #                '--no-wise', '--force-all', '--no-write', '--ceres',
+    #                '--survey-dir', surveydir,
+    #                '--outdir', 'out-testcase3-ceres'] + extra_args)
 
 if __name__ == '__main__':
     rbmain()
