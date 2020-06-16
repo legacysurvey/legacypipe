7d069604e5ac4994f7c8a1c0b9bc4e4666523001
John Moustakas <john.moustakas@gmail.com>
2020-06-16 04:21:05 -0700

    add an M33 special run

--- py/legacypipe/runs.py
+++ py/legacypipe/runs.py
@@ -18,11 +18,20 @@ class NinetyPrimeMosaic(LegacySurveyData):
     def get_default_release(self):
         return 9003
 
+class M33SurveyData(LegacySurveyData):
+    def filter_ccd_kd_files(self, fns):
+        return [fn for fn in fns if 'm33' in fn]
+    def filter_ccds_files(self, fns):
+        return [fn for fn in fns if 'm33' in fn]
+    def get_default_release(self):
+        return 9002
+
 runs = {
     'decam': DecamSurvey,
     '90prime-mosaic': NinetyPrimeMosaic,
     'south': DecamSurvey,
     'north': NinetyPrimeMosaic,
+    'm33': M33SurveyData,
     None: LegacySurveyData,
 }
 
