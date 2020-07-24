c5dc615955b3debf5b7458047a1d329605e5f51c
Eddie Schlafly <schlafly@hotmail.com>
2020-07-23 20:39:41 -0700

    Force hdu names to be upper-case, and strip any whitespace.

--- py/legacyzpts/legacy_zeropoints.py
+++ py/legacyzpts/legacy_zeropoints.py
@@ -1770,7 +1770,8 @@ class Mosaic3Measurer(Measurer):
         if debug:
             return ['CCD2']
         hdu = fitsio.FITS(fn)
-        extlist = [hdu[i].get_extname() for i in range(1,len(hdu))]
+        extlist = [hdu[i].get_extname().upper().strip()
+                   for i in range(1,len(hdu))]
         return extlist
 
     def get_expnum(self, primhdr):
@@ -1859,7 +1860,8 @@ class NinetyPrimeMeasurer(Measurer):
         if debug:
             return ['CCD1']
         hdu = fitsio.FITS(fn)
-        extlist = [hdu[i].get_extname() for i in range(1,len(hdu))]
+        extlist = [hdu[i].get_extname().upper().strip()
+                   for i in range(1,len(hdu))]
         return extlist
 
     def get_expnum(self, primhdr):
