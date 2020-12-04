79255c3094459407ab38c8ab225a7692fec37d50
Dustin Lang <dstndstn@gmail.com>
2020-12-04 14:19:42 -0800

    forced-phot: update queue-calibs and scripts

--- bin/forced-north.sh
+++ bin/forced-north.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 DIR=/global/cfs/cdirs/cosmo/work/legacysurvey/dr9m
+outdir=$CSCRATCH/dr9-forced
 
 # Don't add ~/.local/ to Python's sys.path
 export PYTHONNOUSERSITE=1
@@ -12,31 +13,34 @@ export MPICH_GNI_FORK_MODE=FULLCOPY
 
 camera=$1
 expnum=$2
-ccdname=$3
-outfn=$4
 
-exppre=$(echo $expnum | cut -c 1-3)
+exppre=$(printf %08d $expnum | cut -c 1-3)
 
-outdir=$CSCRATCH/dr9-forced
-
-logdir=$outdir/forced/logs/$camera-$exppre
+logdir=$outdir/logs/$camera/$exppre
 mkdir -p $logdir
-logfile=$logdir/$camera-$expnum-$ccdname.log
-
-cd /src/legacypipe/py
+logfile=$logdir/$expnum.log
 
 ncores=4
 # 128 GB / Cori Haswell node = 134217728 kbytes
-maxmem=134217728
-let usemem=${maxmem}*${ncores}/32
+#maxmem=134217728
+#let usemem=${maxmem}*${ncores}/32
 # Can detect Cori KNL node (96 GB) via:
 # grep -q "Xeon Phi" /proc/cpuinfo && echo Yes
 #ulimit -Sv $usemem
 
-
 echo "Logging to $logfile"
-python -u legacypipe/forced_photom.py --survey-dir $DIR --catalog-dir-north $DIR/north --catalog-dir-south $DIR/south --catalog-resolve-dec-ngc 32.375 \
-       --skip-calibs --apphot --derivs --camera $camera \
-       --threads $ncores \
-       $expnum $ccdname $outdir/$outfn > $logfile 2>&1
 
+python -O $LEGACYPIPE_DIR/legacypipe/forced_photom.py \
+       --survey-dir $DIR \
+       --catalog-dir-north $DIR/north \
+       --catalog-dir-south $DIR/south \
+       --catalog-resolve-dec-ngc 32.375 \
+       --skip-calibs \
+       --apphot \
+       --derivs \
+       --outlier-mask \
+       --camera $camera \
+       --expnum $expnum \
+       --out-dir $outdir \
+       --threads $ncores \
+       > $logfile 2>&1
