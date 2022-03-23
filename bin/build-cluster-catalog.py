f468eafb267c223b9648b493a8acfe1b9928e655
Eddie Schlafly <schlafly@hotmail.com>
2022-03-23 11:35:10 -0700

    Update star-clusters-supplemental to support pa/ba. Add LMC and SMC to star-clusters-supplemental. Fix build-cluster-catalog to reflect changes to source NGC catalog.

--- bin/build-cluster-catalog.py
+++ bin/build-cluster-catalog.py
@@ -19,22 +19,18 @@ from pkg_resources import resource_filename
 
 if not os.path.isfile('/tmp/NGC.csv'):
     os.system('wget -P /tmp https://raw.githubusercontent.com/mattiaverga/OpenNGC/master/NGC.csv')
-    
-names = ('name', 'type', 'ra_hms', 'dec_dms', 'const', 'majax', 'minax',
-         'pa', 'bmag', 'vmag', 'jmag', 'hmag', 'kmag', 'sbrightn', 'hubble',
-         'cstarumag', 'cstarbmag', 'cstarvmag', 'messier', 'ngc', 'ic',
-         'cstarnames', 'identifiers', 'commonnames', 'nednotes', 'ongcnotes')
-NGC = ascii.read('/tmp/NGC.csv', delimiter=';', names=names)
-NGC = NGC[(NGC['ra_hms'] != 'N/A')]
+
+NGC = ascii.read('/tmp/NGC.csv', delimiter=';')
+NGC = NGC[(NGC['RA'] != 'N/A')]
 
 ra, dec = [], []
-for _ra, _dec in zip(ma.getdata(NGC['ra_hms']), ma.getdata(NGC['dec_dms'])):
+for _ra, _dec in zip(ma.getdata(NGC['RA']), ma.getdata(NGC['Dec'])):
     ra.append(hmsstring2ra(_ra.replace('h', ':').replace('m', ':').replace('s','')))
     dec.append(dmsstring2dec(_dec.replace('d', ':').replace('m', ':').replace('s','')))
 NGC['ra'] = ra
 NGC['dec'] = dec
 
-objtype = np.char.strip(ma.getdata(NGC['type']))
+objtype = np.char.strip(ma.getdata(NGC['Type']))
 
 # Keep all globular clusters and planetary nebulae
 keeptype = ('PN', 'GCl')
@@ -48,12 +44,12 @@ clusters = NGC[keep]
 
 # Fill missing major axes with a nominal 0.4 arcmin (roughly works
 # for NGC7009, which is the only missing PN in the footprint).
-ma.set_fill_value(clusters['majax'], 0.4)
-clusters['majax'] = ma.filled(clusters['majax'].data)
+ma.set_fill_value(clusters['MajAx'], 0.4)
+clusters['MajAx'] = ma.filled(clusters['MajAx'].data)
 
 # Increase the radius of IC4593
 # https://github.com/legacysurvey/legacypipe/issues/347
-clusters[clusters['name'] == 'IC4593']['majax'] = 0.5
+clusters[clusters['Name'] == 'IC4593']['MajAx'] = 0.5
 
 #indesi = desimodel.footprint.is_point_in_desi(tiles, ma.getdata(clusters['ra']),
 #                                              ma.getdata(clusters['dec']))
@@ -64,13 +60,13 @@ clusters[clusters['name'] == 'IC4593']['majax'] = 0.5
 # Build the output catalog: select a subset of the columns and rename
 # majax-->radius (arcmin-->degree)
 out = Table()
-out['name'] = clusters['name']
+out['name'] = clusters['Name']
 out['alt_name'] = ['' if mm == 0 else 'M{}'.format(str(mm))
-                   for mm in ma.getdata(clusters['messier'])]
-out['type'] = clusters['type']
+                   for mm in ma.getdata(clusters['M'])]
+out['type'] = clusters['Type']
 out['ra'] = clusters['ra']
 out['dec'] = clusters['dec']
-out['radius'] = (clusters['majax'] / 60).astype('f4') # [degrees]
+out['radius'] = (clusters['MajAx'] / 60).astype('f4') # [degrees]
 #out['radius'] = out['radius_orig']
 
 # Read the updated radii based on visual inspection by Arjun Dey (Feb 2020):
@@ -93,37 +89,18 @@ if False:
     I, J, _ = match_radec(clusters['ra'], clusters['dec'], gcs['RA'], gcs['DEC'], 10./3600., nearest=True)
     out['radius'][I] = (gcs['HALF_LIGHT_RADIUS'][J] / 60).astype('f4') # [degrees]
 
-# Read the supplemental catalog of globular clusters and (compact) open clusters
-# from Arjun Dey (Mar 2020). Note that the NGC open clusters were culled above,
-# but we put them back here because the diameters have been vetted. 
-names = ('name', 'alt_name', 'ra', 'dec', 'type', 'radius')
-suppfile = resource_filename('legacypipe', 'data/star-clusters-supplemental.csv')
-supp = ascii.read(suppfile, delimiter=',', names=names, fill_values='')
-#supp['alt_name'] = supp['alt_name'].astype('U4')
-supp['radius'] = supp['radius'].astype('f4')
-out = vstack((out, supp))
-
 # add a position angle and ellipticity (b/a)
 out['pa'] = np.zeros(len(out), dtype='f4')
 out['ba'] = np.ones(len(out), dtype='f4')
 
-# add Fornax and Scluptor -- LG dwarfs by hand
-#  GALAXY         RA                DEC           D25     PA     BA
-# bytes24      float64            float64       float32 int16 float32
-# ------- ----------------- ------------------- ------- ----- -------
-#   Fornax 39.99708333333332 -34.449166666666656    66.4    41     0.7
-# Sculptor 15.039166666666665 -33.70916666666666    45.2    99    0.68
-
-dwarfs = Table()
-dwarfs['name'] = ['Sculptor', 'Fornax']
-dwarfs['alt_name'] = ['', '']
-dwarfs['type'] = ['Dwrf', 'Dwrf']
-dwarfs['ra'] = np.array([15.039166666666665, 39.99708333333332]).astype('f8')
-dwarfs['dec'] = np.array([-33.70916666666666, -34.449166666666656]).astype('f8')
-dwarfs['radius'] = np.array([45.2/2/60, 66.4/2/60]).astype('f4')
-dwarfs['pa'] = np.array([99, 41]).astype('f4')
-dwarfs['ba'] = np.array([0.68, 0.7]).astype('f4')
-out = vstack((out, dwarfs))
+# Read the supplemental catalog of globular clusters and (compact) open clusters
+# from Arjun Dey (Mar 2020). Note that the NGC open clusters were culled above,
+# but we put them back here because the diameters have been vetted.
+names = ('name', 'alt_name', 'ra', 'dec', 'type', 'radius', 'pa', 'ba')
+suppfile = resource_filename('legacypipe', 'data/star-clusters-supplemental.csv')
+supp = Table.read(suppfile, delimiter=',')
+supp['alt_name'] = supp['alt_name'].astype('U4')
+out = vstack((out, supp))
 
 if False: # debugging
     bb = out[['M' in nn for nn in out['alt_name']]]
@@ -149,4 +126,4 @@ if False:
     check_clusters = NGC[check] # 845 of them
 
     # Write out a catalog, load it into the viewer and look at each of them.
-    check_clusters[['ra', 'dec', 'name']].write('/tmp/check.fits', overwrite=True) # 25 of them
+    check_clusters[['ra', 'dec', 'Name']].write('/tmp/check.fits', overwrite=True) # 25 of them
