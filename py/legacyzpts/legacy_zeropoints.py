32f3a3f628bf911ff076c70bbe3f8a422e73b5df
Dustin Lang <dstndstn@gmail.com>
2023-02-22 14:53:20 -0800

    Omit CCD offset (eg good subregion) when converting fit x,y back to RA,Dec.

--- py/legacyzpts/legacy_zeropoints.py
+++ py/legacyzpts/legacy_zeropoints.py
@@ -1555,7 +1555,7 @@ def tractor_fit_sources(imobj, wcs, ref_ra, ref_dec, ref_flux, img, ierr,
     if noffim > 0:
         print('Off image for %d stars' % noffim)
     cal.to_np_arrays()
-    cal.ra_fit,cal.dec_fit = wcs.pixelxy2radec(cal.x_fit + 1, cal.y_fit + 1)
+    cal.ra_fit,cal.dec_fit = wcs.pixelxy2radec(cal.x_fit - ccd_x0 + 1, cal.y_fit - ccd_y0 + 1)
     return cal
 
 if __name__ == "__main__":
