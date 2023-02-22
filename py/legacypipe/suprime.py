96a7e1e4b7cbdd19e37e31e829be007f338c20a9
Dustin Lang <dstndstn@gmail.com>
2023-02-22 11:17:47 -0500

    suprime: cleanup

--- py/legacypipe/suprime.py
+++ py/legacypipe/suprime.py
@@ -11,6 +11,15 @@ def debug(*args):
     from legacypipe.utils import log_debug
     log_debug(logger, args)
 
+'''
+A class to handle images from the SuprimeCam camera (old prime-focus
+camera on the Subaru telescope), specifically a data set using
+intermediate-band filters that were used to observe the COSMOS field,
+which we are using for DESI-2 Lyman-alpha-emitter selection studies.
+
+Currently, this class only handles the intermediate-band filters that
+are within the g broad-band filter!
+'''
 class SuprimeImage(LegacySurveyImage):
 
     zp0 = {
@@ -31,27 +40,28 @@ class SuprimeImage(LegacySurveyImage):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # "sig1" is in nanomaggy units, but at some points we also
+        # need it in raw image units, which is misnamed sig1_blanton
+        # here!
         self.sig1_blanton = None
-    
+
     def get_ps1_band(self):
         from legacypipe.ps1cat import ps1cat
-        # Returns the integer index of the band in Pan-STARRS1 to use for an image in filter
-        # self.band.
-        # eg, g=0, r=1, i=2, z=3, Y=4
-        # FIXME
+        # Returns the integer index of the band in Pan-STARRS1 to use
+        # for an image in filter self.band.  eg, g=0, r=1, i=2, z=3,
+        # Y=4.  Here, we're assuming g band.
         return ps1cat.ps1band['g']
-    
+
     def read_image_primary_header(self, **kwargs):
-        # SuprimeCam images have an empty primary header, with a bunch of duplicated cards
-        # in the image HDUs, so we'll hack that here!
+        # SuprimeCam images have an empty primary header, with a bunch
+        # of duplicated cards in the image HDUs, so we'll hack that
+        # here!
         self._primary_header = self.read_image_fits()[1].read_header()
         return self._primary_header
 
     def get_band(self, primhdr):
         # FILTER01, but not in the primary header!
         # read from first extension!
-        #self.hdu = 1
-        #imghdr = self.read_image_header()
         band = primhdr['FILTER01']
         band = band.split()[0]
         return band
@@ -72,22 +82,24 @@ class SuprimeImage(LegacySurveyImage):
         return 'det%i' % hdr['DET-ID']
 
     def compute_filenames(self):
-        # Compute data quality and weight-map filenames
-        #self.dqfn = self.imgfn.replace('p.fits.fz', 'p.weight.fits.fz')
-        #assert(self.dqfn != self.imgfn)
+        # Compute data quality and weight-map filenames... we have the
+        # "p.weight.fits.fz" files that have partial DQ information,
+        # but we create our own "dq" calibration product that
+        # incorporates saturation and bleed trails.
         self.dqfn = None
         self.wtfn = None
         self.dq_hdu = 1
 
     def set_calib_filenames(self):
         super().set_calib_filenames()
-
+        # Add our custom "dq" product.
         calibdir = self.survey.get_calib_dir()
         imgdir = os.path.dirname(self.image_filename)
         basename = self.get_base_name()
         calname = self.name
-        self.dqfn = os.path.join(calibdir, 'dq', imgdir, basename, calname + '-dq.fits.fz')
-        
+        self.dqfn = os.path.join(calibdir, 'dq', imgdir, basename,
+                                 calname + '-dq.fits.fz')
+
     def read_invvar(self, slc=None, **kwargs):
         if self.sig1 is None or self.sig1 == 0.:
             if self.sig1_blanton is None:
@@ -97,7 +109,7 @@ class SuprimeImage(LegacySurveyImage):
                 slice2 = (slice(5,None,10),slice(5,None,10))
                 mad = np.median(np.abs(img[slice1] - img[slice2]).ravel())
                 self.sig1_blanton = 1.4826 * mad / np.sqrt(2.)
-                print('Computed sig1 by Blanton method:', self.sig1_blanton)
+                debug('Computed sig1 by Blanton method:', self.sig1_blanton)
             sig1 = self.sig1_blanton
         else:
             from tractor import NanoMaggies
@@ -105,7 +117,7 @@ class SuprimeImage(LegacySurveyImage):
             # but here we need to return in image units.
             zpscale = NanoMaggies.zeropointToScale(self.ccdzpt)
             sig1 = self.sig1 * zpscale
-            print('Suprime read_invvar: sig1 from CCDs file:', self.sig1, '-> image units', sig1)
+            debug('read_invvar: sig1 from CCDs file:', self.sig1, '-> image units', sig1)
 
         if slc is None:
             shape = self.shape
@@ -119,71 +131,18 @@ class SuprimeImage(LegacySurveyImage):
         iv[:,:] = 1./sig1**2
         return iv
 
-    # def read_dq(self, header=None, **kwargs):
-    #     dq = super().read_dq(header=header, **kwargs)
-    #     if header:
-    #         dq,hdr = dq
-    #     # .weight.fits.fz files: 1 = good
-    #     dq = 1 - dq
-    #     if header:
-    #         dq = dq,hdr
-    #     return dq
-
     def fix_saturation(self, img, dq, invvar, primhdr, imghdr, slc):
         invvar[dq > 0] = 0.
-
-    #     import numpy as np
-    #     from scipy.ndimage import label, find_objects
-    #     from legacypipe.bits import DQ_BITS
-    #     print('suprime fix_saturation.  sig1 =', self.sig1)
-    #     print('image max:', max(img))
-    #     #sat[i,j] = True
-    #     H,W = img.shape
-    #     # Find SATUR pixels, and mark as BLEED any vertically-connected runs of pixels
-    #     # with value 2 sigma above the median!
-    #     for col in range(W):
-    #         pixcol = img[:,col]
-    #         colsat = (pixcol > SATUR)
-    #         i, = np.nonzero(colsat)
-    #         if len(i) == 0:
-    #             continue
-    #         # MARK satur
-    #         dq[i,col] |= DQ_BITS['satur']
-    #         invvar[i,col] = 0.
-    # 
-    #         blobs,nblobs = label(colsat)
-    #         slcs = find_objects(blobs)
-    #         for sy, in slcs:
-    #             # Downward
-    #             for y in range(sy.start-1, -1, -1):
-    #                 if pixcol[y] < BLEED:
-    #                     # end of the run of BLEED pixels
-    #                     break
-    #                 if colsat[y]:
-    #                     # hit another SATUR region
-    #                     break
-    #             # MARK IT!
-    #             dq[y+1 : sy.start, col] |= DQ_BITS['bleed']
-    #             invvar[y+1 : sy.start, col] = 0.
-    # 
-    #             # Upward
-    #             for y in range(sy.stop, H):
-    #                 if pixcol[y] < BLEED:
-    #                     break
-    #                 if colsat[y]:
-    #                     break
-    #             # MARK IT!
-    #             dq[sy.stop : y-1, col] |= DQ_BITS['bleed']
-    #             invvar[sy.stop : y-1, col] = 0.
 
     def colorterm_ps1_to_observed(self, cat, band):
         from legacypipe.ps1cat import ps1cat
         g_index = ps1cat.ps1band['g']
-        r_index = ps1cat.ps1band['r']
-        #i_index = ps1cat.ps1band['i']
+        #r_index = ps1cat.ps1band['r']
+        i_index = ps1cat.ps1band['i']
         gmag = cat[:,g_index]
-        rmag = cat[:,r_index]
-        #imag = cat[:,i_index]
+        #rmag = cat[:,r_index]
+        imag = cat[:,i_index]
+        gi = gmag - imag
         colorterm = np.zeros(len(gmag))
         return colorterm
 
@@ -195,7 +154,6 @@ class SuprimeImage(LegacySurveyImage):
     def override_ccd_table_types(self):
         return {'camera':'S10',
                 'filter': 'S8',
-                #'ccdname': 'S4',
                 }
 
     def get_fwhm(self, primhdr, imghdr):
@@ -211,20 +169,17 @@ class SuprimeImage(LegacySurveyImage):
         self.ccdzpt = ccdzpt + 2.5 * np.log10(self.exptime)
 
     def run_calibs(self, **kwargs):
-        print('run_calibs(): dqfn = ', self.dqfn)
         if not os.path.exists(self.dqfn):
             import fitsio
             from scipy.ndimage import label, find_objects
             from legacypipe.bits import DQ_BITS
             from astrometry.util.file import trymakedirs
-
-            print('suprime creating DQ map.')
+            debug('Creating DQ map.')
             img = self.read_image()
             invvar = self.read_invvar()
-            print('image max:', np.max(img))
+            debug('image max:', np.max(img))
             med = np.median(img[invvar > 0])
-            print('image median:', med)
-            print('ccdname', self.ccdname)
+            debug('image median:', med)
             # in image pixel units (not nanomaggies)
             sig1 = self.sig1_blanton
             assert(sig1 is not None)
@@ -235,15 +190,17 @@ class SuprimeImage(LegacySurveyImage):
             dq = np.zeros(img.shape, np.int16)
             dq[msk == 0] = DQ_BITS['badpix']
 
-            # Around the margins, the image pixels are (very) negative, while the good regions
-            # have offsets of, say, 200 or 400 counts.  Zero out the ivar in the margins!
+            # Around the margins, the image pixels are (very)
+            # negative, while the good regions have offsets of, say,
+            # 200 or 400 counts.  Zero out the ivar in the margins!
             dq[img < 0] = DQ_BITS['badpix']
-            
+
             SATUR = 30000
             BLEED = med + 2 * self.sig1
             H,W = img.shape
-            # Find SATUR pixels, and mark as BLEED any vertically-connected runs of pixels
-            # with value 2 sigma above the median!
+            # Find SATUR pixels, and mark as BLEED any
+            # vertically-connected runs of pixels with value 2 sigma
+            # above the median!
             for col in range(W):
                 pixcol = img[:,col]
                 colsat = (pixcol > SATUR)
@@ -252,7 +209,7 @@ class SuprimeImage(LegacySurveyImage):
                     continue
                 # MARK satur
                 dq[i,col] |= DQ_BITS['satur']
-    
+                # Find connected BLEED pixels
                 blobs,nblobs = label(colsat)
                 slcs = find_objects(blobs)
                 for sy, in slcs:
@@ -264,16 +221,15 @@ class SuprimeImage(LegacySurveyImage):
                         if colsat[y]:
                             # hit another SATUR region
                             break
-                    # MARK IT!
+                    # Mark it!
                     dq[y+1 : sy.start, col] |= DQ_BITS['bleed']
-    
                     # Upward
                     for y in range(sy.stop, H):
                         if pixcol[y] < BLEED:
                             break
                         if colsat[y]:
                             break
-                    # MARK IT!
+                    # Mark it!
                     dq[sy.stop : y-1, col] |= DQ_BITS['bleed']
 
             fn = self.dqfn
@@ -282,30 +238,13 @@ class SuprimeImage(LegacySurveyImage):
             dirnm = os.path.dirname(tmpfn)
             trymakedirs(dirnm)
             F = fitsio.FITS(tmpfn, 'rw', clobber=True)
-            F.write(dq, extname=self.ccdname, compress='HCOMPRESS', tile_dims=(100,100))
+            F.write(dq, extname=self.ccdname, compress='HCOMPRESS',
+                    tile_dims=(100,100))
             F.close()
             os.rename(tmpfn, fn)
-            print('Wrote', fn)
-
+            info('Wrote', fn)
         super().run_calibs(**kwargs)
-
-    # Flip the weight map (1=good) to a flag map (1=bad)
-    # def run_se(self, imgfn, maskfn):
-    #     import fitsio
-    #     import os
-    #     from collections import Counter
-    #     from legacypipe.survey import create_temp
-    #     tmpmaskfn  = create_temp(suffix='.fits')
-    #     print('run_se: maskfn', maskfn)
-    #     #mask,hdr = self.read_dq(maskfn, header=True)
-    #     mask,hdr = self.read_dq(header=True)
-    #     print('Mask values:', Counter(mask.ravel()))
-    #     fitsio.write(tmpmaskfn, mask, header=hdr)
-    #     R = super().run_se(imgfn, tmpmaskfn)
-    #     os.unlink(tmpmaskfn)
-    #     return R
 
     @classmethod
     def get_nominal_pixscale(cls):
         return 0.2
-
