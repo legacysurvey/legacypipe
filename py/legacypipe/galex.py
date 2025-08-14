4c9cd5cf0f587634ded8f6352178f68b2a7063ff
Dustin Lang <dstndstn@gmail.com>
2025-08-14 10:04:13 -0400

    try to handle case of no GALEX coverage

--- py/legacypipe/galex.py
+++ py/legacypipe/galex.py
@@ -147,7 +147,7 @@ def stage_galex_forced(
         for i,p in enumerate(phots[:len(args)]):
             if p is None:
                 (_,_,tiles,band) = args[i][:4]
-                print('"None" result from GALEX forced phot:', tiles, band)
+                print('"None" result from GALEX forced phot band', band, '- probably no coverage')
                 continue
             galex_models.extend(p.models)
             if GALEX is None:
@@ -271,6 +271,9 @@ def galex_forcedphot(galex_dir, cat, tiles, band, roiradecbox,
         central_flux_acc[I] += tim.getImage()[y[I], x[I]] * iv
         central_flux_wt [I] += iv
         del x,y,good,I
+
+    if len(tims) == 0:
+        return None
 
     central_flux = central_flux_acc / np.maximum(central_flux_wt, 1e-16)
     del central_flux_acc, central_flux_wt
