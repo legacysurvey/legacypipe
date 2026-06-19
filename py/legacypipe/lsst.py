4cef46870e9d4c54eec9058bb3e4a84507144523
Dustin Lang <dstndstn@gmail.com>
2026-06-19 08:46:05 -0700

    lsst: tidy

--- py/legacypipe/lsst.py
+++ py/legacypipe/lsst.py
@@ -6,31 +6,10 @@ from legacypipe.image import LegacySurveyImage
 class LsstImage(HscImage):
     def __init__(self, survey, ccd, image_fn=None, image_hdu=0,
                  camera_setup=False, **kwargs):
-        print('LsstImage.__init__')
         super().__init__(survey, ccd, image_fn=image_fn, image_hdu=image_hdu, **kwargs)
-
-        # if image_fn is not None:
-        #     # Read metadata from image header.
-        #     self.image_filename = image_fn
-        # else:
-        #     imgfn = ccd.image_filename.strip()
-        #     self.image_filename = imgfn
-
         if camera_setup:
             return
-
         self.set_calib_filenames()
-        # calibdir = self.survey.get_calib_dir()
-        # imgdir = os.path.dirname(self.image_filename)
-        # basename = self.get_base_name()
-        # calname = self.name
-        # #self.sefn         = os.path.join(calibdir, 'se',           imgdir, basename, calname + '-se.fits')
-        # #self.psffn = os.path.join(calibdir, 'psfex-single', imgdir, basename, calname + '-psfex.fits')
-        # self.sefn         = os.path.join(calibdir, 'se', imgdir, calname + '-se.fits')
-        # self.psffn = os.path.join(calibdir, 'psfex-single', imgdir, calname + '-psfex.fits')
-        # print('LSST: setting SE filename:', self.sefn)
-        # print('LSST: setting PsfEx filename:', self.psffn)
-
         # Try grabbing fwhm from PSFEx file, if it exists.
         if hasattr(self, 'fwhm') and not np.isfinite(self.fwhm):
             try:
@@ -44,24 +23,11 @@ class LsstImage(HscImage):
         calibdir = self.survey.get_calib_dir()
         imgdir = os.path.dirname(self.image_filename)
         basename = self.get_base_name()
-        # if len(self.ccdname):
-        #     calname = basename + '-' + self.ccdname
-        # else:
-        #     calname = basename
-        # self.name = calname
         self.name = basename
-        #self.sefn         = os.path.join(calibdir, 'se',           imgdir, basename, calname + '-se.fits')
-        #self.psffn        = os.path.join(calibdir, 'psfex-single', imgdir, basename, calname + '-psfex.fits')
-        #self.skyfn        = os.path.join(calibdir, 'sky-single',   imgdir, basename, calname + '-splinesky.fits')
-        #self.sefn         = os.path.join(calibdir, 'se',           imgdir, calname + '-se.fits')
-        #self.psffn        = os.path.join(calibdir, 'psfex-single', imgdir, calname + '-psfex.fits')
         self.sefn         = os.path.join(calibdir, 'se',           imgdir, basename + '-se.fits')
         self.psffn        = os.path.join(calibdir, 'psfex-single', imgdir, basename + '-psfex.fits')
-        print('ccdname:', self.ccdname, 'psffn:', self.psffn)
         self.skyfn        = None
         self.merged_psffn = None
-        #self.merged_psffn = os.path.join(calibdir, 'psfex', imgdir, basename + '-psfex.fits')
-        #print('Merged PSFFN:', self.merged_psffn)
         self.merged_skyfn = None
         self.old_merged_skyfns = []
         self.old_merged_psffns = []
@@ -73,8 +39,10 @@ class LsstImage(HscImage):
         band = primhdr['FILTBAND']
         band = band.split()[0]
         return band
+
     def get_propid(self, primhdr):
         return primhdr.get('PROGRAM', '')
+
     def get_ccdname(self, primhdr, hdr):
         return primhdr['DETNAME'].strip().upper()
 
@@ -96,11 +64,11 @@ class LsstImage(HscImage):
     def get_radec_bore(self, primhdr):
         # miracle of miracles, they just put in decimal degrees
         return primhdr['RA'], primhdr['DEC']
+
     # There is a CCDGAIN ("rough guess") = 1.0,
     # or HIERARCH LSST ISR GAIN C10 = 1.64786902096928
     # for C0..C15 (oh but with some missing..?)
     def get_gain(self, primhdr, hdr):
-        #print('Primhdr:', primhdr)
         vals = []
         for i in range(16):
             key = 'LSST ISR GAIN C%02i'%i
@@ -111,16 +79,10 @@ class LsstImage(HscImage):
         return np.median(vals)
 
     def estimate_sky(self, img, invvar, dq, primhdr, imghdr):
-        #sup = super(LegacySurveyImage, self)
-        #print('sup:', sup)
-        #return sup.estimate_sky(img, invvar, dq, primhdr, imghdr)
         return LegacySurveyImage.estimate_sky(self, img, invvar, dq, primhdr, imghdr)
 
-    # def read_psf_model(self, x0, y0,
-    #                    gaussPsf=False, pixPsf=False, hybridPsf=False,
-    #                    normalizePsf=False, old_calibs_ok=False,
-    #                    psf_sigma=1., w=0, h=0):
-    #     # DP1 ComCam images: PiffPSF is stored as a pickle in a variable-length binary table.
-    #     # Appears to be Piff version 1.5.0
+    # DP1 ComCam images: PiffPSF is stored as a pickle in a variable-length binary table.
+    # Appears to be Piff version 1.5.0
+    # but that doesn't build at NERSC, so just fall back to PsfEx.
     def read_psf_model(self, *args, **kwargs):
         return LegacySurveyImage.read_psf_model(self, *args, **kwargs)
